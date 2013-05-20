6b8024c973083f25219326d7913ba5d141e1b12c
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-20 13:37:30 +0000

    DevTools: [HeapObjctsTracking] draw the real time data about heap objects in real time.
    Backend part of DevTools provides data about heap objects in real time.
    The UI part draws the data only after finish. But from the user point of view it is much
    more interesting to see the data in real time.
    I moved start/stop tracking code to the tracking profile type,
    added the code that draws the current time marker,
    and introduced support for a profile temporary view.
    
    BUG=none
    TEST=none
    R=alph@chromium.org
    
    Review URL: https://codereview.chromium.org/15297011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150664 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/ProfilesPanel.js
+++ front_end/ProfilesPanel.js
@@ -51,6 +51,14 @@ WebInspector.ProfileType.Events = {
 
 WebInspector.ProfileType.prototype = {
     /**
+     * @return {boolean}
+     */
+    hasTemporaryView: function()
+    {
+        return false;
+    },
+
+    /**
      * @return {string|null}
      */
     fileExtension: function()
@@ -716,7 +724,7 @@ WebInspector.ProfilesPanel.prototype = {
 
         sidebarParent.appendChild(profileTreeElement);
         if (!profile.isTemporary) {
-            if (!this.visibleView)
+            if (!this.visibleView || this.visibleView === this._launcherView)
                 this._showProfile(profile);
             this.dispatchEventToListeners("profile added", {
                 type: typeId
@@ -767,7 +775,7 @@ WebInspector.ProfilesPanel.prototype = {
      */
     _showProfile: function(profile)
     {
-        if (!profile || profile.isTemporary)
+        if (!profile || (profile.isTemporary && !profile.profileType().hasTemporaryView()))
             return null;
 
         var view = profile.view(this);
@@ -1127,6 +1135,8 @@ WebInspector.ProfilesPanel.prototype = {
         if (isProfiling) {
             this._launcherView.profileStarted();
             this._createTemporaryProfile(profileType);
+            if (profileTypeObject.hasTemporaryView())
+                this._showProfile(profileTypeObject.findTemporaryProfile());
         } else
             this._launcherView.profileFinished();
     },
