60d0e045854ac64c560bf49362b50810399ecace
zvorygin@chromium.org <zvorygin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-05 17:57:35 +0000

    [DevTools] Fixed console.table on cornercases with large amount of columns.
    BUG=264230
    
    Review URL: https://chromiumcodereview.appspot.com/21923004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/ConsoleMessage.js
+++ front_end/ConsoleMessage.js
@@ -508,10 +508,13 @@ WebInspector.ConsoleMessageImpl.prototype = {
 
             var rowValue = {};
             const maxColumnsToRender = 20;
-            for (var j = 0; j < rowPreview.properties.length && columnNames.length < maxColumnsToRender; ++j) {
+            for (var j = 0; j < rowPreview.properties.length && j < maxColumnsToRender; ++j) {
                 var cellProperty = rowPreview.properties[j];
-                if (columnNames.indexOf(cellProperty.name) === -1)
+                if (columnNames.indexOf(cellProperty.name) === -1) {
+                    if (columnNames.length === maxColumnsToRender)
+                        continue;
                     columnNames.push(cellProperty.name);
+                }
                 rowValue[cellProperty.name] = this._renderPropertyPreview(cellProperty);
             }
             rows.push([rowProperty.name, rowValue]);
