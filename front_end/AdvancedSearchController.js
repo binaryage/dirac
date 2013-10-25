dd9b1959672a1fd1fd28a80202fc75e50cb5c131
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-25 17:02:47 +0000

    DevTools: Fix search across sources UI and behavior.
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/40753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/AdvancedSearchController.js
+++ front_end/AdvancedSearchController.js
@@ -103,12 +103,12 @@ WebInspector.AdvancedSearchController.prototype = {
         if (selection.rangeCount)
             queryCandidate = selection.toString().replace(/\r?\n.*/, "");
 
-        if (this._searchView && this._searchView.isShowing())
-            this._searchView.focus();
-        else
+        if (!this._searchView || !this._searchView.isShowing())
             WebInspector.inspectorView.showViewInDrawer("search");
         if (queryCandidate)
             this._searchView._search.value = queryCandidate;
+        this._searchView.focus();
+
         this.startIndexing();
     },
 
@@ -230,7 +230,7 @@ WebInspector.SearchView = function(controller)
 
     this._controller = controller;
 
-    this.element.className = "search-view";
+    this.element.className = "search-view vbox";
 
     this._searchPanelElement = this.element.createChild("div", "search-drawer-header");
     this._searchPanelElement.addEventListener("keydown", this._onKeyDown.bind(this), false);
@@ -238,10 +238,9 @@ WebInspector.SearchView = function(controller)
     this._searchResultsElement = this.element.createChild("div");
     this._searchResultsElement.className = "search-results";
     
-    this._searchLabel = this._searchPanelElement.createChild("span");
-    this._searchLabel.textContent = WebInspector.UIString("Search sources");
     this._search = this._searchPanelElement.createChild("input");
-    this._search.setAttribute("type", "search");
+    this._search.placeholder = WebInspector.UIString("Search sources");
+    this._search.setAttribute("type", "text");
     this._search.addStyleClass("search-config-search");
     this._search.setAttribute("results", "0");
     this._search.setAttribute("size", 30);
