8abd4f4f2ce0ca301a0581ab822a97aa10c9ce1f
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-30 18:53:37 +0000

    DevTools: Resize timeline panel when filters are shown.
    BUG=312568
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/51003003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/TimelinePanel.js
+++ front_end/TimelinePanel.js
@@ -346,6 +346,7 @@ WebInspector.TimelinePanel.prototype = {
     {
         var toggled = /** @type {boolean} */ (event.data);
         this._filtersContainer.enableStyleClass("hidden", !toggled);
+        this.onResize();
     },
 
     /**
