b5b3b5caa5e485d1c47df8a72ebf54a584426ed2
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-12 07:20:51 +0000

    Revert "DevTools: Fix the bug when open resource dialog sometimes loses focus when opened at the app start."
    This reverts commit 5a451ea411cafa6498846fac862778e5f5d967e3.
    
    TBR=pfeldman
    BUG=318015
    
    Review URL: https://codereview.chromium.org/60093003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161771 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/FilteredItemSelectionDialog.js
+++ front_end/FilteredItemSelectionDialog.js
@@ -51,7 +51,6 @@ WebInspector.FilteredItemSelectionDialog = function(delegate)
 
     this._promptElement = this.element.createChild("input", "monospace");
     this._promptElement.addEventListener("input", this._onInput.bind(this), false);
-    this._promptElement.addEventListener("blur", this._onBlur.bind(this), false);
     this._promptElement.type = "text";
     this._promptElement.setAttribute("spellcheck", "false");
 
@@ -246,11 +245,6 @@ WebInspector.FilteredItemSelectionDialog.prototype = {
                 this._selectedIndexInFiltered = 0;
             this._updateSelection(this._selectedIndexInFiltered, false);
         }
-    },
-
-    _onBlur: function(event)
-    {
-        this.focus();
     },
 
     _onInput: function(event)
