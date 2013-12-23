704d101a6fad03d6d096fa74a7148572f84bc38e
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-23 13:33:45 +0000

    DevTools: [Elements] Fix click handling for the sidebar tabbed pane chevron
    R=aandrey, eustas, pfeldman@chromium.org
    BUG=330497
    
    Review URL: https://codereview.chromium.org/112903004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164288 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/ElementsPanel.js
+++ front_end/ElementsPanel.js
@@ -1220,10 +1220,8 @@ WebInspector.ElementsPanel.prototype = {
         if (this.sidebarPaneView && vertically === !this.splitView.isVertical())
             return;
 
-        if (this.sidebarPaneView) {
+        if (this.sidebarPaneView)
             this.sidebarPaneView.detach();
-            this.splitView.uninstallResizer(this.sidebarPaneView.headerElement());
-        }
 
         this.splitView.setVertical(!vertically);
 
@@ -1265,7 +1263,6 @@ WebInspector.ElementsPanel.prototype = {
         this.sidebarPaneView = new WebInspector.SidebarTabbedPane();
 
         if (vertically) {
-            this.splitView.installResizer(this.sidebarPaneView.headerElement());
             this.sidebarPanes.metrics.show(computedPane.bodyElement, this.sidebarPanes.computedStyle.element);
             this.sidebarPanes.metrics.setExpandCallback(expandComputed);
 
