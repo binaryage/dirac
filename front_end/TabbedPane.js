binaryage/dirac

commit 64d8cbac198b0b73fce2ad21aa8bdce8e6ae3d45

DevTools: Do not load the first panel appended to the panels tabbed pane
Show the first tab view lazily, when the tabbed pane is shown.

R=pfeldman@chromium.org, pfeldman, vsevik

Review URL: https://codereview.chromium.org/151903005

git-svn-id: svn://svn.chromium.org/blink/trunk@166357 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/TabbedPane.js
@@ -181,7 +181,7 @@ WebInspector.TabbedPane.prototype = {
 
         this._tabsHistory.push(tab);
 
-        if (this._tabsHistory[0] === tab)
+        if (this._tabsHistory[0] === tab && this.isShowing())
             this.selectTab(tab.id, userGesture);
 
         this._updateTabElements();
@@ -368,6 +368,16 @@ WebInspector.TabbedPane.prototype = {
     headerResized: function()
     {
         this._updateTabElements();
+    },
+
+    /**
+     * @param {?Element} parentElement
+     * @param {!Element=} insertBefore
+     */
+    show: function(parentElement, insertBefore)
+    {
+        WebInspector.View.prototype.show.call(this, parentElement, insertBefore);
+        this.selectTab(this._currentTab || this._tabsHistory[0]);
     },
 
     _updateTabElements: function()

front_end/Drawer.js
@@ -48,7 +48,6 @@ WebInspector.Drawer = function(splitView)
     this._tabbedPane = new WebInspector.TabbedPane();
     this._tabbedPane.closeableTabs = false;
     this._tabbedPane.setRetainTabOrder(true, WebInspector.moduleManager.orderComparator(WebInspector.Drawer.ViewFactory, "name", "order"));
-    this._tabbedPane.show(this.element);
 
     this._tabbedPane.addEventListener(WebInspector.TabbedPane.EventTypes.TabSelected, this._tabSelected, this);
     splitView.installResizer(this._tabbedPane.headerElement());
@@ -80,6 +79,7 @@ WebInspector.Drawer.prototype = {
                 this._tabbedPane.appendTab(id, title, new WebInspector.View());
             }
         }
+        this._tabbedPane.show(this.element);
     },
 
     /**

commit 036c322fabbaf6377ef0722f334534683aaa9698

DevTools: Fix default Sources panel editor selection
No editor is currently selected in the Sources panel by default. Regressed in r166357.

R=vsevik
NOTRY=true

Review URL: https://codereview.chromium.org/153633003

git-svn-id: svn://svn.chromium.org/blink/trunk@166388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/TabbedPane.js
@@ -370,14 +370,11 @@ WebInspector.TabbedPane.prototype = {
         this._updateTabElements();
     },
 
-    /**
-     * @param {?Element} parentElement
-     * @param {!Element=} insertBefore
-     */
-    show: function(parentElement, insertBefore)
+    wasShown: function()
     {
-        WebInspector.View.prototype.show.call(this, parentElement, insertBefore);
-        this.selectTab(this._currentTab || this._tabsHistory[0]);
+        var effectiveTab = this._currentTab || this._tabsHistory[0];
+        if (effectiveTab)
+            this.selectTab(effectiveTab.id);
     },
 
     _updateTabElements: function()