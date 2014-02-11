binaryage/dirac

commit d740b401780c756b93c1ff2f4e07a697bcdcf6d4

DevTools: Migrate anchor click-revelation to extensions
R=pfeldman

Review URL: https://codereview.chromium.org/145623010

git-svn-id: svn://svn.chromium.org/blink/trunk@166725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/SourcesPanel.js
@@ -477,24 +477,6 @@ WebInspector.SourcesPanel.prototype = {
     },
 
     /**
-     * @param {!Element} anchor
-     * @return {boolean}
-     */
-    showAnchorLocation: function(anchor)
-    {
-        if (!anchor.uiSourceCode) {
-            var uiSourceCode = WebInspector.workspace.uiSourceCodeForURL(anchor.href);
-            if (uiSourceCode)
-                anchor.uiSourceCode = uiSourceCode;
-        }
-        if (!anchor.uiSourceCode)
-            return false;
-
-        this._showSourceLocation(anchor.uiSourceCode, anchor.lineNumber, anchor.columnNumber);
-        return true;
-    },
-
-    /**
      * @param {!WebInspector.UISourceCode} uiSourceCode
      * @param {number=} lineNumber
      * @param {number=} columnNumber

front_end/AdvancedSearchController.js
@@ -623,12 +623,7 @@ WebInspector.FileBasedSearchResultsPane.prototype = {
      */
     _createAnchor: function(uiSourceCode, lineNumber, columnNumber)
     {
-        var anchor = document.createElement("a");
-        anchor.preferredPanel = "sources";
-        anchor.href = sanitizeHref(uiSourceCode.originURL());
-        anchor.uiSourceCode = uiSourceCode;
-        anchor.lineNumber = lineNumber;
-        return anchor;
+        return WebInspector.Linkifier.linkifyUsingRevealer(new WebInspector.UILocation(uiSourceCode, lineNumber, columnNumber), "", uiSourceCode.url, lineNumber);
     },
 
     /**

front_end/AuditRules.js
@@ -1279,7 +1279,6 @@ WebInspector.AuditRules.VendorPrefixedCSSProperties.prototype = {
                 this._styleSheetResult = result.addChild(rule.sourceURL ? WebInspector.linkifyResourceAsNode(rule.sourceURL) : WebInspector.UIString("<unknown>"));
             if (!this._ruleResult) {
                 var anchor = WebInspector.linkifyURLAsNode(rule.sourceURL, rule.selectorText);
-                anchor.preferredPanel = "resources";
                 anchor.lineNumber = rule.lineNumberInSource();
                 this._ruleResult = this._styleSheetResult.addChild(anchor);
             }

front_end/CPUProfileView.js
@@ -413,9 +413,7 @@ WebInspector.CPUProfileView.prototype = {
         var script = WebInspector.debuggerModel.scriptForId(node.scriptId)
         if (!script)
             return;
-        var uiLocation = script.rawLocationToUILocation(node.lineNumber);
-        if (uiLocation)
-            uiLocation.reveal();
+        WebInspector.Revealer.reveal(script.rawLocationToUILocation(node.lineNumber));
     },
 
     _changeView: function()

front_end/ConsoleMessage.js
@@ -103,6 +103,16 @@ WebInspector.ConsoleMessageImpl.prototype = {
         this._formattedMessage = document.createElement("span");
         this._formattedMessage.className = "console-message-text source-code";
 
+        /**
+         * @param {string} title
+         * @return {!Element}
+         * @this {WebInspector.NetworkRequest}
+         */
+        function linkifyRequest(title)
+        {
+            return WebInspector.Linkifier.linkifyUsingRevealer(this, title, this.url);
+        }
+
         if (!this._messageElement) {
             if (this.source === WebInspector.ConsoleMessage.MessageSource.ConsoleAPI) {
                 switch (this.type) {
@@ -142,13 +152,13 @@ WebInspector.ConsoleMessageImpl.prototype = {
                     this._messageElement = document.createElement("span");
                     if (this.level === WebInspector.ConsoleMessage.MessageLevel.Error) {
                         this._messageElement.appendChild(document.createTextNode(this._request.requestMethod + " "));
-                        this._messageElement.appendChild(WebInspector.linkifyRequestAsNode(this._request));
+                        this._messageElement.appendChild(WebInspector.Linkifier.linkifyUsingRevealer(this._request, this._request.url, this._request.url));
                         if (this._request.failed)
                             this._messageElement.appendChild(document.createTextNode(" " + this._request.localizedFailDescription));
                         else
                             this._messageElement.appendChild(document.createTextNode(" " + this._request.statusCode + " (" + this._request.statusText + ")"));
                     } else {
-                        var fragment = WebInspector.linkifyStringAsFragmentWithCustomLinkifier(this._messageText, WebInspector.linkifyRequestAsNode.bind(null, this._request));
+                        var fragment = WebInspector.linkifyStringAsFragmentWithCustomLinkifier(this._messageText, linkifyRequest.bind(this._request));
                         this._messageElement.appendChild(fragment);
                     }
                 } else {

front_end/DOMAgent.js
@@ -715,13 +715,6 @@ WebInspector.DOMNode.prototype = {
                 return WebInspector.ParsedURL.completeURL(frameOwnerCandidate.baseURL, url);
         }
         return null;
-    },
-
-    reveal: function()
-    {
-        var revealer = WebInspector.moduleManager.instance(WebInspector.Revealer, this);
-        if (revealer)
-            revealer.reveal(this);
     }
 }
 

front_end/ElementsTreeOutline.js
@@ -1010,7 +1010,7 @@ WebInspector.ElementsTreeElement.prototype = {
 
     _preventFollowingLinksOnDoubleClick: function()
     {
-        var links = this.listItemElement.querySelectorAll("li > .webkit-html-tag > .webkit-html-attribute > .webkit-html-external-link, li > .webkit-html-tag > .webkit-html-attribute > .webkit-html-resource-link");
+        var links = this.listItemElement.querySelectorAll("li .webkit-html-tag > .webkit-html-attribute > .webkit-html-external-link, li .webkit-html-tag > .webkit-html-attribute > .webkit-html-resource-link");
         if (!links)
             return;
 

front_end/ExtensionServer.js
@@ -302,10 +302,25 @@ WebInspector.ExtensionServer.prototype = {
 
     _onOpenResource: function(message)
     {
-        var a = document.createElement("a");
-        a.href = message.url;
-        a.lineNumber = message.lineNumber;
-        return WebInspector.showAnchorLocation(a) ? this._status.OK() : this._status.E_NOTFOUND(message.url);
+        var uiSourceCode = WebInspector.workspace.uiSourceCodeForURL(message.url);
+        if (uiSourceCode) {
+            WebInspector.Revealer.reveal(new WebInspector.UILocation(uiSourceCode, message.lineNumber, 0));
+            return this._status.OK();
+        }
+
+        var resource = WebInspector.resourceForURL(message.url);
+        if (resource) {
+            WebInspector.Revealer.reveal(resource, message.lineNumber);
+            return this._status.OK();
+        }
+
+        var request = WebInspector.networkLog.requestForURL(message.url);
+        if (request) {
+            WebInspector.Revealer.reveal(request);
+            return this._status.OK();
+        }
+
+        return this._status.E_NOTFOUND(message.url);
     },
 
     _onSetOpenResourceHandler: function(message, port)

commit 047e83df112422e2347522b92608858b1047111a

DevTools: Fix "Show editor in drawer" experiment
This regressed in r166725

R=vsevik
NOTRY=true

Review URL: https://codereview.chromium.org/157803009

git-svn-id: svn://svn.chromium.org/blink/trunk@166908 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/SourcesPanel.js
@@ -1825,6 +1825,6 @@ WebInspector.SourcesPanel.UILocationRevealer.prototype = {
     reveal: function(uiLocation)
     {
         if (uiLocation instanceof WebInspector.UILocation)
-            /** @type {!WebInspector.SourcesPanel} */ (WebInspector.showPanel("sources")).showUILocation(uiLocation);
+            /** @type {!WebInspector.SourcesPanel} */ (WebInspector.panel("sources")).showUILocation(uiLocation);
     }
 }