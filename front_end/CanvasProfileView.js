53d1b7b630456bc73cea8a5685faba2a4d06acc9
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-27 10:29:23 +0000

    DevTools: Replace Apple styled spinner icon with Chromium one.
    Drive-by: replace animated PNG files with a CSS animation.
    
    R=yurys@chromium.org
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=167914
    
    Review URL: https://codereview.chromium.org/181433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168008 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/CanvasProfileView.js
+++ front_end/CanvasProfileView.js
@@ -55,7 +55,7 @@ WebInspector.CanvasProfileView = function(profile)
     replayImageContainer.id = "canvas-replay-image-container";
     this._replayImageElement = replayImageContainer.createChild("img", "canvas-replay-image");
     this._debugInfoElement = replayImageContainer.createChild("div", "canvas-debug-info hidden");
-    this._spinnerIcon = replayImageContainer.createChild("img", "canvas-spinner-icon hidden");
+    this._spinnerIcon = replayImageContainer.createChild("div", "spinner-icon small hidden");
 
     var replayLogContainer = this._imageSplitView.sidebarElement();
     var controlsContainer = replayLogContainer.createChild("div", "status-bar");
