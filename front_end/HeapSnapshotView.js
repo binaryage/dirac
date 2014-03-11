binaryage/dirac

commit 6dc5391af19ca4e14a5db24152cd62cad4a0f054

Continue recording object allocation after navigation
Old profile is removed on HeapProfiler.resetProfiles message and a new one is added immediately to collect all object allocation events from the backend after navigation.

BUG=None

Review URL: https://codereview.chromium.org/172163005

git-svn-id: svn://svn.chromium.org/blink/trunk@168565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/HeapSnapshotView.js
@@ -1005,6 +1005,12 @@ WebInspector.TrackingHeapSnapshotProfileType.prototype = {
     {
         if (this.profileBeingRecorded())
             return;
+        this._addNewProfile();
+        HeapProfilerAgent.startTrackingHeapObjects(WebInspector.experimentsSettings.allocationProfiler.isEnabled());
+    },
+
+    _addNewProfile: function()
+    {
         this._profileBeingRecorded = new WebInspector.HeapProfileHeader(this);
         this._lastSeenIndex = -1;
         this._profileSamples = {
@@ -1018,7 +1024,6 @@ WebInspector.TrackingHeapSnapshotProfileType.prototype = {
         this._recording = true;
         this.addProfile(this._profileBeingRecorded);
         this._profileBeingRecorded.updateStatus(WebInspector.UIString("Recording\u2026"));
-        HeapProfilerAgent.startTrackingHeapObjects(WebInspector.experimentsSettings.allocationProfiler.isEnabled());
         this.dispatchEventToListeners(WebInspector.TrackingHeapSnapshotProfileType.TrackingStarted);
     },
 
@@ -1066,11 +1071,14 @@ WebInspector.TrackingHeapSnapshotProfileType.prototype = {
 
     _reset: function()
     {
+        var wasRecording = this._recording;
+        // Clear current profile to avoid stopping backend.
+        this._profileBeingRecorded = null;
         WebInspector.HeapSnapshotProfileType.prototype._reset.call(this);
-        if (this._recording)
-            this._stopRecordingProfile();
         this._profileSamples = null;
         this._lastSeenIndex = -1;
+        if (wasRecording)
+            this._addNewProfile();
     },
 
     /**
@@ -1158,7 +1166,8 @@ WebInspector.HeapProfileHeader.prototype = {
 
     _finishLoad: function()
     {
-        this._receiver.close(function() {});
+        if (!this._wasDisposed)
+            this._receiver.close(function() {});
         if (this._bufferedWriter) {
             this._bufferedWriter.close(this._didWriteToTempFile.bind(this));
             this._bufferedWriter = null;
@@ -1167,6 +1176,11 @@ WebInspector.HeapProfileHeader.prototype = {
 
     _didWriteToTempFile: function(tempFile)
     {
+        if (this._wasDisposed) {
+            if (tempFile)
+                tempFile.remove();
+            return;
+        }
         this._tempFile = tempFile;
         if (!tempFile)
             this._failedToCreateTempFile = true;

commit 00674138ce6a0d4dae9f5b7696c5e1cd0669e70d

Remove current profile when clearing all profiles
When all profiles are removed current "Record Object Allocations" profile will also disappear. It fixes regression introduced in r168565: current profile is preserved only in case when _reset is called as a result of HeapProfiler.resetProfiles protocol message handling.

BUG=350799
R=loislo@chromium.org

Review URL: https://codereview.chromium.org/194623004

git-svn-id: svn://svn.chromium.org/blink/trunk@168915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/HeapSnapshotView.js
@@ -1080,12 +1080,15 @@ WebInspector.TrackingHeapSnapshotProfileType.prototype = {
         return WebInspector.UIString("Record JavaScript object allocations over time. Use this profile type to isolate memory leaks.");
     },
 
-    _reset: function()
+    /**
+     * @override
+     */
+    resetProfiles: function()
     {
         var wasRecording = this._recording;
         // Clear current profile to avoid stopping backend.
         this._profileBeingRecorded = null;
-        WebInspector.HeapSnapshotProfileType.prototype._reset.call(this);
+        WebInspector.HeapSnapshotProfileType.prototype.resetProfiles.call(this);
         this._profileSamples = null;
         this._lastSeenIndex = -1;
         if (wasRecording)