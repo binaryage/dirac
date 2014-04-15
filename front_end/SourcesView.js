8222b23ebcb5e7af1aff300d44f89ab6bb6e4cc0
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-15 08:03:43 +0000

    DevTools: Do not show go to line dialog when there is no open tab in the editor.
    BUG=358102
    R=apavlov@chromium.org, apavlov, pfeldman
    
    Review URL: https://codereview.chromium.org/238243004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/SourcesView.js
+++ front_end/SourcesView.js
@@ -627,7 +627,8 @@ WebInspector.SourcesView.prototype = {
      */
     _showGoToLineDialog: function(event)
     {
-        this.showOpenResourceDialog(":");
+        if (this._currentUISourceCode)
+            this.showOpenResourceDialog(":");
         return true;
     },
 
