714e791566ab7894e530e47d214165e9f26a666f
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-20 15:34:57 +0000

    Remove _callSiteStackTrace field from TimelineRecordImpl
    callSiteStackTrace should always return async initiator's stack trace.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/299683003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174386 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/timeline/TimelineModel.js
+++ front_end/timeline/TimelineModel.js
@@ -700,9 +700,6 @@ WebInspector.TimelineModel.RecordImpl = function(model, timelineEvent, parentRec
 
     this._selfTime = this.endTime() - this.startTime();
 
-    if (parentRecord && parentRecord.callSiteStackTrace())
-        this._callSiteStackTrace = parentRecord.callSiteStackTrace();
-
     var recordTypes = WebInspector.TimelineModel.RecordType;
     switch (timelineEvent.type) {
     case recordTypes.ResourceSendRequest:
@@ -743,20 +740,17 @@ WebInspector.TimelineModel.RecordImpl = function(model, timelineEvent, parentRec
     case recordTypes.InvalidateLayout:
         // Consider style recalculation as a reason for layout invalidation,
         // but only if we had no earlier layout invalidation records.
-        var initiator = this;
+        var layoutInitator = this;
         if (!bindings._layoutInvalidate[this.frameId()] && parentRecord.type() === recordTypes.RecalculateStyles)
-            initiator = parentRecord;
-        bindings._layoutInvalidate[this.frameId()] = initiator;
+            layoutInitator = parentRecord._initiator;
+        bindings._layoutInvalidate[this.frameId()] = layoutInitator;
         break;
 
     case recordTypes.Layout:
         this._initiator = bindings._layoutInvalidate[this.frameId()];
-        if (this._initiator)
-            this._callSiteStackTrace = this._initiator.callSiteStackTrace()
+        bindings._layoutInvalidate[this.frameId()] = null;
         if (this.stackTrace())
             this.addWarning(WebInspector.UIString("Forced synchronous layout is a possible performance bottleneck."));
-
-        bindings._layoutInvalidate[this.frameId()] = null;
         this.highlightQuad = timelineEvent.data.root || WebInspector.TimelineModel._quadFromRectData(timelineEvent.data);
         break;
 
@@ -799,8 +793,6 @@ WebInspector.TimelineModel.RecordImpl.prototype = {
      */
     callSiteStackTrace: function()
     {
-        if (this._callSiteStackTrace)
-            return this._callSiteStackTrace;
         return this._initiator ? this._initiator.stackTrace() : null;
     },
 
