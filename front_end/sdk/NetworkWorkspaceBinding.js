bbc374e6b6ad0d6924661fa61fbd0edbb7c2e4cd
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-23 13:43:16 +0000

    DevTools: Unescape urls when showing them in navigator/editor.
    BUG=161251
    
    Review URL: https://codereview.chromium.org/349523003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176754 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/sdk/NetworkWorkspaceBinding.js
+++ front_end/sdk/NetworkWorkspaceBinding.js
@@ -129,8 +129,8 @@ WebInspector.NetworkWorkspaceBinding.prototype = {
     {
         var splitURL = WebInspector.ParsedURL.splitURL(url);
         var projectName = splitURL[0];
-        var parentPath = splitURL.slice(1, splitURL.length - 1).join("/");
-        var name = splitURL[splitURL.length - 1];
+        var parentPath = decodeURI(splitURL.slice(1, splitURL.length - 1).join("/"));
+        var name = decodeURI(splitURL[splitURL.length - 1]);
         var projectDelegate = this._projectDelegate(projectName, isContentScript || false);
         var path = projectDelegate.addFile(parentPath, name, url, contentProvider);
         var uiSourceCode = /** @type {!WebInspector.UISourceCode} */ (this._workspace.uiSourceCode(projectDelegate.id(), path));
