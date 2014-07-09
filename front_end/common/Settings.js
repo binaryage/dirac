893f43d7a08f99a135c6f1a058fc001cbaed8032
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-09 15:25:45 +0000

    DevTools: unhide devices panel experiment.
    TBR=vsevik
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/380753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/common/Settings.js
+++ front_end/common/Settings.js
@@ -353,7 +353,7 @@ WebInspector.ExperimentsSettings = function(experimentsEnabled)
     // Add currently running experiments here.
     this.applyCustomStylesheet = this._createExperiment("applyCustomStylesheet", "Allow custom UI themes");
     this.canvasInspection = this._createExperiment("canvasInspection ", "Canvas inspection");
-    this.devicesPanel = this._createExperiment("devicesPanel", "Devices panel", true);
+    this.devicesPanel = this._createExperiment("devicesPanel", "Devices panel");
     this.disableAgentsWhenProfile = this._createExperiment("disableAgentsWhenProfile", "Disable other agents and UI when profiler is active", true);
     this.dockToLeft = this._createExperiment("dockToLeft", "Dock to left", true);
     this.editorInDrawer = this._createExperiment("showEditorInDrawer", "Editor in drawer", true);
