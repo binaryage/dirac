56f0c3035fab3e541533f345080912ac065bda72
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-19 07:43:44 +0000

    DevTools: Resource Timing Table: adjust UI and cleanup code.
    1) Fix vertical line alignment
    2) Cleanup code
    3) Add JSDocs
    
    BUG=
    
    Review URL: https://codereview.chromium.org/481863002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/network/RequestTimingView.js
+++ front_end/network/RequestTimingView.js
@@ -89,20 +89,39 @@ WebInspector.RequestTimingView.prototype = {
  */
 WebInspector.RequestTimingView.createTimingTable = function(request)
 {
-    var tableElement = document.createElement("table");
-    tableElement.className = "network-timing-table";
-    var rows = [];
+    var tableElement = document.createElementWithClass("table", "network-timing-table");
 
+    /**
+     * @param {string} title
+     * @param {string} className
+     * @param {number} start
+     * @param {number} end
+     */
     function addRow(title, className, start, end)
     {
-        var row = {};
-        row.title = title;
-        row.className = className;
-        row.start = start;
-        row.end = end;
-        rows.push(row);
+        var tr = tableElement.createChild("tr");
+        tr.createChild("td").createTextChild(title);
+        var td = tr.createChild("td");
+        td.width = chartWidth + "px";
+        var row = td.createChild("div", "network-timing-row");
+
+        var bar = row.createChild("span", "network-timing-bar " + className);
+        bar.style.left = Math.floor(scale * start) + "px";
+        bar.style.right = Math.floor(scale * (total - end)) + "px";
+        bar.textContent = "\u200B"; // Important for 0-time items to have 0 width.
+
+        var label = row.createChild("span", "network-timing-bar-title");
+        if (total - end < start)
+            label.style.right = (scale * (total - end)) + "px";
+        else
+            label.style.left = (scale * start) + "px";
+        label.textContent = Number.secondsToString((end - start) / 1000, true);
     }
 
+    /**
+     * @param {!Array.<number>} numbers
+     * @return {number|undefined}
+     */
     function firstPositive(numbers)
     {
         for (var i = 0; i < numbers.length; ++i) {
@@ -116,6 +135,8 @@ WebInspector.RequestTimingView.createTimingTable = function(request)
     var blocking = firstPositive([timing.dnsStart, timing.connectStart, timing.sendStart]);
     var endTime = firstPositive([request.endTime, request.responseReceivedTime, timing.requestTime]);
     var total = (endTime - timing.requestTime) * 1000;
+    const chartWidth = 200;
+    var scale = chartWidth / total;
 
     if (blocking > 0)
         addRow(WebInspector.UIString("Blocking"), "blocking", 0, blocking);
@@ -137,44 +158,6 @@ WebInspector.RequestTimingView.createTimingTable = function(request)
 
     if (request.endTime !== -1)
         addRow(WebInspector.UIString("Receiving"), "receiving", (request.responseReceivedTime - timing.requestTime) * 1000, total);
-
-    const chartWidth = 200;
-    var scale = chartWidth / total;
-
-    for (var i = 0; i < rows.length; ++i) {
-        var tr = document.createElement("tr");
-        tableElement.appendChild(tr);
-
-        var td = document.createElement("td");
-        td.textContent = rows[i].title;
-        tr.appendChild(td);
-
-        td = document.createElement("td");
-        td.width = chartWidth + "px";
-
-        var row = document.createElement("div");
-        row.className = "network-timing-row";
-        td.appendChild(row);
-
-        var bar = document.createElement("span");
-        bar.className = "network-timing-bar " + rows[i].className;
-        bar.style.left = Math.floor(scale * rows[i].start) + "px";
-        bar.style.right = Math.floor(scale * (total - rows[i].end)) + "px";
-        bar.style.backgroundColor = rows[i].color;
-        bar.textContent = "\u200B"; // Important for 0-time items to have 0 width.
-        row.appendChild(bar);
-
-        var title = document.createElement("span");
-        title.className = "network-timing-bar-title";
-        if (total - rows[i].end < rows[i].start)
-            title.style.right = (scale * (total - rows[i].end) + 3) + "px";
-        else
-            title.style.left = (scale * rows[i].start + 3) + "px";
-        title.textContent = Number.secondsToString((rows[i].end - rows[i].start) / 1000, true);
-        row.appendChild(title);
-
-        tr.appendChild(td);
-    }
 
     if (!request.finished) {
         var cell = tableElement.createChild("tr").createChild("td", "caution");
