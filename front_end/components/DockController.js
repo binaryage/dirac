7386d88fe46641625f7e3c5e9e766eaa27ce81b9
vsevik@chromium.org <vsevik@chromium.org>
2014-09-01 11:19:58 +0000

    DevTools: Change degault dock settings.
    BUG=408242
    
    Review URL: https://codereview.chromium.org/527743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/components/DockController.js
+++ front_end/components/DockController.js
@@ -68,7 +68,7 @@ WebInspector.DockController.prototype = {
         if (!this._canDock)
             return;
 
-        this._states = [WebInspector.DockController.State.DockedToBottom, WebInspector.DockController.State.Undocked, WebInspector.DockController.State.DockedToRight];
+        this._states = [WebInspector.DockController.State.DockedToRight, WebInspector.DockController.State.DockedToBottom, WebInspector.DockController.State.Undocked];
         this._titles = [WebInspector.UIString("Dock to main window."), WebInspector.UIString("Undock into separate window."), WebInspector.UIString("Dock to main window.")];
         if (WebInspector.experimentsSettings.dockToLeft.isEnabled()) {
             this._states.push(WebInspector.DockController.State.DockedToLeft);
