0a51fe95339cf4eb58368e2b3e7c4153a597b275
kozyatinskiy@google.com <kozyatinskiy@google.com>
2014-10-31 11:45:25 +0000

    [DevTools] Fix replace script-based uiSourceCode with network-based
    Script-based and network-based source codes can have empty url and in this situtation devtools can replace correct debug-based ui source code with random network-based.
    
    R=vsevik@chromium.org
    
    Review URL: https://codereview.chromium.org/684093007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184714 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/sources/SourcesView.js
+++ front_end/sources/SourcesView.js
@@ -255,7 +255,7 @@ WebInspector.SourcesView.prototype = {
         this._editorContainer.addUISourceCode(uiSourceCode);
         // Replace debugger script-based uiSourceCode with a network-based one.
         var currentUISourceCode = this._currentUISourceCode;
-        if (currentUISourceCode && currentUISourceCode.project().isServiceProject() && currentUISourceCode !== uiSourceCode && currentUISourceCode.url === uiSourceCode.url) {
+        if (currentUISourceCode && currentUISourceCode.project().isServiceProject() && currentUISourceCode !== uiSourceCode && currentUISourceCode.url === uiSourceCode.url && uiSourceCode.url) {
             this._showFile(uiSourceCode);
             this._editorContainer.removeUISourceCode(currentUISourceCode);
         }
