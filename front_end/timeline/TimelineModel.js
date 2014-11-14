9789925d1a39c8f1a262f9645ebeacb415505bb4
caseq@chromium.org <caseq@chromium.org>
2014-11-14 21:20:37 +0000

    Timeline: bring back FirstPaint marker and remove brown ticks near frame markers
    - implement synthetic FirstPaint markers on front-end (they used to be generated
        on the back-end with old Timeline instrumentation which is an ex-parrot)
    - remove brown marks near the frame markers (regressed at r177959)
    
    BUG=
    
    Review URL: https://codereview.chromium.org/732683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/timeline/TimelineModel.js
+++ front_end/timeline/TimelineModel.js
@@ -755,6 +755,8 @@ WebInspector.TimelineModel.prototype = {
         this._lastRecalculateStylesEvent = null;
         this._currentScriptEvent = null;
         this._eventStack = [];
+        this._layerTreeActivatedAfterLoad = false;
+        this._expectFirstPaint = false;
     },
 
     /**
@@ -765,6 +767,7 @@ WebInspector.TimelineModel.prototype = {
      */
     _processThreadEvents: function(startTime, endTime, mainThread, thread)
     {
+        var recordTypes = WebInspector.TimelineModel.RecordType;
         var events = thread.events();
         var length = events.length;
         var i = events.lowerBound(startTime, function (time, event) { return time - event.startTime });
@@ -787,6 +790,10 @@ WebInspector.TimelineModel.prototype = {
             if (endTime && event.startTime >= endTime)
                 break;
             this._processEvent(event);
+            if (this._expectFirstPaint && event.name === recordTypes.DrawFrame && this._layerTreeActivatedAfterLoad) {
+                threadEvents.push(new WebInspector.TracingModel.Event(event.category, recordTypes.MarkFirstPaint, WebInspector.TracingModel.Phase.Instant, event.startTime, event.thread));
+                this._expectFirstPaint = false;
+            }
             threadEvents.push(event);
             this._inspectedTargetEvents.push(event);
         }
@@ -813,7 +820,6 @@ WebInspector.TimelineModel.prototype = {
     _processEvent: function(event)
     {
         var recordTypes = WebInspector.TimelineModel.RecordType;
-
         var eventStack = this._eventStack;
         while (eventStack.length && eventStack.peekLast().endTime < event.startTime)
             eventStack.pop();
@@ -982,6 +988,17 @@ WebInspector.TimelineModel.prototype = {
             this._paintImageEventByPixelRefId[event.args["LazyPixelRef"]] = paintImageEvent;
             event.backendNodeId = paintImageEvent.backendNodeId;
             event.imageURL = paintImageEvent.imageURL;
+            break;
+
+        case recordTypes.MarkDOMContent:
+            if (!event.args["data"]["isMainFrame"])
+                break;
+            this._expectFirstPaint = true;
+            this._layerTreeActivatedAfterLoad = false;
+            break;
+
+        case recordTypes.ActivateLayerTree:
+            this._layerTreeActivatedAfterLoad = true;
             break;
         }
     },
