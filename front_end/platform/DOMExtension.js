binaryage/dirac

commit 1827a081b8db4db79540708dd72be6393ce17c50

DevTools: do not fetch selection from window - it might belong to web component.
BUG=447149

Review URL: https://codereview.chromium.org/813403006

git-svn-id: svn://svn.chromium.org/blink/trunk@188225 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/platform/DOMExtension.js
@@ -275,6 +275,43 @@ Node.prototype.parentNodeOrShadowHost = function()
 }
 
 /**
+ * @return {?Selection}
+ */
+Node.prototype.getComponentSelection = function()
+{
+    var parent = this.parentNode;
+    while (parent && parent.nodeType !== Node.DOCUMENT_FRAGMENT_NODE)
+        parent = parent.parentNode;
+    return parent instanceof ShadowRoot ? parent.getSelection() : this.window().getSelection();
+}
+
+
+/**
+ * @return {boolean}
+ */
+Node.prototype.isComponentSelectionCollapsed = function()
+{
+    // FIXME: crbug.com/447523, use selection.isCollapsed when it is fixed for shadow dom.
+    var selection = this.getComponentSelection();
+    return selection && selection.rangeCount ? selection.getRangeAt(0).collapsed : false;
+}
+
+/**
+ * @return {!Selection}
+ */
+Node.prototype.getDeepSelection = function()
+{
+    var activeElement = this.ownerDocument.activeElement;
+    var shadowRoot = null;
+    while (activeElement && activeElement.shadowRoot) {
+        shadowRoot = activeElement.shadowRoot;
+        activeElement = shadowRoot.activeElement;
+    }
+
+    return shadowRoot ? shadowRoot.getSelection() : this.window().getSelection();
+}
+
+/**
  * @return {!Window}
  */
 Node.prototype.window = function()
@@ -302,7 +339,7 @@ Element.prototype.removeChildren = function()
  */
 Element.prototype.isInsertionCaretInside = function()
 {
-    var selection = this.window().getSelection();
+    var selection = this.getComponentSelection();
     if (!selection.rangeCount || !selection.isCollapsed)
         return false;
     var selectionRange = selection.getRangeAt(0);
@@ -584,7 +621,7 @@ Text.prototype.select = function(start, end)
     if (start < 0)
         start = end + start;
 
-    var selection = this.ownerDocument.defaultView.getSelection();
+    var selection = this.getComponentSelection();
     selection.removeAllRanges();
     var range = this.ownerDocument.createRange();
     range.setStart(this, start);
@@ -600,7 +637,7 @@ Element.prototype.selectionLeftOffset = function()
 {
     // Calculate selection offset relative to the current element.
 
-    var selection = this.window().getSelection();
+    var selection = this.getComponentSelection();
     if (!selection.containsNode(this, true))
         return null;
 

front_end/components/ObjectPropertiesSection.js
@@ -356,7 +356,7 @@ WebInspector.ObjectPropertyTreeElement.prototype = {
         }
 
         var proxyElement = this._prompt.attachAndStartEditing(elementToEdit, blurListener.bind(this));
-        this.listItemElement.window().getSelection().setBaseAndExtent(elementToEdit, 0, elementToEdit, 1);
+        this.listItemElement.getComponentSelection().setBaseAndExtent(elementToEdit, 0, elementToEdit, 1);
         proxyElement.addEventListener("keydown", this._promptKeyDown.bind(this, context), false);
     },
 

front_end/console/ConsoleView.js
@@ -726,7 +726,7 @@ WebInspector.ConsoleView.prototype = {
      */
     _messagesClicked: function(event)
     {
-        if (!this._prompt.isCaretInsidePrompt() && event.target.window().getSelection().isCollapsed)
+        if (!this._prompt.isCaretInsidePrompt() && event.target.isComponentSelectionCollapsed())
             this._prompt.moveCaretToEndOfPrompt();
         var groupMessage = event.target.enclosingNodeOrSelfWithClass("console-group-title");
         if (!groupMessage)

front_end/elements/ElementsTreeElement.js
@@ -659,7 +659,7 @@ WebInspector.ElementsTreeElement.prototype = {
 
         this._editing = WebInspector.InplaceEditor.startEditing(attribute, config);
 
-        this.listItemElement.window().getSelection().setBaseAndExtent(elementForSelection, 0, elementForSelection, 1);
+        this.listItemElement.getComponentSelection().setBaseAndExtent(elementForSelection, 0, elementForSelection, 1);
 
         return true;
     },
@@ -683,7 +683,7 @@ WebInspector.ElementsTreeElement.prototype = {
             container.textContent = textNode.nodeValue(); // Strip the CSS or JS highlighting if present.
         var config = new WebInspector.InplaceEditor.Config(this._textNodeEditingCommitted.bind(this, textNode), this._editingCancelled.bind(this));
         this._editing = WebInspector.InplaceEditor.startEditing(textNodeElement, config);
-        this.listItemElement.window().getSelection().setBaseAndExtent(textNodeElement, 0, textNodeElement, 1);
+        this.listItemElement.getComponentSelection().setBaseAndExtent(textNodeElement, 0, textNodeElement, 1);
 
         return true;
     },
@@ -741,7 +741,7 @@ WebInspector.ElementsTreeElement.prototype = {
 
         var config = new WebInspector.InplaceEditor.Config(editingComitted.bind(this), editingCancelled.bind(this), tagName);
         this._editing = WebInspector.InplaceEditor.startEditing(tagNameElement, config);
-        this.listItemElement.window().getSelection().setBaseAndExtent(tagNameElement, 0, tagNameElement, 1);
+        this.listItemElement.getComponentSelection().setBaseAndExtent(tagNameElement, 0, tagNameElement, 1);
         return true;
     },
 

front_end/elements/ElementsTreeOutline.js
@@ -290,7 +290,7 @@ WebInspector.ElementsTreeOutline.prototype = {
         this._setClipboardData(null);
 
         // Don't prevent the normal copy if the user has a selection.
-        if (!event.target.window().getSelection().isCollapsed)
+        if (!event.target.isComponentSelectionCollapsed())
             return;
 
         // Do not interfere with text editing.
@@ -788,7 +788,7 @@ WebInspector.ElementsTreeOutline.prototype = {
 
     _ondragstart: function(event)
     {
-        if (!event.target.window().getSelection().isCollapsed)
+        if (!event.target.isComponentSelectionCollapsed())
             return false;
         if (event.target.nodeName === "A")
             return false;

front_end/elements/MetricsSidebarPane.js
@@ -343,7 +343,7 @@ WebInspector.MetricsSidebarPane.prototype = {
         var config = new WebInspector.InplaceEditor.Config(this.editingCommitted.bind(this), this.editingCancelled.bind(this), context);
         WebInspector.InplaceEditor.startEditing(targetElement, config);
 
-        targetElement.window().getSelection().setBaseAndExtent(targetElement, 0, targetElement, 1);
+        targetElement.getComponentSelection().setBaseAndExtent(targetElement, 0, targetElement, 1);
     },
 
     _handleKeyDown: function(context, styleProperty, event)

front_end/elements/StylesSidebarPane.js
@@ -1948,7 +1948,7 @@ WebInspector.StylePropertiesSection.prototype = {
         if (!this.editable)
             return;
 
-        if (!event.target.window().getSelection().isCollapsed)
+        if (!event.target.isComponentSelectionCollapsed())
             return;
 
         if (this._checkWillCancelEditing())
@@ -1976,7 +1976,7 @@ WebInspector.StylePropertiesSection.prototype = {
         var config = new WebInspector.InplaceEditor.Config(this._editingMediaCommitted.bind(this, media), this._editingMediaCancelled.bind(this, element), undefined, this._editingMediaBlurHandler.bind(this));
         WebInspector.InplaceEditor.startEditing(element, config);
 
-        element.window().getSelection().setBaseAndExtent(element, 0, element, 1);
+        element.getComponentSelection().setBaseAndExtent(element, 0, element, 1);
         this._parentPane._isEditingStyle = true;
         var parentMediaElement = element.enclosingNodeOrSelfWithClass("media");
         parentMediaElement.classList.add("editing-media");
@@ -2003,7 +2003,7 @@ WebInspector.StylePropertiesSection.prototype = {
         // Mark the selectors in group if necessary.
         // This is overridden by BlankStylePropertiesSection.
         this._markSelectorMatches();
-        element.window().getSelection().collapse(element, 0);
+        element.getComponentSelection().collapse(element, 0);
     },
 
     /**
@@ -2108,7 +2108,7 @@ WebInspector.StylePropertiesSection.prototype = {
         var config = new WebInspector.InplaceEditor.Config(this.editingSelectorCommitted.bind(this), this.editingSelectorCancelled.bind(this), undefined, this._editingSelectorBlurHandler.bind(this));
         WebInspector.InplaceEditor.startEditing(this._selectorElement, config);
 
-        element.window().getSelection().setBaseAndExtent(element, 0, element, 1);
+        element.getComponentSelection().setBaseAndExtent(element, 0, element, 1);
         this._parentPane._isEditingStyle = true;
         this._parentPane._startEditingSelector(this);
     },
@@ -2119,7 +2119,7 @@ WebInspector.StylePropertiesSection.prototype = {
     setSelectorText: function(text)
     {
         this._selectorElement.textContent = text;
-        this._selectorElement.window().getSelection().setBaseAndExtent(this._selectorElement, 0, this._selectorElement, 1);
+        this._selectorElement.getComponentSelection().setBaseAndExtent(this._selectorElement, 0, this._selectorElement, 1);
     },
 
     /**
@@ -3308,7 +3308,7 @@ WebInspector.StylePropertyTreeElement.prototype = {
      */
     _mouseClick: function(event)
     {
-        if (!event.target.window().getSelection().isCollapsed)
+        if (!event.target.isComponentSelectionCollapsed())
             return;
 
         event.consume(true);
@@ -3496,7 +3496,7 @@ WebInspector.StylePropertyTreeElement.prototype = {
         if (isEditingName)
             proxyElement.addEventListener("paste", pasteHandler.bind(this, context), false);
 
-        selectElement.window().getSelection().setBaseAndExtent(selectElement, 0, selectElement, 1);
+        selectElement.getComponentSelection().setBaseAndExtent(selectElement, 0, selectElement, 1);
     },
 
     /**
@@ -3517,7 +3517,7 @@ WebInspector.StylePropertyTreeElement.prototype = {
             result = "cancel";
         else if (!context.isEditingName && this._newProperty && event.keyCode === WebInspector.KeyboardShortcut.Keys.Backspace.code) {
             // For a new property, when Backspace is pressed at the beginning of new property value, move back to the property name.
-            var selection = event.target.window().getSelection();
+            var selection = event.target.getComponentSelection();
             if (selection.isCollapsed && !selection.focusOffset) {
                 event.preventDefault();
                 result = "backward";

front_end/resources/DatabaseQueryView.js
@@ -63,7 +63,7 @@ WebInspector.DatabaseQueryView.prototype = {
 
     _messagesClicked: function()
     {
-        if (!this._prompt.isCaretInsidePrompt() && this.element.window().getSelection().isCollapsed)
+        if (!this._prompt.isCaretInsidePrompt() && this.element.isComponentSelectionCollapsed())
             this._prompt.moveCaretToEndOfPrompt();
     },
 
@@ -115,7 +115,7 @@ WebInspector.DatabaseQueryView.prototype = {
         function moveBackIfOutside()
         {
             delete this._selectionTimeout;
-            if (!this._prompt.isCaretInsidePrompt() && this.element.window().getSelection().isCollapsed)
+            if (!this._prompt.isCaretInsidePrompt() && this.element.isComponentSelectionCollapsed())
                 this._prompt.moveCaretToEndOfPrompt();
             this._prompt.autoCompleteSoon();
         }

commit 1fa2896f6c62e5351acacb875798c56e2a5ae3a8

DevTools: [Elements] fix DOM node drag-and-drop functionality
This patch fixes DOM node drag-and-drop functionality, regressed
after r188225.

TBR=pfeldman

Review URL: https://codereview.chromium.org/853193002

git-svn-id: svn://svn.chromium.org/blink/trunk@188534 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/platform/DOMExtension.js
@@ -293,7 +293,7 @@ Node.prototype.isComponentSelectionCollapsed = function()
 {
     // FIXME: crbug.com/447523, use selection.isCollapsed when it is fixed for shadow dom.
     var selection = this.getComponentSelection();
-    return selection && selection.rangeCount ? selection.getRangeAt(0).collapsed : false;
+    return selection && selection.rangeCount ? selection.getRangeAt(0).collapsed : true;
 }
 
 /**