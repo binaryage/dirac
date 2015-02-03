binaryage/dirac

commit ec69156ad18c21757b0823c9c2657406975e081b

[DevTools] Edit media rule in SSP.
BUG=437214

Review URL: https://codereview.chromium.org/815083005

git-svn-id: svn://svn.chromium.org/blink/trunk@187747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/elements/StylesSidebarPane.js
@@ -296,6 +296,25 @@ WebInspector.StylesSidebarPane.prototype = {
     },
 
     /**
+     * @param {!WebInspector.CSSMedia} oldMedia
+     * @param {!WebInspector.CSSMedia}  newMedia
+     */
+    _styleSheetMediaEdited: function(oldMedia, newMedia)
+    {
+        if (!oldMedia.parentStyleSheetId)
+            return;
+        for (var pseudoId in this.sections) {
+            var styleRuleSections = this.sections[pseudoId];
+            for (var i = 0; i < styleRuleSections.length; ++i) {
+                var section = styleRuleSections[i];
+                if (section.computedStyle)
+                    continue;
+                section._styleSheetMediaEdited(oldMedia, newMedia);
+            }
+        }
+    },
+
+    /**
      * @param {!Event} event
      */
     _contextMenuEventFired: function(event)
@@ -1459,7 +1478,7 @@ WebInspector.StylePropertiesSection = function(parentPane, styleRule)
 
     this._selectorRefElement = createElement("div");
     this._selectorRefElement.className = "subtitle";
-    this._mediaListElement = this.titleElement.createChild("div", "media-list");
+    this._mediaListElement = this.titleElement.createChild("div", "media-list media-matches");
     this._updateMediaList();
     this._updateRuleOrigin();
     selectorContainer.insertBefore(this._selectorRefElement, selectorContainer.firstChild);
@@ -1582,6 +1601,19 @@ WebInspector.StylePropertiesSection.prototype = {
     },
 
     /**
+     * @param {!WebInspector.CSSMedia} oldMedia
+     * @param {!WebInspector.CSSMedia} newMedia
+     */
+    _styleSheetMediaEdited: function(oldMedia, newMedia)
+    {
+        var rule = this.rule();
+        if (!rule || !rule.styleSheetId)
+            return;
+        rule.mediaEdited(oldMedia, newMedia);
+        this._updateMediaList();
+    },
+
+    /**
      * @param {?Array.<!WebInspector.CSSMedia>} mediaRules
      */
     _createMediaList: function(mediaRules)
@@ -1591,20 +1623,6 @@ WebInspector.StylePropertiesSection.prototype = {
         for (var i = mediaRules.length - 1; i >= 0; --i) {
             var media = mediaRules[i];
             var mediaDataElement = this._mediaListElement.createChild("div", "media");
-            var mediaText;
-            switch (media.source) {
-            case WebInspector.CSSMedia.Source.LINKED_SHEET:
-            case WebInspector.CSSMedia.Source.INLINE_SHEET:
-                mediaText = "media=\"" + media.text + "\"";
-                break;
-            case WebInspector.CSSMedia.Source.MEDIA_RULE:
-                mediaText = "@media " + media.text;
-                break;
-            case WebInspector.CSSMedia.Source.IMPORT_RULE:
-                mediaText = "@import " + media.text;
-                break;
-            }
-
             if (media.sourceURL) {
                 var refElement = mediaDataElement.createChild("div", "subtitle");
                 var anchor = this._parentPane._linkifier.linkifyMedia(media);
@@ -1612,9 +1630,29 @@ WebInspector.StylePropertiesSection.prototype = {
                 refElement.appendChild(anchor);
             }
 
-            var mediaTextElement = mediaDataElement.createChild("span");
-            mediaTextElement.textContent = mediaText;
+            var mediaContainerElement = mediaDataElement.createChild("span");
+            var mediaTextElement = mediaContainerElement.createChild("span", "media-text");
             mediaTextElement.title = media.text;
+            switch (media.source) {
+            case WebInspector.CSSMedia.Source.LINKED_SHEET:
+            case WebInspector.CSSMedia.Source.INLINE_SHEET:
+                mediaTextElement.textContent = "media=\"" + media.text + "\"";
+                break;
+            case WebInspector.CSSMedia.Source.MEDIA_RULE:
+                var decoration = mediaContainerElement.createChild("span");
+                mediaContainerElement.insertBefore(decoration, mediaTextElement);
+                decoration.textContent = "@media ";
+                decoration.title = media.text;
+                mediaTextElement.textContent = media.text;
+                if (media.parentStyleSheetId) {
+                    mediaDataElement.classList.add("editable-media");
+                    mediaTextElement.addEventListener("click", this._handleMediaRuleClick.bind(this, media, mediaTextElement), false);
+                }
+                break;
+            case WebInspector.CSSMedia.Source.IMPORT_RULE:
+                mediaTextElement.textContent = "@import " + media.text;
+                break;
+            }
         }
     },
 
@@ -1813,16 +1851,18 @@ WebInspector.StylePropertiesSection.prototype = {
         if (!rule)
             return;
 
-        var matchingSelectors = rule.matchingSelectors;
+        this._mediaListElement.classList.toggle("media-matches", this.styleRule.mediaMatches());
+
         // .selector is rendered as non-affecting selector by default.
-        if (!this.styleRule.hasMatchingSelectors() || matchingSelectors)
+        if (!this.styleRule.hasMatchingSelectors()) {
             this._selectorElement.className = "selector";
-        if (!matchingSelectors)
             return;
+        }
 
         var selectors = rule.selectors;
         var fragment = createDocumentFragment();
         var currentMatch = 0;
+        var matchingSelectors = rule.matchingSelectors;
         for (var i = 0; i < selectors.length ; ++i) {
             if (i)
                 fragment.createTextChild(", ");
@@ -1920,6 +1960,102 @@ WebInspector.StylePropertiesSection.prototype = {
         this.addNewBlankProperty().startEditing();
         event.consume(true);
     },
+
+    /**
+     * @param {!WebInspector.CSSMedia} media
+     * @param {!Element} element
+     * @param {!Event} event
+     */
+    _handleMediaRuleClick: function(media, element, event)
+    {
+        if (WebInspector.isBeingEdited(element))
+            return;
+
+        var config = new WebInspector.InplaceEditor.Config(this._editingMediaCommitted.bind(this, media), this._editingMediaCancelled.bind(this, element), undefined, this._editingMediaBlurHandler.bind(this));
+        WebInspector.InplaceEditor.startEditing(element, config);
+
+        element.window().getSelection().setBaseAndExtent(element, 0, element, 1);
+        this._parentPane._isEditingStyle = true;
+        var parentMediaElement = element.enclosingNodeOrSelfWithClass("media");
+        parentMediaElement.classList.add("editing-media");
+
+        event.consume(true);
+    },
+
+    /**
+     * @param {!Element} element
+     */
+    _editingMediaFinished: function(element)
+    {
+        delete this._parentPane._isEditingStyle;
+        var parentMediaElement = element.enclosingNodeOrSelfWithClass("media");
+        parentMediaElement.classList.remove("editing-media");
+    },
+
+    /**
+     * @param {!Element} element
+     */
+    _editingMediaCancelled: function(element)
+    {
+        this._editingMediaFinished(element);
+        // Mark the selectors in group if necessary.
+        // This is overridden by BlankStylePropertiesSection.
+        this._markSelectorMatches();
+        element.window().getSelection().collapse(element, 0);
+    },
+
+    /**
+     * @param {!Element} editor
+     * @param {!Event} blurEvent
+     * @return {boolean}
+     */
+    _editingMediaBlurHandler: function(editor, blurEvent)
+    {
+        return true;
+    },
+
+    /**
+     * @param {!WebInspector.CSSMedia} media
+     * @param {!Element} element
+     * @param {string} newContent
+     * @param {string} oldContent
+     * @param {(!WebInspector.StylePropertyTreeElement.Context|undefined)} context
+     * @param {string} moveDirection
+     */
+    _editingMediaCommitted: function(media, element, newContent, oldContent, context, moveDirection)
+    {
+        delete this._parentPane._isEditingStyle;
+        this._editingMediaFinished(element);
+
+        if (newContent)
+            newContent = newContent.trim();
+
+        /**
+         * @param {!WebInspector.CSSMedia} newMedia
+         * @this {WebInspector.StylePropertiesSection}
+         */
+        function successCallback(newMedia)
+        {
+            this._parentPane._styleSheetMediaEdited(media, newMedia);
+            this._parentPane._refreshUpdate(this);
+            finishOperation.call(this);
+        }
+
+        /**
+         * @this {WebInspector.StylePropertiesSection}
+         */
+        function finishOperation()
+        {
+            delete this._parentPane._userOperation;
+            this._editingMediaTextCommittedForTest();
+        }
+
+        // This gets deleted in finishOperation(), which is called both on success and failure.
+        this._parentPane._userOperation = true;
+        this._parentPane._target.cssModel.setMediaText(media, newContent, successCallback.bind(this), finishOperation.bind(this));
+    },
+
+    _editingMediaTextCommittedForTest: function() { },
 
     /**
      * @param {!Event} event

front_end/elements/StylesSectionModel.js
@@ -35,7 +35,20 @@ WebInspector.StylesSectionModel.prototype = {
      */
     hasMatchingSelectors: function()
     {
-        return this.rule() ? this.rule().matchingSelectors.length > 0 : true;
+        return this.rule() ? this.rule().matchingSelectors.length > 0 && this.mediaMatches() : true;
+    },
+
+    /**
+     * @return {boolean}
+     */
+    mediaMatches: function()
+    {
+        var media = this.media();
+        for (var i = 0; media && i < media.length; ++i) {
+            if (!media[i].active())
+                return false;
+        }
+        return true;
     },
 
     /**

front_end/elements/elementsPanel.css
@@ -219,9 +219,16 @@
     white-space: normal;
 }
 
-.styles-section .header .title .media,
-.styles-section .header .title .media .subtitle {
-    color: rgb(128, 128, 128);
+.styles-section .header .title .media-list {
+    color: #888;
+}
+
+.styles-section .header .title .media-list.media-matches .media.editable-media {
+    color: #222;
+}
+
+.styles-section .header .title .media:not(.editing-media),
+.styles-section .header .title .media:not(.editing-media) .subtitle {
     overflow: hidden;
 }
 

front_end/sdk/CSSStyleModel.js
@@ -316,6 +316,37 @@ WebInspector.CSSStyleModel.prototype = {
     },
 
     /**
+     * @param {!WebInspector.CSSMedia} media
+     * @param {string} newMediaText
+     * @param {function(!WebInspector.CSSMedia)} successCallback
+     * @param {function()} failureCallback
+     */
+    setMediaText: function(media, newMediaText, successCallback, failureCallback)
+    {
+        /**
+         * @param {function(!WebInspector.CSSMedia)} successCallback
+         * @param {function()} failureCallback
+         * @param {?Protocol.Error} error
+         * @param {!CSSAgent.CSSMedia} mediaPayload
+         * @this {WebInspector.CSSStyleModel}
+         */
+        function callback(successCallback, failureCallback, error, mediaPayload)
+        {
+            this._pendingCommandsMajorState.pop();
+            if (error) {
+                failureCallback();
+                return;
+            }
+            this._domModel.markUndoableState();
+            successCallback(WebInspector.CSSMedia.parsePayload(this, mediaPayload));
+        }
+
+        console.assert(!!media.parentStyleSheetId);
+        this._pendingCommandsMajorState.push(true);
+        this._agent.setMediaText(media.parentStyleSheetId, media.range, newMediaText, callback.bind(this, successCallback, failureCallback));
+    },
+
+    /**
      * @param {!CSSAgent.CSSRule} rulePayload
      * @param {!DOMAgent.NodeId} nodeId
      * @param {function(!WebInspector.CSSRule)} successCallback
@@ -989,6 +1020,18 @@ WebInspector.CSSRule.prototype = {
      */
     sourceStyleSheetEdited: function(styleSheetId, oldRange, newRange)
     {
+        this._sourceStyleSheetEditedWithMedia(styleSheetId, oldRange, newRange, null, null);
+    },
+
+    /**
+     * @param {string} styleSheetId
+     * @param {!WebInspector.TextRange} oldRange
+     * @param {!WebInspector.TextRange} newRange
+     * @param {?WebInspector.CSSMedia} oldMedia
+     * @param {?WebInspector.CSSMedia} newMedia
+     */
+    _sourceStyleSheetEditedWithMedia: function(styleSheetId, oldRange, newRange, oldMedia, newMedia)
+    {
         if (this.styleSheetId === styleSheetId) {
             if (this.selectorRange)
                 this.selectorRange = this.selectorRange.rebaseAfterTextEdit(oldRange, newRange);
@@ -996,10 +1039,24 @@ WebInspector.CSSRule.prototype = {
                 this.selectors[i].sourceStyleRuleEdited(oldRange, newRange);
         }
         if (this.media) {
-            for (var i = 0; i < this.media.length; ++i)
-                this.media[i].sourceStyleSheetEdited(styleSheetId, oldRange, newRange);
+            for (var i = 0; i < this.media.length; ++i) {
+                if (oldMedia && newMedia && oldMedia.equal(this.media[i])) {
+                    this.media[i] = newMedia;
+                } else {
+                    this.media[i].sourceStyleSheetEdited(styleSheetId, oldRange, newRange);
+                }
+            }
         }
         this.style.sourceStyleSheetEdited(styleSheetId, oldRange, newRange);
+    },
+
+    /**
+     * @param {!WebInspector.CSSMedia} oldMedia
+     * @param {!WebInspector.CSSMedia} newMedia
+     */
+    mediaEdited: function(oldMedia, newMedia)
+    {
+        this._sourceStyleSheetEditedWithMedia(/** @type {string} */ (oldMedia.parentStyleSheetId), oldMedia.range, newMedia.range, oldMedia, newMedia);
     },
 
     _setFrameId: function()
@@ -1376,7 +1433,7 @@ WebInspector.CSSMediaQueryExpression.prototype = {
  */
 WebInspector.CSSMedia = function(cssModel, payload)
 {
-    this._cssModel = cssModel
+    this._cssModel = cssModel;
     this.text = payload.text;
     this.source = payload.source;
     this.sourceURL = payload.sourceURL || "";
@@ -1432,6 +1489,31 @@ WebInspector.CSSMedia.prototype = {
             return;
         if (this.range)
             this.range = this.range.rebaseAfterTextEdit(oldRange, newRange);
+    },
+
+    /**
+     * @param {!WebInspector.CSSMedia} other
+     * @return {boolean}
+     */
+    equal: function(other)
+    {
+        if (!this.parentStyleSheetId || !this.range || !other.range)
+            return false;
+        return  this.parentStyleSheetId === other.parentStyleSheetId && this.range.equal(other.range);
+    },
+
+    /**
+     * @return {boolean}
+     */
+    active: function()
+    {
+        if (!this.mediaList)
+            return true;
+        for (var i = 0; i < this.mediaList.length; ++i) {
+            if (this.mediaList[i].active())
+                return true;
+        }
+        return false;
     },
 
     /**

commit 15fbb629072e6374bcbbdcb36110b2e7b9790aa4

DevTools: [SSP] fix matched selector highlight for style rules
Selector highlight was regressed in r187747.

BUG=453706
R=apavlov

Review URL: https://codereview.chromium.org/898673004

git-svn-id: svn://svn.chromium.org/blink/trunk@189425 bbb929c8-8fbe-4397-9dbb-9b2b20218538

front_end/elements/StylesSidebarPane.js
@@ -1445,7 +1445,7 @@ WebInspector.StylePropertiesSection = function(parentPane, styleRule)
     this.propertiesTreeOutline.section = this;
 
     var selectorContainer = createElement("div");
-    this._selectorElement = createElement("span");
+    this._selectorElement = createElementWithClass("span", "selector");
     this._selectorElement.textContent = styleRule.selectorText();
     selectorContainer.appendChild(this._selectorElement);
 
@@ -1839,11 +1839,8 @@ WebInspector.StylePropertiesSection.prototype = {
 
         this._mediaListElement.classList.toggle("media-matches", this.styleRule.mediaMatches());
 
-        // .selector is rendered as non-affecting selector by default.
-        if (!this.styleRule.hasMatchingSelectors()) {
-            this._selectorElement.className = "selector";
+        if (!this.styleRule.hasMatchingSelectors())
             return;
-        }
 
         var selectors = rule.selectors;
         var fragment = createDocumentFragment();