ddd7647a90ac72d903d733531edad16ddda9cf7c
lushnikov@chromium.org <lushnikov@chromium.org>
2015-06-22 18:21:18 +0000

    DevTools: [Elements] F2 should commit changes in edit-as-html
    BUG=493091
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/1200833006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197588 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/elements/ElementsTreeElement.js
+++ front_end/elements/ElementsTreeElement.js
@@ -666,24 +666,16 @@ WebInspector.ElementsTreeElement.prototype = {
 
         var config = new WebInspector.InplaceEditor.Config(this._attributeEditingCommitted.bind(this), this._editingCancelled.bind(this), attributeName);
 
-        function handleKeyDownEvents(event)
+        /**
+         * @param {!Event} event
+         * @return {string}
+         */
+        function postKeyDownFinishHandler(event)
         {
-            var isMetaOrCtrl = WebInspector.isMac() ?
-                event.metaKey && !event.shiftKey && !event.ctrlKey && !event.altKey :
-                event.ctrlKey && !event.shiftKey && !event.metaKey && !event.altKey;
-            if (isEnterKey(event) && (event.isMetaOrCtrlForTest || !config.multiline || isMetaOrCtrl))
-                return "commit";
-            else if (event.keyCode === WebInspector.KeyboardShortcut.Keys.Esc.code || event.keyIdentifier === "U+001B")
-                return "cancel";
-            else if (event.keyIdentifier === "U+0009") // Tab key
-                return "move-" + (event.shiftKey ? "backward" : "forward");
-            else {
-                WebInspector.handleElementValueModifications(event, attribute);
-                return "";
-            }
+            WebInspector.handleElementValueModifications(event, attribute);
+            return "";
         }
-
-        config.customFinishHandler = handleKeyDownEvents;
+        config.setPostKeydownFinishHandler(postKeyDownFinishHandler);
 
         this._editing = WebInspector.InplaceEditor.startEditing(attribute, config);
 
@@ -848,8 +840,20 @@ WebInspector.ElementsTreeElement.prototype = {
             this.treeOutline.focus();
         }
 
+        /**
+         * @param {!Event} event
+         * @return {string}
+         */
+        function postKeyDownFinishHandler(event)
+        {
+            if (event.keyCode === WebInspector.KeyboardShortcut.Keys.F2.code)
+                return "commit";
+            return "";
+        }
+
         var config = new WebInspector.InplaceEditor.Config(commit.bind(this), dispose.bind(this));
         config.setMultilineOptions(initialValue, { name: "xml", htmlMode: true }, "web-inspector-html", WebInspector.moduleSetting("domWordWrap").get(), true);
+        config.setPostKeydownFinishHandler(postKeyDownFinishHandler);
         WebInspector.InplaceEditor.startMultilineEditing(this._htmlEditElement, config).then(markAsBeingEdited.bind(this));
 
         /**
