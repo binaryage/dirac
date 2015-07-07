59ec0b370e8edd0f5eb4ce98110d7288fa96acc9
lushnikov@chromium.org <lushnikov@chromium.org>
2015-07-07 19:26:06 +0000

    DevTools: show a tip about "file:" in the search-across-files placeholder
    As nobody knows about a "file:" trick in "Search Across Files" to narrow
    search scope, mention it in the search box placeholder text.
    
    BUG=488112
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/1211203007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- front_end/sources/AdvancedSearchView.js
+++ front_end/sources/AdvancedSearchView.js
@@ -24,7 +24,7 @@ WebInspector.AdvancedSearchView = function()
 
     this._search = WebInspector.HistoryInput.create();
     this._searchPanelElement.appendChild(this._search);
-    this._search.placeholder = WebInspector.UIString("Search sources");
+    this._search.placeholder = WebInspector.UIString("Use 'file:' to define search scope");
     this._search.setAttribute("type", "text");
     this._search.classList.add("search-config-search");
     this._search.setAttribute("results", "0");
