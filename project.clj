d028f345f1014a7816b10eb2900138679bbb8c16
Antonin Hildebrand <antonin@hildebrand.cz>
2016-01-12 23:13:19 +0100

    set version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/dirac "0.1.0-SNAPSHOT"
+(defproject binaryage/dirac "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.2.374"]
