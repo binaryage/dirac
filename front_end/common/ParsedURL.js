5439212472c6c55a7930ad317243dbbcb94890f7
Antonin Hildebrand <antonin@hildebrand.cz>
2016-01-14 00:25:44 +0100

    squash 'resources/unpacked/devtools' changes from 1fb4a98..ee84143
    ee84143 DevTools: restore statusbar overflow ellipsis.
    f13c7cd Devtools: Remove unused events in StylesSidebarPane
    4feb920 DevTools: include history items in the console suggest box.
    9844585 DevTools: use timeout to release ignoreExecutionLineEvents flag
    5dc7ddd [Media Router] Replace old MR extension ID with "new" extension ID.
    b5329c0 Revert of [DevTools] Device Mode polish. (patchset #1 id:1 of https://codereview.chromium.org/1564363005/ )
    9a15d38 Revert of [DevTools] More Device Mode polish. (patchset #1 id:1 of https://codereview.chromium.org/1575203003/ )
    6c41282 Devtools: Fix PauseInSharedWorkerInitialization test
    e519341 [DevTools] More Device Mode polish.
    5b231ae Devtools CSS: Show custom variables in styles sidebar pane
    714e010 DevTools: [profiler] high resolution sampling on by default
    14be7d0 Add missing parenthesis to a DevTools protocol comment.
    68d93e5 [DevTools] Removed unused code in SourceMap.js
    763257c [DevTools] Fixed the rendering of the timeline flamechart, which was disappearing when scrolling the frames to the right with the arrow keys.
    afcece0 Devtools: Split InspectorBackend into two files
    0f411ee [DevTools] Device Mode polish.
    e2268c1 DevTools: decode urls only on the presentation level.
    2ccd467 DevTools: merge uisourcecode's url-alike members.
    af12426 Revert of DevTools: fix toolbar <select> background in Dark theme (patchset #1 id:1 of https://codereview.chromium.org/1567203004/ )
    d1290f2 DevTools: Order the device list deliberately.
    399bbc9 DevTools: fix toolbar <select> background in Dark theme
    c335daa DevTools: fix timeline banners to scroll on overflow
    c037c1d DevTools: fix selection adjustment in DataGrid
    5ef30d6 DevTools: preserve hover state rendering upon tabbed pane tab selection.
    787e525 [DevTools] Shows Manifest file in Sources tab
    d205ce8 Devtools: Add source offsets to keyframe animations
    
    git-subtree-dir: resources/unpacked/devtools
    git-subtree-split: ee841436b11aeba5b373cd4247cd7bd44d473c25

--- front_end/common/ParsedURL.js
+++ front_end/common/ParsedURL.js
@@ -88,25 +88,11 @@ WebInspector.ParsedURL = function(url)
 
 /**
  * @param {string} url
- * @return {string}
- */
-WebInspector.ParsedURL._decodeIfPossible = function(url)
-{
-    var decodedURL = url;
-    try {
-        decodedURL = decodeURI(url);
-    } catch (e) { }
-    return decodedURL;
-}
-
-/**
- * @param {string} url
  * @return {!Array.<string>}
  */
 WebInspector.ParsedURL.splitURLIntoPathComponents = function(url)
 {
-    var decodedURL = WebInspector.ParsedURL._decodeIfPossible(url);
-    var parsedURL = new WebInspector.ParsedURL(decodedURL);
+    var parsedURL = new WebInspector.ParsedURL(url);
     var origin;
     var folderPath;
     var name;
@@ -132,6 +118,22 @@ WebInspector.ParsedURL.splitURLIntoPathComponents = function(url)
     }
     result.push(name);
     return result;
+}
+
+/**
+ * @param {string} url
+ * @return {string}
+ */
+WebInspector.ParsedURL.extractOrigin = function(url)
+{
+    var parsedURL = new WebInspector.ParsedURL(url);
+    if (!parsedURL.isValid)
+        return "";
+
+    var origin = parsedURL.scheme + "://" + parsedURL.host;
+    if (parsedURL.port)
+        origin += ":" + parsedURL.port;
+    return origin;
 }
 
 /**
