4ed4f90ecc502d0d16329749bf3608e3a9bf994c
Antonin Hildebrand <antonin@hildebrand.cz>
2016-01-17 14:53:44 +0100

    improvements to agent-launcher.sh

--- scripts/agent-launcher.sh
+++ scripts/agent-launcher.sh
@@ -1,17 +1,39 @@
 #!/usr/bin/env bash
 
-set -ex
+# this script tries to launch dirac agent from command line
+#
+# usage ./agent-launcher.sh [args]
+#
+# it uses maven to download dirac library with all dependencies from clojars.org into local maven repo
+# then it tries to launch it from command-line using current java environment
 
-DIRAC_VERSION=0.1.0
+set -e
+
+DIRAC_VERSION=LATEST # e.g. 0.1.0 or LATEST
 DIRAC_GROUP=binaryage
 DIRAC_ARTIFACT=dirac
 
-mvn dependency:get -DgroupId="$DIRAC_GROUP" -DartifactId="$DIRAC_ARTIFACT" -Dversion="$DIRAC_VERSION"
+NO_DEPENDENCY_GET=$NO_DEPENDENCY_GET
+
 LOCAL_REPO=`mvn help:evaluate -Dexpression=settings.localRepository | grep -v '[INFO]'`
 
-LOCAL_DIRAC_PATH="$LOCAL_REPO/$DIRAC_GROUP/$DIRAC_ARTIFACT/$DIRAC_VERSION"
-LOCAL_DIRAC_POM_PATH="$LOCAL_DIRAC_PATH/$DIRAC_ARTIFACT-$DIRAC_VERSION.pom"
-LOCAL_DIRAC_JAR_PATH="$LOCAL_DIRAC_PATH/$DIRAC_ARTIFACT-$DIRAC_VERSION.jar"
+pushd () {
+  command pushd "$@" > /dev/null
+}
+
+popd () {
+  command popd "$@" > /dev/null
+}
+
+# dependency:get downloads library with all dependencies
+if [ -z "$NO_DEPENDENCY_GET" ] ; then
+  mvn -q dependency:get -DgroupId="$DIRAC_GROUP" -DartifactId="$DIRAC_ARTIFACT" -Dversion="$DIRAC_VERSION"
+fi
+
+RESOLVED_VERSION=`mvn help:evaluate -Dartifact=$DIRAC_GROUP:$DIRAC_ARTIFACT -Dexpression=project.version | grep -v '[INFO]'`
+LOCAL_DIRAC_PATH="$LOCAL_REPO/$DIRAC_GROUP/$DIRAC_ARTIFACT/$RESOLVED_VERSION"
+LOCAL_DIRAC_POM_PATH="$LOCAL_DIRAC_PATH/$DIRAC_ARTIFACT-$RESOLVED_VERSION.pom"
+LOCAL_DIRAC_JAR_PATH="$LOCAL_DIRAC_PATH/$DIRAC_ARTIFACT-$RESOLVED_VERSION.jar"
 
 if [ ! -r "$LOCAL_DIRAC_POM_PATH" ] ; then
   echo "unexpected error: clojars' pom file does not exist '$LOCAL_DIRAC_POM_PATH'"
@@ -28,9 +50,9 @@ fi
 function cleanup {
   if [[ "$WORK_DIR" == /tmp/dirac-agent-launcher* ]] ; then  # this is just a paranoid test if something went terribly wrong and mktemp returned "/" or something non-tmp-ish
     rm -rf "$WORK_DIR"
-    echo "Deleted temp working directory $WORK_DIR"
+    # echo "Deleted temp working directory $WORK_DIR"
   else
-    echo "Delete temp dir manually '$WORK_DIR'"
+    echo "Unexpected temporary directory. Delete it manually '$WORK_DIR'."
   fi
 }
 trap cleanup EXIT
@@ -41,5 +63,7 @@ cp "$LOCAL_DIRAC_POM_PATH" pom.xml
 CLASS_PATH=`mvn dependency:build-classpath | grep -v '[INFO]'`
 popd
 
+FULL_CLASS_PATH="$CLASS_PATH:$LOCAL_DIRAC_JAR_PATH"
+
 # launch the agent with full classpath
-java -classpath "$CLASS_PATH:$LOCAL_DIRAC_JAR_PATH" clojure.main --main dirac.agent-cli "$@"
+java -classpath "$FULL_CLASS_PATH" clojure.main --main dirac.agent-cli "$@"
