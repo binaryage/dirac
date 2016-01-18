cf3497fbb98eac44a1be98e7f82dce8be8077ff7
Antonin Hildebrand <antonin@hildebrand.cz>
2016-01-18 17:00:18 +0100

    tests: move nrepl server into a fixture

--- test/support/dirac/test/logging.clj
+++ test/support/dirac/test/logging.clj
@@ -5,11 +5,14 @@
 (def base-options
   {:pattern (str (utils/wrap-with-ansi-color utils/ANSI_MAGENTA "# %m") "%n")})
 
+(def root-options
+  {:pattern (str (utils/wrap-with-ansi-color utils/ANSI_CYAN "# %m") "%n")})
+
 ; -- our default setup ------------------------------------------------------------------------------------------------------
 
 (defn setup-logging! [& [config]]
   (let [options (utils/config->logging-options config)]
     (config/set-loggers!
-      "com.ning.http.client.providers.netty.request.NettyConnectListener" (utils/make-logging-options base-options options)
+      :root (utils/make-logging-options root-options options)
       "dirac.agent-test" (utils/make-logging-options base-options options)
       "dirac.test.mock-tunnel-client" (utils/make-logging-options base-options options))))
