f58e24e541526d3fd2985fbef00b74356a55e0ae
Antonin Hildebrand <antonin@hildebrand.cz>
2016-01-20 00:27:47 +0100

    rename mock-tunnel-client to mock-nrepl-tunnel-client

--- test/support/dirac/test/mock_nrepl_tunnel_client.clj
+++ test/support/dirac/test/mock_nrepl_tunnel_client.clj
@@ -1,4 +1,4 @@
-(ns dirac.test.mock-tunnel-client
+(ns dirac.test.mock-nrepl-tunnel-client
   (:require [clojure.core.async :refer [chan <!! <! >!! put! alts!! timeout close! go go-loop]]
             [http.async.client :as http]
             [clojure.tools.logging :as log]))
