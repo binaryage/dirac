fd8fb97623011232d53e070663a1f6d9ee09e3ae
Antonin Hildebrand <antonin@hildebrand.cz>
2016-01-23 14:35:50 +0100

    prevent "The given range isn't in document." errors

--- resources/unpacked/devtools/front_end/ui/UIUtils.js
+++ resources/unpacked/devtools/front_end/ui/UIUtils.js
@@ -768,6 +768,11 @@ WebInspector._isTextEditingElement = function(element)
  */
 WebInspector.setCurrentFocusElement = function(x)
 {
+    // this is an edge case of restoring focus to already removed element from document
+    if (!document.body.contains(x)) {
+        x = null;
+    }
+
     if (WebInspector._glassPane && x && !WebInspector._glassPane.element.isAncestor(x))
         return;
     if (WebInspector._currentFocusElement !== x)
