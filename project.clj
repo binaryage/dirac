607c1655e7d351d500af80571bf2833866c596bf
Antonin Hildebrand <antonin@hildebrand.cz>
2016-01-27 23:56:52 +0100

    bump version to 0.1.2
    Chrome extension manifest file must not contain SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/dirac "0.1.2-SNAPSHOT"
+(defproject binaryage/dirac "0.1.2"
   :description "Dirac DevTools - a Chrome DevTools fork for ClojureScript developers."
   :url "https://github.com/binaryage/dirac"
   :license {:name         "MIT License"
--- src/project/dirac/version.clj
+++ src/project/dirac/version.clj
@@ -4,4 +4,4 @@
 ; but with leiningen design it is non-trivial
 ; the problem is when dirac is used as a checkouts dependency
 
-(def version "0.1.2-SNAPSHOT")
+(def version "0.1.2")
