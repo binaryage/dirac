24a209cc061017cf319dbcd0064c2b365f243401
Antonin Hildebrand <antonin@hildebrand.cz>
2016-02-09 01:35:26 +0100

    fix confused console filter buttons (close #4)

--- resources/unpacked/devtools/front_end/ui/FilterBar.js
+++ resources/unpacked/devtools/front_end/ui/FilterBar.js
@@ -545,7 +545,7 @@ WebInspector.NamedBitSetFilterUI.prototype = {
             this._allowedTypes[WebInspector.NamedBitSetFilterUI.ALL_TYPES] = true;
         }
         for (var typeName in this._typeFilterElements)
-            this._typeFilterElements[typeName].classList.toggle("selected", this._allowedTypes[typeName]);
+            this._typeFilterElements[typeName].classList.toggle("selected", this._allowedTypes[typeName]?true:false);
         this.dispatchEventToListeners(WebInspector.FilterUI.Events.FilterChanged, null);
     },
 
