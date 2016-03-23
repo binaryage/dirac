eeab3bc4c9f9fc6744383c3d6d03961a9af6c049
Antonin Hildebrand <antonin@hildebrand.cz>
2016-03-23 18:58:18 +0100

    do not enable dirac feature in Marion's devtools

--- test/marion/src/dev/marion/dev/devtools.cljs
+++ test/marion/src/dev/marion/dev/devtools.cljs
@@ -1,7 +1,5 @@
 (ns marion.dev.devtools
   (:require [devtools.core :as devtools]))
 
-; -------------------------------------------------------------------------------------------------------------------
-
-(devtools/enable-feature! :sanity-hints :dirac)
+(devtools/enable-feature! :sanity-hints)
 (devtools/install!)
