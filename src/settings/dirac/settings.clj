29ed04cc887f1f1396a58816b91aac1d466bfde6
Antonin Hildebrand <antonin@hildebrand.cz>
2016-04-09 17:01:11 +0200

    settings: add SECOND, MINUTE constants

--- src/settings/dirac/settings.clj
+++ src/settings/dirac/settings.clj
@@ -2,16 +2,19 @@
 
 ; we want this stuff to be accessible both from clojure and clojurescript
 
+(def ^:const SECOND 1000)
+(def ^:const MINUTE (* 60 SECOND))
+
 (def ^:const TEST_DIRAC_AGENT_PORT 8021)
-(def ^:const DIRAC_AGENT_BOOT_TIME 2000)
 (def ^:const TEST_NREPL_SERVER_PORT 8020)
+(def ^:const DIRAC_AGENT_BOOT_TIME (* 2 SECOND))
+(def ^:const TRANSCRIPT_MATCH_TIMEOUT (* 5 SECOND))
 (def ^:const LAUNCH_TASK_KEY "diracLaunchTask")
 (def ^:const LAUNCH_TASK_MESSAGE "dirac-launch-task")
-(def ^:const TRANSCRIPT_MATCH_TIMEOUT 5000)
 
-(def ^:const MARION_INITIAL_WAIT_TIME 1000)
-(def ^:const MARION_RECONNECTION_ATTEMPT_DELAY 2000)
-(def ^:const MARION_MESSAGE_REPLY_TIME 2000)
+(def ^:const MARION_INITIAL_WAIT_TIME (* 1 SECOND))
+(def ^:const MARION_RECONNECTION_ATTEMPT_DELAY (* 2 SECOND))
+(def ^:const MARION_MESSAGE_REPLY_TIME (* 2 SECOND))
 
 ; -- cljs access ------------------------------------------------------------------------------------------------------------
 
