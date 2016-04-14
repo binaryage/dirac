7dacefc03e1eb431ca84161fbec08aa6ef57f7cd
Antonin Hildebrand <antonin@hildebrand.cz>
2016-04-14 19:19:09 +0200

    devtools: call deepTextContent() to retrieve textual rep of console msg
    deepTextContent() properly walks through shadow DOM which is important
    for custom formatters.

--- resources/unpacked/devtools/front_end/console/ConsoleView.js
+++ resources/unpacked/devtools/front_end/console/ConsoleView.js
@@ -777,7 +777,7 @@ WebInspector.ConsoleView.prototype = {
             try {
               var levelText = this._levelForFeedback(message.level);
               var typeText = this._typeForFeedback(message.type, isDiracFlavoredMessage);
-              var messageText = result.formattedMessage().querySelector("span").textContent;
+              var messageText = result.formattedMessage().querySelector("span").deepTextContent();
               var glue = (messageText.indexOf("\n")==-1)?"> ":">\n"; // log multi-line log messages on a new line
               dirac.implant.feedback(typeText+"."+levelText+glue+messageText);
             } catch (e) {};
