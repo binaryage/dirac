5d351b1584e8f3b677eba4321ed7c89c779c4f09
Antonin Hildebrand <antonin@hildebrand.cz>
2016-04-18 02:03:18 +0200

    logging: implement custom log4j layouter

--- src/logging/dirac/logging.clj
+++ src/logging/dirac/logging.clj
@@ -1,30 +1,26 @@
 (ns dirac.logging
-  (:require [dirac.logging.utils :as utils]
-            [dirac.lib.utils :as lib-utils]
+  (:require [dirac.logging.utils :refer [merge-options standard-layout convert-config-to-logging-options standard-layout]]
             [clj-logging-config.log4j :as config]
-            [clojure.tools.logging :as log]
-            [clansi :refer [style]]))
+            [clojure.tools.logging :as log]))
 
 (def initialized? (volatile! false))
 
 ; -- options ----------------------------------------------------------------------------------------------------------------
 
-(defn merge-options [& option-maps]
-  (or (apply lib-utils/deep-merge-ignoring-nils option-maps) {}))
-
-; -- options ----------------------------------------------------------------------------------------------------------------
-
 (def root-options
-  {:pattern (str (style "_ %m" :default) "%n")})
+  (standard-layout :default))
 
 (def base-options
-  {:pattern (str (style "%m" :yellow) "%n")})
+  (standard-layout :yellow))
 
 (def lib-options
-  {:pattern (str (style "%m" :blue) "%n")})
+  (standard-layout :blue))
 
 (def nrepl-options
-  {:pattern (str (style "%m" :green) "%n")})
+  (standard-layout :green))
+
+(def tests-options
+  (standard-layout :magenta))
 
 ; -- public API -------------------------------------------------------------------------------------------------------------
 
@@ -33,28 +29,19 @@
     (log/warn "dirac.logging/setup! already called => ignoring this call")
     (do
       (vreset! initialized? true)
-      (let [options (utils/config->logging-options config)]
-        (println "logging setup")
+      (let [options (convert-config-to-logging-options config)]
         (config/set-loggers!
           :root (merge-options root-options options)
           ; test runners
-          "dirac.browser-tests" (merge-options base-options options)
-          "dirac.agent-tests" (merge-options base-options options)
+          "dirac.browser-tests" (merge-options tests-options options)
+          "dirac.agent-tests" (merge-options tests-options options)
           ; test mocks / helpers
-          "dirac.test.mock-nrepl-tunnel-client" (merge-options base-options options)
-          "dirac.test.mock-weasel-client" (merge-options base-options options)
+          "dirac.test" (merge-options tests-options options)
           ; agent
           "dirac.agent" (merge-options base-options options)
           "dirac.agent-impl" (merge-options base-options options)
-          "dirac.agent.logging" (merge-options base-options options)
-          "dirac.agent.config" (merge-options base-options options)
           ; nrepl
           "dirac.nrepl" (merge-options nrepl-options options)
-          "dirac.nrepl.middleware" (merge-options nrepl-options options)
-          "dirac.nrepl.piggieback" (merge-options nrepl-options options)
           ; lib
-          "dirac.lib.nrepl-client" (merge-options lib-options options)
-          "dirac.lib.nrepl-tunnel" (merge-options lib-options options)
-          "dirac.lib.nrepl-tunnel-server" (merge-options lib-options options)
-          "dirac.lib.weasel-server" (merge-options lib-options options)
-          "dirac.lib.ws-server" (merge-options lib-options options))))))
+          "dirac.logging" (merge-options lib-options options)
+          "dirac.lib" (merge-options lib-options options))))))
--- src/logging/dirac/logging/utils.clj
+++ src/logging/dirac/logging/utils.clj
@@ -1,14 +1,59 @@
 (ns dirac.logging.utils
-  (:require [clojure.set :refer [rename-keys]])
-  (:import (org.apache.log4j Level)))
+  (:require [clojure.set :refer [rename-keys]]
+            [dirac.lib.utils :as lib-utils]
+            [clansi :refer [style]]
+            [cuerdas.core :as cuerdas])
+  (:import (org.apache.log4j Level Layout)
+           (org.apache.log4j.spi LoggingEvent)))
+
+(def default-indent 32)
+(def default-prefix-size 2)
+
+(defn prepare-friendly-logger-name [logger-name indent]
+  {:pre [(> indent 3)]}
+  (let [truncated-name (if (<= (count logger-name) indent)
+                         logger-name
+                         (str "..." (cuerdas/slice logger-name (- (- indent 3)))))]
+    (cuerdas/pad truncated-name {:length indent
+                                 :type   :right})))
+
+(defn prepare-indented-message [message indent]
+  (let [indent-str (cuerdas/pad "" {:length indent})]
+    (->> message
+         (cuerdas/lines)
+         (map-indexed (fn [i s] (str (if (pos? i) indent-str "") " | " s)))
+         (cuerdas/unlines))))
+
+(defn standard-formatter [indent prefix-size event & [style-spec prefix]]
+  (let [{:keys [renderedMessage loggerName]} event
+        friendly-logger-name (prepare-friendly-logger-name loggerName indent)
+        indented-message (prepare-indented-message renderedMessage (+ indent prefix-size))
+        massaged-prefix (or (cuerdas/slice prefix 0 prefix-size) "  ")
+        style-args (if (coll? style-spec) style-spec [style-spec])]
+    (str (apply style (str massaged-prefix friendly-logger-name indented-message) style-args) "\n")))
+
+(defn as-map [^LoggingEvent ev]
+  (assoc (bean ev) :event ev))
+
+(defn create-layout-adaptor [f & args]
+  (proxy [Layout] []
+    (format [ev] (apply f (as-map ev) args))
+    (ignoresThrowable [] true)))
 
 (defn remove-keys-with-nil-val [m]
   (into {} (remove (comp nil? second) m)))
 
-(defn config->logging-options [config]
+(defn convert-config-to-logging-options [config]
   (-> config
       (select-keys [:log-out :log-level])
       (rename-keys {:log-out   :out
                     :log-level :level})
       (update :level #(if % (Level/toLevel % Level/INFO)))
       (remove-keys-with-nil-val)))
+
+(defn merge-options [& option-maps]
+  (or (apply lib-utils/deep-merge-ignoring-nils option-maps) {}))
+
+; https://logging.apache.org/log4j/1.2/apidocs/org/apache/log4j/PatternLayout.html
+(defn standard-layout [& args]
+  {:layout (apply create-layout-adaptor (partial standard-formatter default-indent default-prefix-size) args)})
