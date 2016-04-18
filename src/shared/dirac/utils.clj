64414c771cd1fd65523d88ef4966512ca7e626ff
Antonin Hildebrand <antonin@hildebrand.cz>
2016-04-18 18:36:40 +0200

    utils: fix runonce macro to generate stable names

--- src/shared/dirac/utils.clj
+++ src/shared/dirac/utils.clj
@@ -1,5 +1,9 @@
 (ns dirac.utils)
 
 (defmacro runonce [& body]
-  (let [name (gensym "runonce_")]
-    `(defonce ~name (do ~@body))))
+  (let [code (cons 'do body)
+        code-string (pr-str code)
+        code-hash (hash code-string)
+        name (symbol (str "runonce_" code-hash))]
+    `(defonce ~name {:value ~code
+                     :code  ~code-string})))
