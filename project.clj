88704b80e452a274d2c84b64cbb4929859518251
Antonin Hildebrand <antonin@hildebrand.cz>
2016-04-20 23:40:43 +0200

    bump dirac version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/dirac "0.1.4"
+(defproject binaryage/dirac "0.2.0"
   :description "Dirac DevTools - a Chrome DevTools fork for ClojureScript developers."
   :url "https://github.com/binaryage/dirac"
   :license {:name         "MIT License"
--- src/project/dirac/project.clj
+++ src/project/dirac/project.clj
@@ -4,7 +4,7 @@
 ; but with leiningen design it is non-trivial
 ; the problem is when dirac is used as a checkouts dependency
 
-(def version "0.1.4")
+(def version "0.2.0")
 
 (defmacro get-current-version []
   version)
