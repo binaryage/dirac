e8768974c93390d0a8f35e7d1a373e115baf76b2
Antonin Hildebrand <antonin@hildebrand.cz>
2016-04-30 21:27:19 +0200

    logging: implement our own pprint utility function

--- src/logging/dirac/logging.clj
+++ src/logging/dirac/logging.clj
@@ -1,7 +1,7 @@
 (ns dirac.logging
   (:require [dirac.logging.utils :refer [merge-options standard-layout convert-config-to-logging-options standard-layout]]
             [clj-logging-config.log4j :as config]
-            [clojure.tools.logging :as log]))
+            [clojure.pprint :as clojure-pprint]))
 
 (def initialized? (volatile! false))
 
@@ -25,24 +25,27 @@
 ; -- public API -------------------------------------------------------------------------------------------------------------
 
 (defn setup! [& [config]]
-  (if @initialized?
-    (do
-      #_(log/warn "dirac.logging/setup! already called => ignoring this call"))
-    (do
-      (vreset! initialized? true)
-      (let [options (convert-config-to-logging-options config)]
-        (config/set-loggers!
-          :root (merge-options root-options options)
-          ; test runners
-          "dirac.browser-tests" (merge-options tests-options options)
-          "dirac.agent-tests" (merge-options tests-options options)
-          ; test mocks / helpers
-          "dirac.test" (merge-options tests-options options)
-          ; agent
-          "dirac.agent" (merge-options base-options options)
-          "dirac.agent-impl" (merge-options base-options options)
-          ; nrepl
-          "dirac.nrepl" (merge-options nrepl-options options)
-          ; lib
-          "dirac.logging" (merge-options lib-options options)
-          "dirac.lib" (merge-options lib-options options))))))
+  (when-not @initialized?
+    (vreset! initialized? true)
+    (let [options (convert-config-to-logging-options config)]
+      (config/set-loggers!
+        :root (merge-options root-options options)
+        ; test runners
+        "dirac.browser-tests" (merge-options tests-options options)
+        "dirac.agent-tests" (merge-options tests-options options)
+        ; test mocks / helpers
+        "dirac.test" (merge-options tests-options options)
+        ; agent
+        "dirac.agent" (merge-options base-options options)
+        "dirac.agent-impl" (merge-options base-options options)
+        ; nrepl
+        "dirac.nrepl" (merge-options nrepl-options options)
+        ; lib
+        "dirac.logging" (merge-options lib-options options)
+        "dirac.lib" (merge-options lib-options options)))))
+
+(defn pprint [data]
+  (with-out-str
+    (binding [*print-level* 5                                                                                                 ; we have to be careful here, data might contain circular references
+              *print-length* 30]
+      (clojure-pprint/pprint data))))
