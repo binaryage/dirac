cd92a7152ba5254172cf448b11660db82e796826
Antonin Hildebrand <antonin@hildebrand.cz>
2016-05-05 23:25:05 +0200

    scripts: fix sed options in update-versions.sh

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -16,10 +16,10 @@ if [ -z "$VERSION" ] ; then
   exit 1
 fi
 
-sed -i "" -e "s/defproject binaryage\/dirac \".*\"/defproject binaryage\/dirac \"$VERSION\"/g" "$PROJECT_FILE"
-sed -i "" -e "s/def version \".*\"/def version \"$VERSION\"/g" "$PROJECT_VERSION_FILE"
-sed -i "" -e "s/\"version\"\: \".*\"/\"version\": \"$VERSION\"/g" "$UNPACKED_MANIFEST_FILE"
-sed -i "" -e "s/\"version\"\: \".*\"/\"version\": \"$VERSION\"/g" "$RELEASE_MANIFEST_FILE"
+sed -i -e "s/defproject binaryage\/dirac \".*\"/defproject binaryage\/dirac \"$VERSION\"/g" "$PROJECT_FILE"
+sed -i -e "s/def version \".*\"/def version \"$VERSION\"/g" "$PROJECT_VERSION_FILE"
+sed -i -e "s/\"version\"\: \".*\"/\"version\": \"$VERSION\"/g" "$UNPACKED_MANIFEST_FILE"
+sed -i -e "s/\"version\"\: \".*\"/\"version\": \"$VERSION\"/g" "$RELEASE_MANIFEST_FILE"
 
 # this is just a sanity check
 ./scripts/check-versions.sh
