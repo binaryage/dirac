2a97874546ff663942ba6f269b54ef448bfb9ceb
Antonin Hildebrand <antonin@hildebrand.cz>
2016-05-05 23:25:39 +0200

    bump dirac version to 0.3.0

--- src/project/dirac/project.clj
+++ src/project/dirac/project.clj
@@ -4,7 +4,7 @@
 ; but with leiningen design it is non-trivial
 ; the problem is when dirac is used as a checkouts dependency
 
-(def version "0.2.0")
+(def version "0.3.0")
 
 (defmacro get-current-version []
   version)
