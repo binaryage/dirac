787817371c485b10de34670897bdd8cd3c359f2e
Antonin Hildebrand <antonin@hildebrand.cz>
2016-05-13 19:39:53 +0200

    devtools: implement dirac.extractInfoFromScopeChainPromise

--- resources/unpacked/devtools/front_end/dirac/dirac.js
+++ resources/unpacked/devtools/front_end/dirac/dirac.js
@@ -147,6 +147,95 @@ function evalInDefaultContext(code, callback) {
   evalInContext(lookupDefaultContext(), code, callback);
 }
 
+function getScopeTitle(scope) {
+  var title = null;
+
+  switch (scope.type()) {
+    case DebuggerAgent.ScopeType.Local:
+      title = WebInspector.UIString("Local");
+      break;
+    case DebuggerAgent.ScopeType.Closure:
+      var scopeName = scope.name();
+      if (scopeName)
+        title = WebInspector.UIString("Closure (%s)", WebInspector.beautifyFunctionName(scopeName));
+      else
+        title = WebInspector.UIString("Closure");
+      break;
+    case DebuggerAgent.ScopeType.Catch:
+      title = WebInspector.UIString("Catch");
+      break;
+    case DebuggerAgent.ScopeType.Block:
+      title = WebInspector.UIString("Block");
+      break;
+    case DebuggerAgent.ScopeType.Script:
+      title = WebInspector.UIString("Script");
+      break;
+    case DebuggerAgent.ScopeType.With:
+      title = WebInspector.UIString("With Block");
+      break;
+    case DebuggerAgent.ScopeType.Global:
+      title = WebInspector.UIString("Global");
+      break;
+  }
+
+  return title;
+}
+
+function extractNamesFromScopePromise(scope) {
+  var title = getScopeTitle(scope);
+  var remoteObject = WebInspector.SourceMapNamesResolver.resolveScopeInObject(scope);
+
+  var result = {title: title};
+
+  return new Promise(function (resolve) {
+
+    function processProperties(properties, internalProperties) {
+      if (properties) {
+        var props = properties.map(function(property) {
+          var propertyRecord = { name: property.name };
+          if (property.resolutionSourceProperty) {
+            var identifier = property.resolutionSourceProperty.name;
+            if (identifier != property.name) {
+              propertyRecord.identifier = identifier;
+            }
+          }
+          return propertyRecord;
+        });
+        result.props = props;
+      }
+
+      resolve(result);
+    }
+
+    remoteObject.getAllProperties(false, processProperties);
+  });
+}
+
+function extractInfoFromScopeChainPromise(callFrame) {
+  if (!callFrame) {
+    return Promise.resolve(null);
+  }
+
+  return new Promise(function (resolve) {
+    var scopeNamesPromises = [];
+
+    var scopeChain = callFrame.scopeChain();
+    for (var i = 0; i < scopeChain.length; ++i) {
+      var scope = scopeChain[i];
+      if (scope.type() === DebuggerAgent.ScopeType.Global) {
+        continue;
+      }
+
+      scopeNamesPromises.push(extractNamesFromScopePromise(scope));
+    }
+
+    Promise.all(scopeNamesPromises).then(function(frames){
+      var result = {frames: frames};
+      resolve(result);
+    });
+  });
+}
+
 // don't forget to update externs.js too
 window.dirac = {
   _DEBUG_EVAL: false,
@@ -159,6 +248,7 @@ window.dirac = {
   codeAsString: codeAsString,
   stringEscape: stringEscape,
   evalInCurrentContext: evalInCurrentContext,
+  extractInfoFromScopeChainPromise: extractInfoFromScopeChainPromise,
   hasCurrentContext: hasCurrentContext,
   evalInDefaultContext: evalInDefaultContext,
   hasDefaultContext: hasDefaultContext
