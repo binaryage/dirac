29a0786f57b0472a2136bba434137910f6fb62d0
Antonin Hildebrand <antonin@hildebrand.cz>
2016-05-21 01:50:13 +0200

    runtime: use Integer/parseInt

--- src/runtime/dirac/runtime/repl.clj
+++ src/runtime/dirac/runtime/repl.clj
@@ -19,21 +19,21 @@
 (defmacro gen-config []
   (merge {}
          (if *agent-host* [:agent-host (str *agent-host*)])
-         (if *agent-port* [:agent-port (int *agent-port*)])
+         (if *agent-port* [:agent-port (Integer/parseInt *agent-port*)])
          (if *agent-verbose* [:agent-verbose (boolean *agent-verbose*)])
          (if *agent-auto-reconnect* [:agent-auto-reconnect (boolean *agent-auto-reconnect*)])
-         (if *agent-response-timeout* [:agent-response-timeout (int *agent-response-timeout*)])
+         (if *agent-response-timeout* [:agent-response-timeout (Integer/parseInt *agent-response-timeout*)])
          (if *weasel-verbose* [:weasel-verbose (boolean *weasel-verbose*)])
          (if *weasel-auto-reconnect* [:weasel-auto-reconnect (boolean *weasel-auto-reconnect*)])
-         (if *weasel-pre-eval-delay* [:weasel-pre-eval-delay (int *weasel-pre-eval-delay*)])
-         (if *install-check-total-time-limit* [:install-check-total-time-limit (int *install-check-total-time-limit*)])
-         (if *install-check-next-trial-waiting-time* [:install-check-next-trial-waiting-time (int *install-check-next-trial-waiting-time*)])
-         (if *install-check-eval-time-limit* [:install-check-eval-time-limit (int *install-check-eval-time-limit*)])
-         (if *context-availablity-total-time-limit* [:context-availablity-total-time-limit (int *context-availablity-total-time-limit*)])
-         (if *context-availablity-next-trial-waiting-time* [:context-availablity-next-trial-waiting-time (int *context-availablity-next-trial-waiting-time*)])
-         (if *eval-time-limit* [:eval-time-limit (int *eval-time-limit*)])))
+         (if *weasel-pre-eval-delay* [:weasel-pre-eval-delay (Integer/parseInt *weasel-pre-eval-delay*)])
+         (if *install-check-total-time-limit* [:install-check-total-time-limit (Integer/parseInt *install-check-total-time-limit*)])
+         (if *install-check-next-trial-waiting-time* [:install-check-next-trial-waiting-time (Integer/parseInt *install-check-next-trial-waiting-time*)])
+         (if *install-check-eval-time-limit* [:install-check-eval-time-limit (Integer/parseInt *install-check-eval-time-limit*)])
+         (if *context-availablity-total-time-limit* [:context-availablity-total-time-limit (Integer/parseInt *context-availablity-total-time-limit*)])
+         (if *context-availablity-next-trial-waiting-time* [:context-availablity-next-trial-waiting-time (Integer/parseInt *context-availablity-next-trial-waiting-time*)])
+         (if *eval-time-limit* [:eval-time-limit (Integer/parseInt *eval-time-limit*)])))
 
 (defmacro with-safe-printing [& body]
   `(binding [cljs.core/*print-level* (dirac.runtime.prefs/pref :safe-print-level)
              cljs.core/*print-length* (dirac.runtime.prefs/pref :safe-print-length)]
-          ~@body))
+     ~@body))
