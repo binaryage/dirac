2dfeb7184bff6c4a8c929128e6320a35e072123e
Antonin Hildebrand <antonin@hildebrand.cz>
2016-05-21 01:55:46 +0200

    tests: implement some basic auto-completion tests

--- test/browser/fixtures/src/tests/dirac/tests/tasks/suite01/completions.cljs
+++ test/browser/fixtures/src/tests/dirac/tests/tasks/suite01/completions.cljs
@@ -1,16 +1,33 @@
 (ns dirac.tests.tasks.suite01.completions
   (:require [cljs.core.async :refer [<! timeout]]
+            [cljs.test :refer-macros [deftest testing is]]
             [dirac.automation :as auto :refer-macros [go-task with-devtools]]))
 
 (go-task
   (<! (auto/open-tab-with-scenario! "completions"))
-  (with-devtools (<! (auto/open-dirac-devtools!))
-    (auto/switch-to-console-and-wait-for-it)
-    (auto/switch-to-dirac-prompt!)
-    (auto/wait-for-prompt-edit)
-    (auto/enable-console-feedback!)
+  (let [devtools-id (<! (auto/open-dirac-devtools!))]
+    (<! (auto/switch-to-console-and-wait-for-it devtools-id))
+    (<! (auto/switch-to-dirac-prompt! devtools-id))
+    (<! (auto/wait-for-prompt-edit devtools-id))
+    (<! (auto/enable-console-feedback! devtools-id))
     ; ---
-    (auto/console-exec-and-wait-for-match! "(in-ns 'dirac.tests.scenarios.completions.workspace)"
-                                           "setDiracPromptNS('dirac.tests.scenarios.completions.workspace')")
-    (auto/dispatch-console-prompt-input! "sample")
-    (auto/dispatch-console-prompt-action! "backspace")))
+    ; test in-ns completions for our namespace
+    (auto/clear-console-prompt! devtools-id)
+    (<! (auto/console-exec-and-wait-for-match! devtools-id "(in-ns 'dirac.tests.scenarios.completions.workspace)"
+                                               "setDiracPromptNS('dirac.tests.scenarios.completions.workspace')"))
+    (<! (auto/dispatch-console-prompt-input! devtools-id "sample"))
+    (<! (auto/print-suggest-box-representation devtools-id))
+    ; test javascript completions in js/ namespace
+    (auto/clear-console-prompt! devtools-id)
+    (<! (auto/dispatch-console-prompt-input! devtools-id "js/docume"))
+    (<! (auto/print-suggest-box-representation devtools-id))
+    (<! (auto/dispatch-console-prompt-input! devtools-id "nt.get"))
+    (<! (auto/print-suggest-box-representation devtools-id))
+    ; test fully qualified complations
+    (auto/clear-console-prompt! devtools-id)
+    (<! (auto/dispatch-console-prompt-input! devtools-id "cljs.core/part"))
+    (<! (auto/print-suggest-box-representation devtools-id))
+    ; test namespace completions
+    (auto/clear-console-prompt! devtools-id)
+    (<! (auto/dispatch-console-prompt-input! devtools-id "devtools."))
+    (<! (auto/print-suggest-box-representation devtools-id))))
