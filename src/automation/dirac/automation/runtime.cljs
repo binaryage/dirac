226d3bf2fb644b79e1227198c1b0732fb8584249
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-01 21:14:38 +0200

    automation: add support for :old-runtime and :future-runtime mocks

--- src/automation/dirac/automation/runtime.cljs
+++ src/automation/dirac/automation/runtime.cljs
@@ -1,5 +1,6 @@
 (ns dirac.automation.runtime
   (:require [chromex.logging :refer-macros [log warn error info]]
+            [chromex.support :refer-macros [oset oget]]
             [dirac.runtime :as runtime]
             [dirac.runtime.prefs :as runtime-prefs]
             [dirac.automation.helpers :as helpers]))
@@ -13,6 +14,9 @@
       (warn "setting dirac runtime prefs via url params" (pr-str prefs))                                                      ; use pr-str because cljs-devtools is not yet installed
       (runtime-prefs/merge-prefs! prefs))))
 
+(defn monkey-patch-runtime-get-version! [mock-version]
+  (oset js/window ["dirac" "runtime" "get_version"] (constantly mock-version)))
+
 (defn init-runtime! [& [config]]
   (configure-runtime-from-url-params! (helpers/get-document-url))
   (when-let [runtime-prefs (:runtime-prefs config)]                                                                           ; override runtime prefs
@@ -22,4 +26,8 @@
     (let [features-to-enable (cond-> []
                                (not (:do-not-enable-repl config)) (conj :repl))]
       (runtime/install! features-to-enable))
-    (warn "dirac runtime override: do not install")))
+    (warn "dirac runtime override: do not install"))
+  (if (:old-runtime config)
+    (monkey-patch-runtime-get-version! "0.0.1"))
+  (if (:future-runtime config)
+    (monkey-patch-runtime-get-version! "1000.0.1")))
