254f8c00092eb7f5dd2f2fc806d2c70f2d5c86d5
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-01 23:24:47 +0200

    tests: make use of new with-scenario macron in version-checks

--- test/browser/fixtures/src/tests/dirac/tests/tasks/suite01/version_checks.cljs
+++ test/browser/fixtures/src/tests/dirac/tests/tasks/suite01/version_checks.cljs
@@ -2,28 +2,26 @@
   (:require [cljs.core.async]
             [cljs.test :refer-macros [is testing]]
             [dirac.settings :refer-macros [seconds minutes]]
-            [dirac.automation :refer-macros [<!* go-task] :as a]))
+            [dirac.automation :refer-macros [<!* go-task with-scenario] :as a]))
 
 (go-task
-  (let [scenario-id (<!* a/open-tab-with-scenario! "old-runtime")]
+  (with-scenario "old-runtime"
     (<!* a/open-devtools!)
     (<!* a/switch-to-console-panel!)
     (<!* a/switch-prompt-to-dirac!)
     (<!* a/wait-for-match "Version mismatch: Dirac Runtime installed in the page has different version")
     (<!* a/wait-for-devtools-match "console prompt focused")
     (<!* a/wait-for-prompt-to-enter-edit-mode)
-    (<!* a/close-devtools!)
-    (<!* a/close-tab-with-scenario! scenario-id))
-  (let [scenario-id (<!* a/open-tab-with-scenario! "future-runtime")]
+    (<!* a/close-devtools!))
+  (with-scenario "future-runtime"
     (<!* a/open-devtools!)
     (<!* a/switch-to-console-panel!)
     (<!* a/switch-prompt-to-dirac!)
     (<!* a/wait-for-match "Version mismatch: Dirac Runtime installed in the page has different version")
     (<!* a/wait-for-devtools-match "console prompt focused")
     (<!* a/wait-for-prompt-to-enter-edit-mode)
-    (<!* a/close-devtools!)
-    (<!* a/close-tab-with-scenario! scenario-id))
-  (let [scenario-id (<!* a/open-tab-with-scenario! "normal-with-feedback")]
+    (<!* a/close-devtools!))
+  (with-scenario "normal-with-feedback"
     (<!* a/open-devtools! {"mock_old_extension_version" 1})
     (<!* a/switch-to-console-panel!)
     (<!* a/switch-prompt-to-dirac!)
@@ -31,9 +29,8 @@
     (<!* a/wait-for-match #"Version mismatch: Dirac Agent has different version .*? than Dirac Chrome Extension \(v0\.0\.1\)\.")
     (<!* a/wait-for-devtools-match "console prompt focused")
     (<!* a/wait-for-prompt-to-enter-edit-mode)
-    (<!* a/close-devtools!)
-    (<!* a/close-tab-with-scenario! scenario-id))
-  (let [scenario-id (<!* a/open-tab-with-scenario! "normal-with-feedback")]
+    (<!* a/close-devtools!))
+  (with-scenario "normal-with-feedback"
     (<!* a/open-devtools! {"mock_future_extension_version" 1})
     (<!* a/switch-to-console-panel!)
     (<!* a/switch-prompt-to-dirac!)
@@ -41,39 +38,34 @@
     (<!* a/wait-for-match #"Version mismatch: Dirac Agent has different version .*? than Dirac Chrome Extension \(v1000\.0\.1\)\.")
     (<!* a/wait-for-devtools-match "console prompt focused")
     (<!* a/wait-for-prompt-to-enter-edit-mode)
-    (<!* a/close-devtools!)
-    (<!* a/close-tab-with-scenario! scenario-id))
-  (let [scenario-id (<!* a/open-tab-with-scenario! "old-repl-api")]
+    (<!* a/close-devtools!))
+  (with-scenario "old-repl-api"
     (<!* a/open-devtools!)
     (<!* a/switch-to-console-panel!)
     (<!* a/switch-prompt-to-dirac!)
     (<!* a/wait-for-match "Dirac REPL API version mismatch detected.")
     (<!* a/wait-for-devtools-match "console prompt focused")
     (<!* a/wait-for-devtools-match "setDiracPromptStatusStyle('error')")
-    (<!* a/close-devtools!)
-    (<!* a/close-tab-with-scenario! scenario-id))
-  (let [scenario-id (<!* a/open-tab-with-scenario! "future-repl-api")]
+    (<!* a/close-devtools!))
+  (with-scenario "future-repl-api"
     (<!* a/open-devtools!)
     (<!* a/switch-to-console-panel!)
     (<!* a/switch-prompt-to-dirac!)
     (<!* a/wait-for-match "Dirac REPL API version mismatch detected.")
     (<!* a/wait-for-devtools-match "console prompt focused")
     (<!* a/wait-for-devtools-match "setDiracPromptStatusStyle('error')")
-    (<!* a/close-devtools!)
-    (<!* a/close-tab-with-scenario! scenario-id))
-  (let [scenario-id (<!* a/open-tab-with-scenario! "no-runtime")]
+    (<!* a/close-devtools!))
+  (with-scenario "no-runtime"
     (<!* a/open-devtools!)
     (<!* a/switch-to-console-panel!)
     (<!* a/switch-prompt-to-dirac!)
     (<!* a/wait-for-match "Dirac requires runtime support from the page context" (seconds 20))
     (<!* a/wait-for-devtools-match "setDiracPromptStatusStyle('error')" (seconds 20))
-    (<!* a/close-devtools!)
-    (<!* a/close-tab-with-scenario! scenario-id))
-  (let [scenario-id (<!* a/open-tab-with-scenario! "no-repl")]
+    (<!* a/close-devtools!))
+  (with-scenario "no-repl"
     (<!* a/open-devtools!)
     (<!* a/switch-to-console-panel!)
     (<!* a/switch-prompt-to-dirac!)
     (<!* a/wait-for-match "Dirac requires runtime support from the page context" (seconds 20))
     (<!* a/wait-for-devtools-match "setDiracPromptStatusStyle('error')" (seconds 20))
-    (<!* a/close-devtools!)
-    (<!* a/close-tab-with-scenario! scenario-id)))
+    (<!* a/close-devtools!)))
