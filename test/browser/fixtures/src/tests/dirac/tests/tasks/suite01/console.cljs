8ebd7648cf8531dba20a5eb6407ed57d7efd1119
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-01 10:57:50 +0200

    tests: add testing keyboard shortcuts for switching between prompts

---
+++ test/browser/fixtures/src/tests/dirac/tests/tasks/suite01/console.cljs
@@ -0,0 +1,32 @@
+(ns dirac.tests.tasks.suite01.console
+  (:require [cljs.core.async]
+            [cljs.test :refer-macros [is testing]]
+            [dirac.automation :refer-macros [<!* go-task go-job] :as a]))
+
+(go-task
+  (<!* a/open-tab-with-scenario! "normal")
+  (<!* a/open-devtools!)
+  (<!* a/switch-to-console-panel!)
+  (<!* a/enable-console-feedback!)
+  ; -------------------------------------------------------------------------------------------------------------------------
+  (testing "keyboard shortcuts for switching between prompts"
+    (<!* a/simulate-console-action! "CTRL+.")
+    (<!* a/wait-for-prompt-switch-to-dirac)
+    (<!* a/wait-for-prompt-to-enter-edit-mode)
+    (<!* a/simulate-console-action! "CTRL+.")
+    (<!* a/wait-for-prompt-switch-to-js)
+    (<!* a/simulate-console-action! "CTRL+.")
+    (<!* a/wait-for-prompt-switch-to-dirac)
+    (<!* a/simulate-console-action! "CTRL+,")
+    (<!* a/wait-for-prompt-switch-to-js)
+    (<!* a/simulate-console-action! "CTRL+.")
+    (<!* a/simulate-console-action! "CTRL+.")
+    (<!* a/wait-for-prompt-switch-to-js)
+    (<!* a/simulate-console-action! "CTRL+,")
+    (<!* a/wait-for-prompt-switch-to-dirac))
+  ; -------------------------------------------------------------------------------------------------------------------------
+  (testing "prompt input simulation and feedback"
+    (<!* a/simulate-console-input! "hello!")
+    (is (= (<!* a/print-prompt!) "hello!"))
+    (<!* a/clear-console-prompt!)
+    (is (= (<!* a/get-prompt-representation) ""))))
