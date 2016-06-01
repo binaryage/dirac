89f1d07044ac2916b8b8ffaa5e3b1b0fdbf28224
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-01 11:05:53 +0200

    options: refactor dirac.options.ui

--- src/options/dirac/options/ui.cljs
+++ src/options/dirac/options/ui.cljs
@@ -1,8 +1,9 @@
 (ns dirac.options.ui
   (:require [chromex.logging :refer-macros [log info warn error group group-end]]
-            [reforms.rum :include-macros true :as f]
             [rum.core :include-macros true :as rum :refer-macros [defc]]
-            [dirac.options.model :as model]))
+            [dirac.options.model :as model]
+            [reforms.rum :include-macros true :as fr]
+            [reforms.core :as f]))
 
 (defonce default-state {:options model/default-options})
 
@@ -30,30 +31,30 @@
 ; -- views ------------------------------------------------------------------------------------------------------------------
 
 (defc options-view < rum/cursored rum/cursored-watch [data]
-      (f/with-options
-        {:form         {:horizontal true}
-         :button-group {:align "text-right"}}
-        (f/panel
-          (f/form
-            {:on-submit save-state-and-exit!}
-            (f/url "Target URL for debugger:" data [:options :target-url] :placeholder "http://localhost:9222")
-            (f/select "Open Dirac DevTools:" data [:options :open-as]
-                      [["panel" "as a new panel (recommended)"]
-                       ["window" "as a new window"]
-                       ["tab" "as a new tab"]])
-            [:div {:class "switches"}
-             (f/checkbox "Enable REPL" data [:options :enable-repl])
-             (f/checkbox "Enable Parinfer" data [:options :enable-parinfer])
-             (f/checkbox "Enable friendly locals" data [:options :enable-friendly-locals])
-             (f/checkbox "Enable clustered locals" data [:options :enable-clustered-locals])
-             (f/checkbox "Inline Custom Formatters in sources" data [:options :inline-custom-formatters])])
-          (f/form-buttons
-            (f/button "Reset to Defaults" reset-to-defaults!)
-            (f/button "Save and Exit" save-state-and-exit!)))))
+  (let [form-options {:form         {:horizontal true}
+                      :button-group {:align "text-right"}}]
+    (fr/with-options form-options
+      (f/panel
+        (f/form
+          {:on-submit save-state-and-exit!}
+          (f/url "Target URL for debugger:" data [:options :target-url] :placeholder "http://localhost:9222")
+          (f/select "Open Dirac DevTools:" data [:options :open-as]
+                    [["panel" "as a new panel (recommended)"]
+                     ["window" "as a new window"]
+                     ["tab" "as a new tab"]])
+          [:div {:class "switches"}
+           (f/checkbox "Enable REPL" data [:options :enable-repl])
+           (f/checkbox "Enable Parinfer" data [:options :enable-parinfer])
+           (f/checkbox "Enable friendly locals" data [:options :enable-friendly-locals])
+           (f/checkbox "Enable clustered locals" data [:options :enable-clustered-locals])
+           (f/checkbox "Inline Custom Formatters in sources" data [:options :inline-custom-formatters])])
+        (f/form-buttons
+          (f/button "Reset to Defaults" reset-to-defaults!)
+          (f/button "Save and Exit" save-state-and-exit!))))))
 
 (defc main-view [state]
-      [:div
-       (options-view state)])
+  [:div
+   (options-view state)])
 
 ; -- mounting ---------------------------------------------------------------------------------------------------------------
 
