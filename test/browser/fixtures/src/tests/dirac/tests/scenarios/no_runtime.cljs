57fb9a5de7806b73ab43bd856e5e245e7858e585
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-02 20:36:10 +0200

    tests: make sure no-runtime scenario really does not include runtime

--- test/browser/fixtures/src/tests/dirac/tests/scenarios/no_runtime.cljs
+++ test/browser/fixtures/src/tests/dirac/tests/scenarios/no_runtime.cljs
@@ -1,7 +1,5 @@
 (ns dirac.tests.scenarios.no-runtime
-  (:require [dirac.automation.runtime :refer [init-runtime!]]
-            [dirac.automation.scenario :as scenario]))
+  (:require [dirac.automation.scenario :as scenario]))
 
-(init-runtime! {:do-not-install-runtime true})
 (scenario/capture-console-as-feedback!)
 (scenario/ready!)
