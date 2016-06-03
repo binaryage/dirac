4a8c420bf8585abb0acbf8938e13809dfa3ce663
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-03 23:00:14 +0200

    tests: minor cleanup in 'version-checks'

--- test/browser/fixtures/src/tests/dirac/tests/tasks/suite01/version_checks.cljs
+++ test/browser/fixtures/src/tests/dirac/tests/tasks/suite01/version_checks.cljs
@@ -10,14 +10,14 @@
       (<!* a/switch-to-console-panel!)
       (<!* a/switch-prompt-to-dirac!)
       (<!* a/wait-for-match "Version mismatch: Dirac Runtime installed in your app has different version")
-      (<!* a/wait-for-devtools-match "console prompt focused")
+      (<!* a/wait-for-console-focused)
       (<!* a/wait-for-prompt-to-enter-edit-mode)))
   (with-scenario "future-runtime"
     (with-devtools
       (<!* a/switch-to-console-panel!)
       (<!* a/switch-prompt-to-dirac!)
       (<!* a/wait-for-match "Version mismatch: Dirac Runtime installed in your app has different version")
-      (<!* a/wait-for-devtools-match "console prompt focused")
+      (<!* a/wait-for-console-focused)
       (<!* a/wait-for-prompt-to-enter-edit-mode)))
   (with-scenario "normal-with-feedback"
     (with-devtools {"mock_old_extension_version" 1}
@@ -25,7 +25,7 @@
       (<!* a/switch-prompt-to-dirac!)
       (<!* a/wait-for-match "Version mismatch: Dirac Runtime installed in your app has different version")
       (<!* a/wait-for-match "Version mismatch: Dirac Agent has different version")
-      (<!* a/wait-for-devtools-match "console prompt focused")
+      (<!* a/wait-for-console-focused)
       (<!* a/wait-for-prompt-to-enter-edit-mode)))
   (with-scenario "normal-with-feedback"
     (with-devtools {"mock_future_extension_version" 1}
@@ -33,21 +33,19 @@
       (<!* a/switch-prompt-to-dirac!)
       (<!* a/wait-for-match "Version mismatch: Dirac Runtime installed in your app has different version")
       (<!* a/wait-for-match "Version mismatch: Dirac Agent has different version")
-      (<!* a/wait-for-devtools-match "console prompt focused")
+      (<!* a/wait-for-console-focused)
       (<!* a/wait-for-prompt-to-enter-edit-mode)))
   (with-scenario "old-repl-api"
     (with-devtools
       (<!* a/switch-to-console-panel!)
       (<!* a/switch-prompt-to-dirac!)
       (<!* a/wait-for-match "Dirac REPL API version mismatch detected.")
-      (<!* a/wait-for-devtools-match "console prompt focused")
       (<!* a/wait-for-devtools-match "setDiracPromptStatusStyle('error')")))
   (with-scenario "future-repl-api"
     (with-devtools
       (<!* a/switch-to-console-panel!)
       (<!* a/switch-prompt-to-dirac!)
       (<!* a/wait-for-match "Dirac REPL API version mismatch detected.")
-      (<!* a/wait-for-devtools-match "console prompt focused")
       (<!* a/wait-for-devtools-match "setDiracPromptStatusStyle('error')")))
   (with-scenario "no-runtime"
     (with-devtools
