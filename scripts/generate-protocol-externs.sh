265eb543b9a4b5182fb310a0086ebe63fd9bea80
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-06 00:19:51 +0200

    scripts: an attempt fix the build again

--- scripts/generate-protocol-externs.sh
+++ scripts/generate-protocol-externs.sh
@@ -6,7 +6,7 @@ set -e
 
 pushd "$DEVTOOLS_ROOT"
 
-python scripts/generate_protocol_externs.py -o "$PROTOCOL_EXTERNS_OUTPUT_FILE" "$PROTOCOL_JSON_FILE"
+python scripts/generate_protocol_externs.py -o "$PROTOCOL_EXTERNS_OUTPUT_FILE" "$BROWSER_PROTOCOL_JSON_FILE" "$V8_PROTOCOL_JSON_FILE"
 echo "Generated fresh $PROTOCOL_EXTERNS_OUTPUT_FILE"
 
 popd
