0c1fcd1c6cb7d20f1d3272e01fd2132f2ca31302
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-05 17:01:51 +0200

    tests: update tests for new code completions code

--- test/browser/fixtures/src/tests/dirac/tests/scenarios/completions/workspace.cljs
+++ test/browser/fixtures/src/tests/dirac/tests/scenarios/completions/workspace.cljs
@@ -1,4 +1,9 @@
-(ns dirac.tests.scenarios.completions.workspace)
+(ns dirac.tests.scenarios.completions.workspace
+  (:require-macros [cljs.core.async.macros :as alias-cljs-core-async-macros :refer [go]]
+                   [chromex.logging :refer [log warn]]
+                   [chromex.support])
+  (:require [cljs.core.async :as alias-cljs-core-async :refer [put!]]
+            [dirac.settings :as alias-dirac-settings :refer-macros [get-dirac-window-top]]))
 
 (def sample-def "sample-def")
 (defonce sample-defonce "sample-defonce")
