cce56470975a287c0164e6f79cd525d6ed27a543
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-05 20:01:42 +0200

    automation: chunkify to work around stack overflow in cljs compiler

--- src/automation/dirac/automation.clj
+++ src/automation/dirac/automation.clj
@@ -22,6 +22,20 @@
                                                      (cljs.test/empty-env :cljs.test/default))))
        (dirac.automation.task/task-setup!))))
 
+; this macro exists solely to work around a bug in cljs compiler
+; too many commands on single go-block level causes infinite compiler loop:
+; https://gist.github.com/darwin/19552acb6f94887d641e878272a19f3c
+; I believe this to be related to http://dev.clojure.org/jira/browse/CLJS-365
+; I'm not sure what go macro does exactly, but it might generate an apply call which exceeds safe number of args
+(defmacro chunkify [& commands]
+  (let [safe-number-of-commands 17                                                                                            ; this number was determined experimentally, 18 seems to be smallest size causing stack overflow
+        command-chunks (partition-all safe-number-of-commands commands)
+        gen-chunk (fn [commands]
+                    `(cljs.core.async/<! (cljs.core.async.macros/go
+                                           ~@commands)))]
+    `(do
+       ~@(map gen-chunk command-chunks))))
+
 (defmacro <!* [action & args]
   {:pre [(symbol? action)]}
   `(cljs.core.async/<! (dirac.automation/action! ~action (meta #'~action) ~@args)))
--- test/browser/fixtures/src/tests/dirac/tests/tasks/suite01/completions.cljs
+++ test/browser/fixtures/src/tests/dirac/tests/tasks/suite01/completions.cljs
@@ -1,84 +1,85 @@
 (ns dirac.tests.tasks.suite01.completions
   (:require [cljs.core.async]
             [cljs.test :refer-macros [is testing]]
-            [dirac.automation :refer-macros [<!* go-task with-scenario with-devtools] :as a]))
+            [dirac.automation :refer-macros [<!* go-task with-scenario with-devtools chunkify] :as a]))
 
 (go-task
   (with-scenario "completions"
     (with-devtools
-      (<!* a/switch-to-console-panel!)
-      (<!* a/switch-prompt-to-dirac!)
-      (<!* a/wait-for-prompt-to-enter-edit-mode)
-      (<!* a/enable-console-feedback!)
-      (<!* a/console-exec-and-match!
-           "(in-ns 'dirac.tests.scenarios.completions.workspace)"
-           "setDiracPromptNS('dirac.tests.scenarios.completions.workspace')")
-      (testing "in-ns completions for our namespace"
-        (<!* a/simulate-console-input! "sample")
-        (<!* a/scrape! :suggest-box))
-      (testing "javascript completions in js/ namespace"
-        (<!* a/clear-console-prompt!)
-        (<!* a/simulate-console-input! "js/docume")
-        (<!* a/scrape! :suggest-box)
-        (<!* a/simulate-console-action! "TAB")
-        (is (= (<!* a/print-prompt!) "js/document"))
-        (<!* a/simulate-console-input! ".get")
-        (<!* a/scrape! :suggest-box)
-        (<!* a/simulate-console-action! "TAB")
-        (is (= (<!* a/print-prompt!) "js/document.getElementById")))
-      (testing "qualified completions"
-        (<!* a/clear-console-prompt!)
-        (<!* a/simulate-console-input! "cljs.core/part")
-        (<!* a/scrape! :suggest-box)
-        (<!* a/simulate-console-action! "TAB")
-        (is (= (<!* a/print-prompt!) "cljs.core/partial")))
-      (testing "namespace names completions"
-        (<!* a/clear-console-prompt!)
-        (<!* a/simulate-console-input! "devtools.")
-        (<!* a/scrape! :suggest-box)
-        (<!* a/simulate-console-action! "DOWN")
-        (<!* a/simulate-console-action! "TAB")
-        (is (= (<!* a/print-prompt!) "devtools.custom-formatters")))
-      (testing "right-arrow completions"
-        (<!* a/clear-console-prompt!)
-        (<!* a/simulate-console-input! "cljs.cor")
-        (<!* a/scrape! :suggest-box)
-        (<!* a/simulate-console-action! "RIGHT")
-        (is (= (<!* a/print-prompt!) "cljs.core"))
-        (<!* a/simulate-console-input! "/clj-")
-        (<!* a/scrape! :suggest-box)
-        (<!* a/simulate-console-action! "RIGHT")
-        (is (= (<!* a/print-prompt!) "cljs.core/clj->js")))
-      (testing "opening suggestion box"
-        (<!* a/clear-console-prompt!)
-        (is (nil? (<!* a/get-suggest-box-item-count)))
-        (<!* a/simulate-console-action! "CTRL+SPACE")                                                                         ; https://github.com/binaryage/dirac/issues/22)
-        (is (> (<!* a/get-suggest-box-item-count) 100)))
-      (testing "aliases to namespaces and macro namespaces"
-        (<!* a/clear-console-prompt!)
-        (<!* a/simulate-console-input! "alias")
-        (<!* a/scrape! :suggest-box))
-      (testing "refer namespace symbols"
-        (<!* a/clear-console-prompt!)
-        (<!* a/simulate-console-input! "pu")
-        (<!* a/scrape! :suggest-box))
-      (testing "refer macro namespace symbols"
-        (<!* a/clear-console-prompt!)
-        (<!* a/simulate-console-input! "wa")
-        (<!* a/scrape! :suggest-box))
-      ;(testing "refer macro namespace symbols coming from :refer-macros"
-      ;  (<!* a/clear-console-prompt!)
-      ;  (<!* a/simulate-console-input! "get-dirac-window")
-      ;  (<!* a/scrape! :suggest-box))
-      ;(testing "qualified completions of namespace alias"
-      ;  (<!* a/clear-console-prompt!)
-      ;  (<!* a/simulate-console-input! "alias-cljs-core-async/tak")
-      ;  (<!* a/scrape! :suggest-box))
-      (testing "qualified completions of macro namespace alias"
-        (<!* a/clear-console-prompt!)
-        (<!* a/simulate-console-input! "alias-cljs-core-async-macros/go")
-        (<!* a/scrape! :suggest-box))
-      (testing "qualified completions of macro namespace"
-        (<!* a/clear-console-prompt!)
-        (<!* a/simulate-console-input! "chromex.logging/lo")
-        (<!* a/scrape! :suggest-box)))))
+      (chunkify
+        (<!* a/switch-to-console-panel!)
+        (<!* a/switch-prompt-to-dirac!)
+        (<!* a/wait-for-prompt-to-enter-edit-mode)
+        (<!* a/enable-console-feedback!)
+        (<!* a/console-exec-and-match!
+             "(in-ns 'dirac.tests.scenarios.completions.workspace)"
+             "setDiracPromptNS('dirac.tests.scenarios.completions.workspace')")
+        (testing "in-ns completions for our namespace"
+          (<!* a/simulate-console-input! "sample")
+          (<!* a/scrape! :suggest-box))
+        (testing "javascript completions in js/ namespace"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "js/docume")
+          (<!* a/scrape! :suggest-box)
+          (<!* a/simulate-console-action! "TAB")
+          (is (= (<!* a/print-prompt!) "js/document"))
+          (<!* a/simulate-console-input! ".get")
+          (<!* a/scrape! :suggest-box)
+          (<!* a/simulate-console-action! "TAB")
+          (is (= (<!* a/print-prompt!) "js/document.getElementById")))
+        (testing "qualified completions"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "cljs.core/part")
+          (<!* a/scrape! :suggest-box)
+          (<!* a/simulate-console-action! "TAB")
+          (is (= (<!* a/print-prompt!) "cljs.core/partial")))
+        (testing "namespace names completions"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "devtools.")
+          (<!* a/scrape! :suggest-box)
+          (<!* a/simulate-console-action! "DOWN")
+          (<!* a/simulate-console-action! "TAB")
+          (is (= (<!* a/print-prompt!) "devtools.custom-formatters")))
+        (testing "right-arrow completions"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "cljs.cor")
+          (<!* a/scrape! :suggest-box)
+          (<!* a/simulate-console-action! "RIGHT")
+          (is (= (<!* a/print-prompt!) "cljs.core"))
+          (<!* a/simulate-console-input! "/clj-")
+          (<!* a/scrape! :suggest-box)
+          (<!* a/simulate-console-action! "RIGHT")
+          (is (= (<!* a/print-prompt!) "cljs.core/clj->js")))
+        (testing "opening suggestion box"
+          (<!* a/clear-console-prompt!)
+          (is (nil? (<!* a/get-suggest-box-item-count)))
+          (<!* a/simulate-console-action! "CTRL+SPACE")                                                                       ; https://github.com/binaryage/dirac/issues/22)
+          (is (> (<!* a/get-suggest-box-item-count) 100)))
+        (testing "aliases to namespaces and macro namespaces"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "alias")
+          (<!* a/scrape! :suggest-box))
+        (testing "refer namespace symbols"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "pu")
+          (<!* a/scrape! :suggest-box))
+        (testing "refer macro namespace symbols"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "wa")
+          (<!* a/scrape! :suggest-box))
+        (testing "refer macro namespace symbols coming from :refer-macros"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "get-dirac-window")
+          (<!* a/scrape! :suggest-box))
+        (testing "qualified completions of namespace alias"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "alias-cljs-core-async/tak")
+          (<!* a/scrape! :suggest-box))
+        (testing "qualified completions of macro namespace alias"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "alias-cljs-core-async-macros/go")
+          (<!* a/scrape! :suggest-box))
+        (testing "qualified completions of macro namespace"
+          (<!* a/clear-console-prompt!)
+          (<!* a/simulate-console-input! "chromex.logging/lo")
+          (<!* a/scrape! :suggest-box))))))
