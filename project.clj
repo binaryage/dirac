1291029eef8c4c64c139f1386bccf84acfa00606
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-26 22:50:39 +0200

    project: bump dependencies

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
         :url  "https://github.com/binaryage/dirac"}
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.76" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.93" :scope "provided"]
                  [org.clojure/core.async "0.2.385"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.cli "0.3.5"]
@@ -18,12 +18,12 @@
                  [version-clj "0.1.2"]
                  [environ "1.0.3"]
                  [clansi "1.0.0"]
-                 [funcool/cuerdas "0.7.2"]
+                 [funcool/cuerdas "0.8.0"]
 
                  ; we cannot use :dependencies under individual profiles because Cursive recognizes only root level
                  ; thus we mark extra deps with :scope "test" and filter them later when producing jar library
                  [binaryage/chromex "0.4.1" :scope "test"]
-                 [binaryage/devtools "0.7.1" :scope "test"]
+                 [binaryage/devtools "0.7.2" :scope "test"]
                  [cljs-http "0.1.41" :scope "test"]
                  [figwheel "0.5.4-3" :scope "test"]
                  [reforms "0.4.3" :scope "test"]
