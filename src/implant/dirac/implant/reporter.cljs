2b72880520114bcb306670e499e8da3316459e22
Antonin Hildebrand <antonin@hildebrand.cz>
2016-08-16 14:49:52 +0200

    implant: subscribe "unhandledrejection" to report failed promises

--- src/implant/dirac/implant/reporter.cljs
+++ src/implant/dirac/implant/reporter.cljs
@@ -3,7 +3,7 @@
             [chromex.logging :refer-macros [log warn error group group-end]]
             [dirac.utils :as utils]))
 
-; -- handling exceptions ----------------------------------------------------------------------------------------------------
+; -- handling global exceptions ---------------------------------------------------------------------------------------------
 
 (defn devtools-exception-handler! [_message _source _lineno _colno e]
   (let [text (str "Internal Dirac Error: DevTools code has thrown an unhandled exception\n" (utils/format-error e))]
@@ -13,5 +13,19 @@
 (defn register-global-exception-handler! []
   (oset js/window ["onerror"] devtools-exception-handler!))
 
+; -- handling unhandled rejections in promises ------------------------------------------------------------------------------
+
+(defn devtools-unhandled-rejection-handler! [event]
+  (let [reason (oget event "reason")
+        text (str "Internal Dirac Error: DevTools code has thrown an unhandled rejection (in promise)\n"
+                  (utils/format-error reason))]
+    (ocall (oget js/window "dirac") "addConsoleMessageToMainTarget" "error" text)))
+
+(defn register-unhandled-rejection-handler! []
+  (.addEventListener js/window "unhandledrejection" devtools-unhandled-rejection-handler!))
+
+; -- installation -----------------------------------------------------------------------------------------------------------
+
 (defn install! []
-  (register-global-exception-handler!))
+  (register-global-exception-handler!)
+  (register-unhandled-rejection-handler!))
