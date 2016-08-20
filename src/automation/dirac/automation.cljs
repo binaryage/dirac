834ff2450ef98339b1a673d751fe82549970c00b
Antonin Hildebrand <antonin@hildebrand.cz>
2016-08-20 22:14:03 +0200

    automation: interleave actions with (timeout 0) to prevent starvation
    run-output-matching-loop! could be starved if we ran a block of
    non-async actions.

--- src/automation/dirac/automation.cljs
+++ src/automation/dirac/automation.cljs
@@ -216,29 +216,39 @@
 (defn make-action-signature [metadata & [args]]
   (str (:name metadata) (if-not (empty? args) (str " " (vec args)))))
 
-(defn action! [action-fn metadata & args]
-  (if-not (task/running?)
-    (go)
-    (let [name (str (:name metadata))
-          automation-action? (nil? (re-find #"^wait-" name))]
-      (log "action!" automation-action? name args)
-      (if automation-action?
-        (do
-          (test/record-action-execution!)
-          (transcript/reset-output-segment!))
-        (test/record-transcript-checkpoint!))
-      (cond
-        (:without-devtools-id metadata) (do
-                                          (if automation-action?
-                                            (append-to-transcript! (make-action-signature metadata args)))
-                                          (apply action-fn args))
-        (instance? DevToolsID (first args)) (let [devtools-id (first args)
-                                                  action-signature (make-action-signature metadata (rest args))]
-                                              (if automation-action?
-                                                (append-to-transcript! action-signature devtools-id))
-                                              (apply action-fn (:id (first args)) (rest args)))
-        :else (let [action-signature (make-action-signature metadata args)]
-                (assert *last-devtools-id* (str "action " name " requires prior :open-dirac-devtools call"))
-                (if automation-action?
-                  (append-to-transcript! action-signature *last-devtools-id*))
-                (apply action-fn *last-devtools-id* args))))))
+(defn do-action! [action-fn metadata & args]
+  (let [name (str (:name metadata))
+        automation-action? (nil? (re-find #"^wait-" name))]
+    (if (task/frozen?)
+      (do
+        (log "skipping action!" name " (task is fronzen due to failures)")
+        (go))
+      (do
+        (log "action!" automation-action? name args)
+        (if automation-action?
+          (do
+            (test/record-action-execution!)
+            (transcript/reset-output-segment!))
+          (test/record-transcript-checkpoint!))
+        (cond
+          (:without-devtools-id metadata) (do
+                                            (if automation-action?
+                                              (append-to-transcript! (make-action-signature metadata args)))
+                                            (apply action-fn args))
+          (instance? DevToolsID (first args)) (let [devtools-id (first args)
+                                                    action-signature (make-action-signature metadata (rest args))]
+                                                (if automation-action?
+                                                  (append-to-transcript! action-signature devtools-id))
+                                                (apply action-fn (:id (first args)) (rest args)))
+          :else (let [action-signature (make-action-signature metadata args)]
+                  (assert *last-devtools-id* (str "action " name " requires prior :open-dirac-devtools call"))
+                  (if automation-action?
+                    (append-to-transcript! action-signature *last-devtools-id*))
+                  (apply action-fn *last-devtools-id* args)))))))
+
+(defn action! [& args]
+  (go
+    ; this timeout is important for run-output-matching-loop! and other async operations
+    ; without this we could starve those loops and their reaction could be delayed
+    (<! (timeout 0))
+    (<! (apply do-action! args))))
