5e88664fe21034ccd2f695cdcf02ce0036533027
Antonin Hildebrand <antonin@hildebrand.cz>
2016-08-25 16:19:24 +0200

    devtools: allow toggling debug flags via url params

--- resources/unpacked/devtools/front_end/dirac/dirac.js
+++ resources/unpacked/devtools/front_end/dirac/dirac.js
@@ -19,18 +19,26 @@ Object.assign(window.dirac, (function() {
         "beautify-function-names"];
 
     function hasFeature(feature) {
-        var flag = featureFlags[feature];
+        const flag = featureFlags[feature];
         if (flag !== undefined) {
             return flag;
         }
-        var featureIndex = knownFeatureFlags.indexOf(feature);
+        const featureIndex = knownFeatureFlags.indexOf(feature);
         if (featureIndex === -1) {
             return true;
         }
-        var activeFlags = Runtime.queryParam("dirac_flags") || "";
-        var result = activeFlags[featureIndex] !== '0';
+        const activeFlags = Runtime.queryParam("dirac_flags") || "";
+        const result = activeFlags[featureIndex] !== '0';
         featureFlags[feature] = result;
         return result;
+    }
+
+    function hasDebugFlag(flagName) {
+        if (Runtime.queryParam("debug_all") == "1") {
+            return true;
+        }
+        const paramName = "debug_" + flagName.toLowerCase();
+        return Runtime.queryParam(paramName) == "1";
     }
 
 // taken from https://github.com/joliss/js-string-escape/blob/master/index.js
@@ -242,14 +250,14 @@ Object.assign(window.dirac, (function() {
 
     // don't forget to update externs.js too
     return {
-        _DEBUG_EVAL: false,
-        _DEBUG_COMPLETIONS: false,
-        _DEBUG_KEYSIM: false,
-        _DEBUG_FEEDBACK: false,
-        _DEBUG_WATCHING: false,
-        _DEBUG_CACHES: false,
-        _DEBUG_BACKEND_API: false,
-        _DEBUG_BACKEND_CSS: false,
+        _DEBUG_EVAL: hasDebugFlag("eval"),
+        _DEBUG_COMPLETIONS: hasDebugFlag("completions"),
+        _DEBUG_KEYSIM: hasDebugFlag("keysim"),
+        _DEBUG_FEEDBACK: hasDebugFlag("feedback"),
+        _DEBUG_WATCHING: hasDebugFlag("watching"),
+        _DEBUG_CACHES: hasDebugFlag("caches"),
+        _DEBUG_BACKEND_API: hasDebugFlag("backend_api"),
+        _DEBUG_BACKEND_CSS: hasDebugFlag("backend_css"),
 
         // -- feature toggles -----------------------------------------------------------------------------------------------
         hasREPL: hasFeature("enable-repl"),
