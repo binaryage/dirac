41f057cf1bad915b6678b1dfcfffa4160eb9f469
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-07 23:03:34 +0200

    runtime: fix preload namespace to include dirac.runtime
    dirac.runtime namespace is what Dirac checks to be present. Using
    just preloads approach without manually requiring dirac.runtime
    does not work in v0.6.5 release.

--- src/runtime/dirac/runtime/preload.cljs
+++ src/runtime/dirac/runtime/preload.cljs
@@ -1,14 +1,14 @@
 (ns dirac.runtime.preload
   (:require-macros [dirac.runtime.preload :refer [gen-config]])
-  (:require [dirac.runtime.prefs :as prefs]
-            [dirac.runtime.core :as core]))
+  (:require [dirac.runtime :as dirac]))
 
 ; this namespace is intended to be included in cljs compiler :preloads
 ; overrides for default configuration can be specified in :external-config > :dirac.runtime/config
+; see https://github.com/binaryage/dirac/blob/master/docs/configuration.md#dirac-runtime---page-specific-configuration
 
 (def config (gen-config))
 
-(prefs/merge-prefs! config)
+(dirac/set-prefs! (merge (dirac/get-prefs) config))
 
-(if-not (prefs/pref :suppress-preload-install)
-  (core/install!))
+(if-not (dirac/get-pref :suppress-preload-install)
+  (dirac/install!))
