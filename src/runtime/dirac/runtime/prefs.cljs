fe637b44b62a7b8de036582e777e3328ac434fbe
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-28 21:34:27 +0200

    runtime: move misplaced :nrepl-config pref

--- src/runtime/dirac/runtime/prefs.cljs
+++ src/runtime/dirac/runtime/prefs.cljs
@@ -28,9 +28,10 @@
    :eval-time-limit                              10000
    :java-trace-header-style                      "color:red"
    :runtime-tag                                  "unidentified"
+   :nrepl-config                                 {:preferred-compiler "dirac/new"}                                            ; see https://github.com/binaryage/dirac/blob/master/src/nrepl/dirac/nrepl/config.clj
    :silence-use-of-undeclared-var-warnings       true
    :silence-no-such-namespace-warnings           true})
-   :nrepl-config                                 {:preferred-compiler "dirac/new"}                                            ; see https://github.com/binaryage/dirac/blob/master/src/nrepl/dirac/nrepl/config.clj
+
 
 (def static-prefs (gen-static-prefs))                                                                                         ; this config is comming from environment and system properties
 
