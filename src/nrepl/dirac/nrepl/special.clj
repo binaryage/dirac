c714e35c9c6bbac7d19931ed0de33fe5c1ee673d
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-02 18:36:57 +0200

    nrepl: refactor dirac.nrepl.special

--- src/nrepl/dirac/nrepl/special.clj
+++ src/nrepl/dirac/nrepl/special.clj
@@ -1,13 +1,14 @@
 (ns dirac.nrepl.special
-  (:require [clojure.tools.nrepl.middleware.interruptible-eval :as nrepl-ieval]
-            [clojure.main]
+  (:require [clojure.main :refer [root-cause repl-caught]]
             [clojure.tools.logging :as log]
             [clojure.string :as string]
-            [dirac.nrepl.state :as state]
+            [dirac.nrepl.controls :as controls]
             [dirac.nrepl.sessions :as sessions]
             [dirac.nrepl.helpers :as helpers]
-            [dirac.nrepl.controls :as controls]
-            [dirac.nrepl.transports.output-capturing :refer [make-nrepl-message-with-captured-output]]))
+            [dirac.nrepl.transports.output-capturing :refer [make-nrepl-message-with-captured-output]]
+            [dirac.nrepl.state :as state]
+            [clojure.tools.nrepl.middleware.interruptible-eval :as nrepl-ieval])
+  (:import (clojure.lang Namespace)))
 
 ; -- handlers for middleware operations -------------------------------------------------------------------------------------
 
@@ -16,45 +17,47 @@
     (if (string? code)
       (some? (re-find #"^\(?dirac!" code)))))                                                                                 ; we don't want to use read-string here, regexp test should be safe and quick
 
-(defn special-repl-eval! [nrepl-message code ns]
-  (let [{:keys [session]} nrepl-message]
-    (let [result (with-bindings @session
-                   (try
-                     (let [form (read-string code)]
-                       (binding [state/*reply!* #(helpers/send-response! nrepl-message %)
-                                 *ns* ns
-                                 nrepl-ieval/*msg* nrepl-message]
-                         (eval form)))
-                     (catch Throwable e
-                       (let [root-ex (clojure.main/root-cause e)
-                             details (helpers/get-exception-details nrepl-message e)]
-                         (log/error (str "Clojure eval error during eval of a special dirac command: " details))
-                         ; repl-caught will produce :err message, but we are not under driver, so it won't be converted to :print-output
-                         ; that is why we present error output to user REPL manually
-                         (clojure.main/repl-caught e)
-                         (helpers/send-response! nrepl-message {:op      :print-output
-                                                                :kind    :java-trace
-                                                                :content (helpers/capture-exception-details e)})
-                         (helpers/send-response! nrepl-message {:status  :eval-error
-                                                                :ex      (-> e class str)
-                                                                :root-ex (-> root-ex class str)
-                                                                :details details})
-                         ::exception))
-                     (finally
-                       (.flush *out*)
-                       (.flush *err*))))
-          base-reply {:status :done}
-          reply (if (= ::controls/no-result ::exception result)
-                  base-reply
-                  (assoc base-reply
-                    :value (helpers/safe-pr-str result)
-                    :printed-value 1))]
-      (if-not (= ::exception result)
-        (helpers/send-response! nrepl-message reply)))))
+(defn special-repl-eval! [nrepl-message code-str ns]
+  {:pre [(string? code-str)
+         (instance? Namespace ns)]}
+  (log/debug "special-repl-eval!" code-str "in" ns)
+  (let [result (with-bindings @(:session nrepl-message)
+                 (try
+                   (binding [*ns* ns
+                             state/*reply!* #(helpers/send-response! nrepl-message %)
+                             nrepl-ieval/*msg* nrepl-message]
+                     (eval (read-string code-str)))
+                   (catch Throwable e
+                     (let [root-ex (root-cause e)
+                           details (helpers/get-exception-details nrepl-message e)]
+                       (log/error (str "Clojure error during eval of a special dirac command: " details))
+                       ; repl-caught will produce :err message, but we are not under driver,
+                       ; so it won't be converted to :print-output
+                       ; that is why we present error output to user REPL manually
+                       (repl-caught e)
+                       (helpers/send-response! nrepl-message {:op      :print-output
+                                                              :kind    :java-trace
+                                                              :content (helpers/capture-exception-details e)})
+                       (helpers/send-response! nrepl-message {:status  :eval-error
+                                                              :ex      (-> e class str)
+                                                              :root-ex (-> root-ex class str)
+                                                              :details details})
+                       ::exception))
+                   (finally
+                     (.flush *out*)
+                     (.flush *err*))))
+        reply (cond
+                (= ::exception result) nil
+                (= ::controls/no-result result) {:status :done}
+                :else {:status        :done
+                       :value         (helpers/safe-pr-str result)
+                       :printed-value 1})]
+    (if (some? reply)
+      (helpers/send-response! nrepl-message reply))))
 
-(defn sanitize-dirac-command [code]
+(defn sanitize-dirac-command [code-str]
   ; this is just for convenience, we convert some common forms to canonical (dirac! :help) form
-  (let [trimmed-code (string/trim code)]
+  (let [trimmed-code (string/trim code-str)]
     (if (or (= trimmed-code "dirac!")
             (= trimmed-code "(dirac!)"))
       "(dirac! :help)"
