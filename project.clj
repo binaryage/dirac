f6a9ed788fca28140ef543b7a884da5b20a10c75
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-22 22:01:40 +0200

    bump dirac version to 0.7.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/dirac "0.7.2"
+(defproject binaryage/dirac "0.7.3"
   :description "Dirac DevTools - a Chrome DevTools fork for ClojureScript developers."
   :url "https://github.com/binaryage/dirac"
   :license {:name         "MIT License"
