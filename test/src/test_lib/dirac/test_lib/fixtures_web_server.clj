8ad63ea1161664c672b2aa5bbe8da4597d612589
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-28 11:44:59 +0200

    Revert "tests: maybe ring.middleware.reload is causing the troubles?"
    8637d50dbf287d132d52671cfada5e5ac430f1a0

--- test/src/test_lib/dirac/test_lib/fixtures_web_server.clj
+++ test/src/test_lib/dirac/test_lib/fixtures_web_server.clj
@@ -4,7 +4,8 @@
             [clojure.string :as string])
   (:use ring.middleware.resource
         ring.middleware.content-type
-        ring.middleware.not-modified)
+        ring.middleware.not-modified
+        ring.middleware.reload)
   (:import (java.io IOException)))
 
 (def default-options
@@ -26,7 +27,7 @@
   (require 'ring.adapter.jetty)
   (let [run-jetty (resolve 'ring.adapter.jetty/run-jetty)]
     (log/info "starting fixtures web server at" (get-fixtures-server-url))
-    (run-jetty (get-fixtures-server) (merge default-options options))))
+    (run-jetty (wrap-reload (get-fixtures-server)) (merge default-options options))))
 
 (defn stop-fixtures-web-server [server]
   (try
