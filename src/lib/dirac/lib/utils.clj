5bdc205c42749e5975d9d82abdeab63749f4c02c
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-29 23:59:56 +0200

    lib: add env-config utils

--- src/lib/dirac/lib/utils.clj
+++ src/lib/dirac/lib/utils.clj
@@ -2,7 +2,7 @@
   (:require [clojure.core.async :refer [chan <!! <! >!! put! alts!! timeout close! go go-loop]]
             [clojure.pprint :refer [pprint]]
             [clojure.string :as string]
-            [environ.core :as environ]))
+            [env-config.core :as env-config]))
 
 (defn get-nrepl-server-url [host port]
   (str "nrepl://" host ":" port))
@@ -25,20 +25,13 @@
                          (seq? thing) (str (first thing))
                          :else (str thing)))))
 
-(defn env-val [key & [type]]
-  (if-let [val (environ/env key)]
-    (case type
-      :bool (or (= val "1")
-                (= (string/lower-case val) "true")
-                (= (string/lower-case val) "yes"))
-      :int (Integer/parseInt val)
-      (str val))))
+(defn get-env-vars []
+  (-> {}
+      (into (System/getenv))
+      (into (System/getProperties))))
 
-
-(defn assoc-env-val [config ks key & [type]]
-  (if-let [val (env-val key type)]
-    (assoc-in config ks val)
-    config))
+(defn read-env-config [prefix]
+  (env-config/make-config-with-logging prefix (get-env-vars)))
 
 (defn deep-merge-ignoring-nils
   "Recursively merges maps. If keys are not maps, the last value wins. Nils are ignored."
