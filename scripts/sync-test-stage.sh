6d0450233e0623e554efc90d34436c09f8621706
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-30 13:41:21 +0100

    scripts: rsync use --info=progress2 and test rsync version

--- scripts/sync-test-stage.sh
+++ scripts/sync-test-stage.sh
@@ -8,7 +8,21 @@ source "./config.sh"
 pushd "$ROOT"
 
 mkdir -p "$DIRAC_TEST_STAGE_DIR"
-rsync -a -v --delete --exclude-from="$DIRAC_TEST_STAGE_RSYNC_EXCLUDE_FILE" "$ROOT/" "$DIRAC_TEST_STAGE_DIR"
+
+RSYNC_VERSION=`rsync --version | head -n 1 | cut -d " " -f 4`
+
+set +e
+vercomp "$RSYNC_VERSION" "3.1.1"
+if [ $? != 1 ]; then
+  echo "rsync version 3.1.2+ needed, your version $RSYNC_VERSION"
+  echo "under mac use 'brew install homebrew/dupes/rsync' to get the latest rsync"
+  exit 1
+fi
+
+set -e
+
+echo "syncing test stage in $DIRAC_TEST_STAGE_DIR"
+rsync -a --info=progress2 --delete --exclude-from="$DIRAC_TEST_STAGE_RSYNC_EXCLUDE_FILE" "$ROOT/" "$DIRAC_TEST_STAGE_DIR"
 
 popd
 
