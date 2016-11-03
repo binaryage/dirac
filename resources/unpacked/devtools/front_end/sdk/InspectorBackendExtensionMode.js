1d810121353371d85442c1993fed8f1596177d16
Antonin Hildebrand <antonin@hildebrand.cz>
2016-11-03 01:14:00 +0100

    devtools: more cleanup

--- resources/unpacked/devtools/front_end/sdk/InspectorBackendExtensionMode.js
+++ resources/unpacked/devtools/front_end/sdk/InspectorBackendExtensionMode.js
@@ -1,44 +1,44 @@
 WebInspector.InspectorBackendExtensionMode = {};
 
 WebInspector.InspectorBackendExtensionMode.loadFromExtensionIfNeeded = function() {
-    if (InspectorBackend.isInitialized()) {
-        return;
-    }
+  if (InspectorBackend.isInitialized()) {
+    return;
+  }
 
-    const evalAPI = (lines) => {
-        let lineNum = 0;
-        for (let line of lines) {
-            lineNum += 1;
-            if (dirac._DEBUG_BACKEND_API) {
-                console.log("BackendAPI: (eval) ", line);
-            }
-            try {
-                eval(line);
-            } catch (e) {
-                throw new Error("BackendAPI: unable to eval API at line #" + lineNum + ":\n" + "  " + line + "\n" + e);
-            }
-        }
-    };
-
-    const backendAPI = Runtime.queryParam("backend_api");
-    if (backendAPI) {
-        const decodedBackendAPI = decodeURIComponent(backendAPI);
-        const lines = decodedBackendAPI.split("\n").filter(s => s.length);
-        WebInspector.BakedInspectorBackendMode = "external";
-        WebInspector.BakedInspectorBackendModeInfo = lines.length;
-        if (dirac._DEBUG_BACKEND_API) {
-            console.log("BackendAPI: backend_api url parameter present (" + WebInspector.BakedInspectorBackendModeInfo + ").");
-        }
-        evalAPI(lines);
-    } else {
-        const lines = WebInspector.BakedInspectorBackendAPI.split("\n").filter(s => s.length);
-        WebInspector.BakedInspectorBackendMode = "internal";
-        WebInspector.BakedInspectorBackendModeInfo = lines.length;
-        if (dirac._DEBUG_BACKEND_API) {
-            console.log("BackendAPI: backend_api url parameter not present. Using pre-baked backend API (" + WebInspector.BakedInspectorBackendModeInfo + ").");
-        }
-        evalAPI(lines);
+  const evalAPI = (lines) => {
+    let lineNum = 0;
+    for (let line of lines) {
+      lineNum += 1;
+      if (dirac._DEBUG_BACKEND_API) {
+        console.log("BackendAPI: (eval) ", line);
+      }
+      try {
+        eval(line);
+      } catch (e) {
+        throw new Error("BackendAPI: unable to eval API at line #" + lineNum + ":\n" + "  " + line + "\n" + e);
+      }
     }
+  };
+
+  const backendAPI = Runtime.queryParam("backend_api");
+  if (backendAPI) {
+    const decodedBackendAPI = decodeURIComponent(backendAPI);
+    const lines = decodedBackendAPI.split("\n").filter(s => s.length);
+    WebInspector.BakedInspectorBackendMode = "external";
+    WebInspector.BakedInspectorBackendModeInfo = lines.length;
+    if (dirac._DEBUG_BACKEND_API) {
+      console.log("BackendAPI: backend_api url parameter present (" + WebInspector.BakedInspectorBackendModeInfo + ").");
+    }
+    evalAPI(lines);
+  } else {
+    const lines = WebInspector.BakedInspectorBackendAPI.split("\n").filter(s => s.length);
+    WebInspector.BakedInspectorBackendMode = "internal";
+    WebInspector.BakedInspectorBackendModeInfo = lines.length;
+    if (dirac._DEBUG_BACKEND_API) {
+      console.log("BackendAPI: backend_api url parameter not present. Using pre-baked backend API (" + WebInspector.BakedInspectorBackendModeInfo + ").");
+    }
+    evalAPI(lines);
+  }
 };
 
 WebInspector.InspectorBackendExtensionMode.loadFromExtensionIfNeeded();
--- resources/unpacked/devtools/front_end/sdk/SupportedCSSPropertiesExtensionMode.js
+++ resources/unpacked/devtools/front_end/sdk/SupportedCSSPropertiesExtensionMode.js
@@ -1,34 +1,34 @@
 WebInspector.SupportedCSSPropertiesExtensionMode = {};
 
 WebInspector.SupportedCSSPropertiesExtensionMode.loadFromExtensionIfNeeded = function() {
-    const evalCSS = (definitions) => {
-        try {
-            eval("WebInspector.CSSMetadata._generatedProperties = " + definitions);
-        } catch (e) {
-            throw new Error("BackendCSS: unable to eval CSS" + e);
-        }
-    };
-
-    const encodedBackendCSS = Runtime.queryParam("backend_css");
-    if (encodedBackendCSS) {
-        const backendCSS = decodeURIComponent(encodedBackendCSS);
-        const lines = backendCSS.split("\n").filter(s => s.length);
-        WebInspector.BakedSupportedCSSPropertiesMode = "external";
-        WebInspector.BakedSupportedCSSPropertiesModeInfo = lines.length;
-        if (dirac._DEBUG_BACKEND_CSS) {
-            console.log("BackendCSS: backend_css url parameter present (" + WebInspector.BakedSupportedCSSPropertiesModeInfo + ").");
-        }
-        evalCSS(backendCSS);
-    } else {
-        const backendCSS = WebInspector.BakedSupportedCSSProperties;
-        const lines = backendCSS.split("\n").filter(s => s.length);
-        WebInspector.BakedSupportedCSSPropertiesMode = "internal";
-        WebInspector.BakedSupportedCSSPropertiesModeInfo = lines.length;
-        if (dirac._DEBUG_BACKEND_CSS) {
-            console.log("BackendCSS: backend_css url parameter not present. Using pre-baked backend CSS (" + WebInspector.BakedSupportedCSSPropertiesModeInfo + ").");
-        }
-        evalCSS(backendCSS);
+  const evalCSS = (definitions) => {
+    try {
+      eval("WebInspector.CSSMetadata._generatedProperties = " + definitions);
+    } catch (e) {
+      throw new Error("BackendCSS: unable to eval CSS" + e);
     }
+  };
+
+  const encodedBackendCSS = Runtime.queryParam("backend_css");
+  if (encodedBackendCSS) {
+    const backendCSS = decodeURIComponent(encodedBackendCSS);
+    const lines = backendCSS.split("\n").filter(s => s.length);
+    WebInspector.BakedSupportedCSSPropertiesMode = "external";
+    WebInspector.BakedSupportedCSSPropertiesModeInfo = lines.length;
+    if (dirac._DEBUG_BACKEND_CSS) {
+      console.log("BackendCSS: backend_css url parameter present (" + WebInspector.BakedSupportedCSSPropertiesModeInfo + ").");
+    }
+    evalCSS(backendCSS);
+  } else {
+    const backendCSS = WebInspector.BakedSupportedCSSProperties;
+    const lines = backendCSS.split("\n").filter(s => s.length);
+    WebInspector.BakedSupportedCSSPropertiesMode = "internal";
+    WebInspector.BakedSupportedCSSPropertiesModeInfo = lines.length;
+    if (dirac._DEBUG_BACKEND_CSS) {
+      console.log("BackendCSS: backend_css url parameter not present. Using pre-baked backend CSS (" + WebInspector.BakedSupportedCSSPropertiesModeInfo + ").");
+    }
+    evalCSS(backendCSS);
+  }
 };
 
 WebInspector.SupportedCSSPropertiesExtensionMode.loadFromExtensionIfNeeded();
