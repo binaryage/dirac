07f7af12f1c7f88d873a7dd61b15a6b197c28a25
Antonin Hildebrand <antonin@hildebrand.cz>
2016-11-10 00:04:15 +0100

    devtools: comment out legacy code in addExtensions
    we were seeing "Uncaught TypeError: Cannot read property 'addExtensions' of undefined", source: chrome-devtools://devtools/bundled/devtools_compatibility.js (67)
    
    but I don't think this will help, devtools_compatibility is included from bundled (internal) devtools

--- resources/unpacked/devtools/front_end/devtools_compatibility.js
+++ resources/unpacked/devtools/front_end/devtools_compatibility.js
@@ -64,9 +64,9 @@
      */
     addExtensions(extensions) {
       // Support for legacy front-ends (<M41).
-      if (window['WebInspector']['addExtensions'])
-        window['WebInspector']['addExtensions'](extensions);
-      else
+      // if (window['WebInspector']['addExtensions'])
+      //   window['WebInspector']['addExtensions'](extensions);
+      // else
         this._dispatchOnInspectorFrontendAPI('addExtensions', [extensions]);
     }
 
