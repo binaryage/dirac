ca0039240f45f81f4a335a87992bbfd47375243d
Antonin Hildebrand <antonin@hildebrand.cz>
2016-11-10 10:50:48 +0100

    shared: improve cannot-attach-dirac error message

--- src/shared/dirac/i18n.cljs
+++ src/shared/dirac/i18n.cljs
@@ -1,12 +1,17 @@
 (ns dirac.i18n)
 
+(def cannot-attach-help-url
+  "https://github.com/binaryage/dirac/blob/master/docs/faq.md#getting-error-cannot-attach-dirac-devtools-what-now")
+
 (defn unable-to-create-dirac-tab []
   "Unable to create a new tab for Dirac DevTools.")
 
-(defn cannot-attach-dirac [target-url tab-url]
+(defn cannot-attach-dirac [debugger-url tab-url]
   (str "Cannot attach Dirac DevTools. "
-       "Likely cause: another instance of DevTools is already attached. "
-       "target-url=" target-url ", tab-url=" tab-url))
+       "Likely cause: another instance of DevTools is already attached.\n"
+       "Don't you have internal DevTools open in the tab?\n"
+       "See " cannot-attach-help-url ".\n"
+       "tab-url=" tab-url ", debugger-url=" debugger-url))
 
 (defn unable-to-resolve-backend-url [target-url tab-url]
   (str "Unable to resolve backend-url for Dirac DevTools. "
