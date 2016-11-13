14baef016649a13f4e6fec1915f959f1685b465d
Antonin Hildebrand <antonin@hildebrand.cz>
2016-11-14 00:06:38 +0100

    script: add health-check scripts

---
+++ scripts/test-health-check-job.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+launchctl start com.binaryage.dirac-health-check.plist
+
+# note: you may want to
+#
+#   sudo launchctl log level debug
+#   sudo tail -f /var/log/system.log
+# 
+# => http://serverfault.com/questions/249199/manually-start-scheduled-launchd-job
