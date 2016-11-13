8d581df995fa84308229566abd183442735127a4
Antonin Hildebrand <antonin@hildebrand.cz>
2016-11-13 21:38:33 +0100

    tests: do not call window.location.reload immediately
    could this prevent "Cannot find context with specified id" errors
    on travis machine?

--- test/browser/fixtures/src/scenarios01/dirac/tests/scenarios/repl.cljs
+++ test/browser/fixtures/src/scenarios01/dirac/tests/scenarios/repl.cljs
@@ -1,10 +1,17 @@
 (ns dirac.tests.scenarios.repl
-  (:require [dirac.automation.runtime :refer [init-runtime!]]
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.core.async :refer [timeout]]
+            [dirac.automation.runtime :refer [init-runtime!]]
             [dirac.tests.scenarios.repl.workspace :as workspace]
             [dirac.automation.scenario :as scenario]))
 
+(defn safe-reload! []
+  (go
+    (<! (timeout 1000))                                                                                                       ; revent "Cannot find context with specified id" V8 errors ?
+    (js/window.location.reload)))
+
 (init-runtime!)
-(scenario/register-trigger! :reload #(js/window.location.reload))
+(scenario/register-trigger! :reload safe-reload!)
 (scenario/register-trigger! :navigate #(set! js/window.location.pathname %))
 (scenario/register-trigger! :eval-cljs #(js/dirac.runtime.repl.request-eval-cljs %))
 (scenario/register-trigger! :eval-js #(js/dirac.runtime.repl.request-eval-js %))
