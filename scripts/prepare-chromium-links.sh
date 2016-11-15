3b2f46fce67cfe4f5b9b722b1d35a9db1cbf2732
Antonin Hildebrand <antonin@hildebrand.cz>
2016-11-15 10:29:41 +0100

    scripts: fix prepare-chromium-links.sh (close #51)

--- scripts/prepare-chromium-links.sh
+++ scripts/prepare-chromium-links.sh
@@ -11,7 +11,7 @@ VERSION=$1
 
 if [ ! -z "$VERSION" ] ; then
   echo "looking up position for version $VERSION..."
-  POSITION=`curl -s "https://omahaproxy.appspot.com/deps.json?version=55.0.2882.4"| python -mjson.tool | grep chromium_base_position | cut -d ":" -f 2 | sed "s/[ ,\"]//g"`
+  POSITION=`curl -s "https://omahaproxy.appspot.com/deps.json?version=$VERSION"| python -mjson.tool | grep chromium_base_position | cut -d ":" -f 2 | sed "s/[ ,\"]//g"`
   echo " => $POSITION"
 else
   echo "looking up latest versions..."
