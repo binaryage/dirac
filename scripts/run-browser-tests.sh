30740092dc47f54208ef4cf02b967acaeecef951
Antonin Hildebrand <antonin@hildebrand.cz>
2016-12-02 18:18:44 +0100

    scripts: instruct grep to do line buffering

--- scripts/run-browser-tests.sh
+++ scripts/run-browser-tests.sh
@@ -8,7 +8,7 @@ source "./config.sh"
 pushd "$ROOT"
 
 echo "Running browser tests..."
-lein with-profile +test-runner trampoline run -m "$@" 2>&1 | grep -vE "(org\.openqa\.selenium\.remote\.ProtocolHandshake|assuming Postel|INFO: Detected dialect)"
+lein with-profile +test-runner trampoline run -m "$@" 2>&1 | grep --line-buffered -vE "(org\.openqa\.selenium\.remote\.ProtocolHandshake|assuming Postel|INFO: Detected dialect)"
 
 popd
 
