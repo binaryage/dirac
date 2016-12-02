f97fa10c8a527e37f784f805f2ec9d053eb05bd7
Antonin Hildebrand <antonin@hildebrand.cz>
2016-12-02 17:31:25 +0100

    scripts: grep out some selenium trash logs

--- scripts/run-browser-tests.sh
+++ scripts/run-browser-tests.sh
@@ -8,7 +8,7 @@ source "./config.sh"
 pushd "$ROOT"
 
 echo "Running browser tests..."
-lein with-profile +test-runner run -m "$@"
+lein with-profile +test-runner run -m "$@" 2>&1 | grep -vE "(org\.openqa\.selenium\.remote\.ProtocolHandshake|assuming Postel|INFO: Detected dialect)"
 
 popd
 
