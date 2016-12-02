binaryage/dirac

commit 79d8176a33cddd981ff341056494a2b88ab9bebe

shared: fix that nasty options model race
this bug was causing random failures in travis because some options
like debugger port were not properly set and were overwritten back to
original values due to races.

src/shared/dirac/options/model.cljs
@@ -88,27 +88,29 @@
   (binding [*auto-sync* false]
     (reset! cached-options options)))
 
+(defn parse-options [serialized-options]
+  (let [options (unserialize-options serialized-options)]
+    (merge default-options options)))                                                                                         ; merge is important for upgrading options schema between versions
+
 (defn read-options []
   (go
     (let [local-storage (storage/get-local)
           [[items] _error] (<! (get local-storage "options"))
-          serialized-options (oget items "options")
-          unserialized-options (unserialize-options serialized-options)
-          options (merge default-options unserialized-options)]
+          options (parse-options (oget items "options"))]
       (info "read options:" options)
       options)))
 
-(defn reload-options! []
+(defn reload-options! [serialized-options]
   {:pre [*initialized*]}
-  (go
-    (let [options (<! (read-options))]
-      (reset-cached-options-without-sync! options))))
+  (let [options (parse-options serialize-options)]
+    (info "reload options:" options)
+    (reset-cached-options-without-sync! options)))
 
 ; -- events -----------------------------------------------------------------------------------------------------------------
 
-(defn process-on-changed! [_changes area-name]
+(defn process-on-changed! [changes area-name]
   (when (= area-name "local")
-    (reload-options!)))
+    (reload-options! (oget changes "newValue"))))
 
 (defn process-chrome-event [event]
   (log "got chrome event" event)
@@ -133,7 +135,7 @@
   (go
     (let [options (<! (read-options))]
       (set! *initialized* true)
-      (reset-cached-options-without-sync! (merge default-options options))                                                    ; merge is important for upgrading options schema
+      (reset-cached-options-without-sync! options)
       (add-watch cached-options ::watch (fn [_ _ _ new-state]
                                           (on-cached-options-change! new-state)))
       (run-chrome-event-loop! chrome-event-channel)

commit e0a54829bfe57a17452ac104872a87240a5264da

shared: fix options model broken in 79d8176a33cddd981ff341056494a2b88ab9bebe

src/shared/dirac/options/model.cljs
@@ -66,9 +66,12 @@
     (.stringify js/JSON json)))
 
 (defn unserialize-options [serialized-options]
-  (if (string? serialized-options)
-    (let [json (.parse js/JSON serialized-options)]
-      (js->clj json :keywordize-keys true))))
+  (assert (or (string? serialized-options) (object? serialized-options))
+          (str "unexpected serialized-options of type " (type serialized-options) ": " (pr-str serialized-options)))
+  (let [json (if (string? serialized-options)
+               (.parse js/JSON serialized-options)
+               serialized-options)]
+    (js->clj json :keywordize-keys true)))
 
 ; -- read/write -------------------------------------------------------------------------------------------------------------
 
@@ -102,15 +105,16 @@
 
 (defn reload-options! [serialized-options]
   {:pre [*initialized*]}
-  (let [options (parse-options serialize-options)]
+  (let [options (parse-options serialized-options)]
     (info "reload options:" options)
     (reset-cached-options-without-sync! options)))
 
 ; -- events -----------------------------------------------------------------------------------------------------------------
 
 (defn process-on-changed! [changes area-name]
-  (when (= area-name "local")
-    (reload-options! (oget changes "newValue"))))
+  (go
+    (when (= area-name "local")
+      (reload-options! (oget changes "options.newValue")))))
 
 (defn process-chrome-event [event]
   (log "got chrome event" event)