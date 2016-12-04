738b7c34b21b06660660b6b65c26165c9c1e4655
Antonin Hildebrand <antonin@hildebrand.cz>
2016-12-04 18:25:14 +0100

    scripts: move plists into a subdirectory

--- scripts/install-health-checking-agent.sh
+++ scripts/install-health-checking-agent.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
+false && source _config.sh # never executes, this is here just for IntelliJ Bash support to understand our sourcing
+
+pushd "$SCRIPTS"
+
 AGENTS_DIR=~/Library/LaunchAgents
 PLIST_NAME=com.binaryage.dirac-health-check.plist
+PLIST_SOURCE="plists/$PLIST_NAME"
 PLIST_PATH="$AGENTS_DIR/$PLIST_NAME"
 
-cp "$PLIST_NAME" "$AGENTS_DIR"
+cp "$PLIST_SOURCE" "$AGENTS_DIR"
 launchctl unload "$PLIST_PATH"
 launchctl load -w "$PLIST_PATH"
+
+popd
