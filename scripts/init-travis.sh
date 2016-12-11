binaryage/dirac

commit 957879ef2f466bb6cb9c6a718283447d5bc90bab

travis: prefer '-z' test instead of '! -v'

scripts/init-travis.sh
@@ -12,7 +12,7 @@ false && source _config.sh # never executes, this is here just for IntelliJ Bash
 export DIRAC_CHROME_DRIVER_VERBOSE=1
 export LEIN_FAST_TRAMPOLINE=1
 
-if [[ ! -v TRAVIS_SKIP_LEIN_UPGRADE ]]; then
+if [[ -z "$TRAVIS_SKIP_LEIN_UPGRADE" ]]; then
   # we need lein 2.5.3+ because of https://github.com/technomancy/leiningen/issues/1762
   # update lein to latest, https://github.com/technomancy/leiningen/issues/2014#issuecomment-153829977
   set +e
@@ -21,24 +21,24 @@ if [[ ! -v TRAVIS_SKIP_LEIN_UPGRADE ]]; then
 fi
 
 # install xvfb (for chrome tests)
-if [[ ! -v TRAVIS_SKIP_XVFB_SETUP ]]; then
+if [[ -z "${TRAVIS_SKIP_XVFB_SETUP}" ]]; then
   export DISPLAY=:99.1
   sudo /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 1 1024x768x24
 fi
 
-if [[ ! -v TRAVIS_SKIP_COLORDIFF_INSTALL ]]; then
+if [[ -z "${TRAVIS_SKIP_COLORDIFF_INSTALL}" ]]; then
   sudo apt-get install -y colordiff
 fi
 
 # install latest chromium
-if [[ ! -v TRAVIS_SKIP_CHROMIUM_SETUP ]]; then
+if [[ -z "${TRAVIS_SKIP_CHROMIUM_SETUP}" ]]; then
   if cd chromium-latest-linux; then
     git pull
     cd ..
   else
     git clone --depth 1 https://github.com/scheib/chromium-latest-linux.git
   fi
-  if [[ ! -v TRAVIS_SKIP_CHROMIUM_UPDATE ]]; then
+  if [[ -z "${TRAVIS_SKIP_CHROMIUM_UPDATE}" ]]; then
     ./chromium-latest-linux/update.sh
   fi
 fi
@@ -46,10 +46,10 @@ export DIRAC_CHROME_BINARY_PATH=`pwd`/chromium-latest-linux/latest/chrome
 export CHROME_LOG_FILE=`pwd`
 
 # install chromedriver
-if [[ ! -v TRAVIS_SKIP_CHROMEDRIVER_UPDATE ]]; then
-  if [[ -v TRAVIS_DONT_CACHE_CHROMEDRIVER -o ! -f chromedriver ]]; then
-    if [[ -v TRAVIS_USE_CUSTOM_CHROMEDRIVER ]]; then
-      wget -O chromedriver.zip "$TRAVIS_USE_CUSTOM_CHROMEDRIVER" # http://x.binaryage.com/chromedriver.zip
+if [[ -z "${TRAVIS_SKIP_CHROMEDRIVER_UPDATE}" ]]; then
+  if [[ -z "${TRAVIS_DONT_CACHE_CHROMEDRIVER}" -o ! -f chromedriver ]]; then
+    if [[ -z "${TRAVIS_USE_CUSTOM_CHROMEDRIVER}" ]]; then
+      wget -O chromedriver.zip "${TRAVIS_USE_CUSTOM_CHROMEDRIVER}" # http://x.binaryage.com/chromedriver.zip
     else
       wget -O chromedriver.zip https://chromedriver.storage.googleapis.com/2.26/chromedriver_linux64.zip
     fi

commit c44418f6a098a606ea2ac1916a111fad875a36a1

travis: fix chromedriver conditions broken in 957879ef2

scripts/init-travis.sh
@@ -64,8 +64,8 @@ export CHROME_LOG_FILE=`pwd`
 
 # install chromedriver
 if [[ -z "${TRAVIS_SKIP_CHROMEDRIVER_UPDATE}" ]]; then
-  if [[ -z "${TRAVIS_DONT_CACHE_CHROMEDRIVER}" -o ! -f chromedriver ]]; then
-    if [[ -z "${TRAVIS_USE_CUSTOM_CHROMEDRIVER}" ]]; then
+  if [[ ! -z "${TRAVIS_DONT_CACHE_CHROMEDRIVER}" -o ! -f chromedriver ]]; then
+    if [[ ! -z "${TRAVIS_USE_CUSTOM_CHROMEDRIVER}" ]]; then
       wget -O chromedriver.zip "${TRAVIS_USE_CUSTOM_CHROMEDRIVER}" # http://x.binaryage.com/chromedriver.zip
     else
       wget -O chromedriver.zip https://chromedriver.storage.googleapis.com/2.26/chromedriver_linux64.zip