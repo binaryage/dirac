a2a7fd5bb7d9133e25640b8cb2d997a7a58cd397
Antonin Hildebrand <antonin@hildebrand.cz>
2016-12-12 02:47:21 +0100

    tests: fix with-travis-fold again

--- test/browser/src/browser_tests/dirac/tests/browser/tasks/travis.clj
+++ test/browser/src/browser_tests/dirac/tests/browser/tasks/travis.clj
@@ -7,14 +7,10 @@
   (str "travis_fold:" action ":" name "\r" ANSI_CLEAR))
 
 (defmacro with-travis-fold [title name & body]
-  (let [sanitized-name (cuerdas/kebab name)
-        fold-start (travis-fold-command "start" sanitized-name)
-        fold-end (travis-fold-command "end" sanitized-name)]
-    `(do
-       (print ~fold-start)
-       (println ~title)
-       (try
-         ~@body
-         (finally
-           (print ~fold-end)                                                                                                  ; for some reason print does not work without following println, I assume it is some java buffering magic
-           (println))))))
+  `(let [sanitized-name# (cuerdas/kebab ~name)]
+     (print (travis-fold-command "start" sanitized-name#))
+     (println ~title)
+     (try
+       ~@body
+       (finally
+         (print (travis-fold-command "end" sanitized-name#))))))
