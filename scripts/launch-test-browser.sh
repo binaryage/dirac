3ad267ac4c2da89909957f135c483ce8c3afa035
Antonin Hildebrand <antonin@hildebrand.cz>
2017-01-15 00:51:08 +0100

    scripts: chrome --disable-gpu causing troubles, remove it

--- scripts/launch-test-browser.sh
+++ scripts/launch-test-browser.sh
@@ -52,7 +52,6 @@ set -x
       --dom-automation \
       --full-memory-crash-report \
       --no-default-browser-check \
-      --disable-gpu \
       --disable-infobars \
       --disable-default-apps \
       --load-extension="$DEV_DIRAC_EXTENSION_PATH,$DEV_MARION_EXTENSION_PATH" \
