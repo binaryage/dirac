79ea0b6edfe90716e1cfb9e5b204d41893b434cc
Antonin Hildebrand <antonin@hildebrand.cz>
2017-01-22 19:46:47 +0100

    runtime: refactor tag.cljs

--- src/runtime/dirac/runtime/tag.cljs
+++ src/runtime/dirac/runtime/tag.cljs
@@ -1,33 +1,26 @@
 (ns dirac.runtime.tag
-  (:require-macros [dirac.runtime.tag :refer [get-current-browser-name get-current-platform-name]])
-  (:require [dirac.runtime.util :refer [get-js-context-description in-node-context? platform-str]]
-            [goog.labs.userAgent.browser :as ua-browser]
-            [goog.labs.userAgent.platform :as ua-platform]
+  (:require [dirac.runtime.util :refer [get-js-context-description in-node-context?
+                                        get-browser-platform-info get-browser-version-info]]
             [dirac.runtime.prefs :as prefs]))
+
+; -- helpers ----------------------------------------------------------------------------------------------------------------
+
+(defn- prepare-tag-line [items]
+  (apply str (interpose " | " items)))
 
 ; -- tag api ----------------------------------------------------------------------------------------------------------------
 
 (defn get-browser-tag-data []
-  (let [tag (prefs/pref :runtime-tag)
-        url (str js/location)
-        browser-name (get-current-browser-name)
-        browser-version (ua-browser/getVersion)
-        browser (platform-str browser-name "?" browser-version "?")
-        platform-name (get-current-platform-name)
-        platform-version (ua-platform/getVersion)
-        platform (platform-str platform-name "?" platform-version "?")]
-    {:tag      tag
-     :url      url
-     :browser  browser
-     :platform platform}))
+  {:tag      (prefs/pref :runtime-tag)
+   :url      (str js/location)
+   :browser  (get-browser-version-info)
+   :platform (get-browser-platform-info)})
 
 (defn get-node-tag-data []
-  (let [tag (prefs/pref :runtime-tag)
-        platform (get-js-context-description)]
-    {:tag      tag
-     :platform platform}))
+  {:tag      (prefs/pref :runtime-tag)
+   :platform (get-js-context-description)})
 
 (defn get-tag []
-  (apply str (interpose " | " (if (in-node-context?)
-                                ((juxt :tag :platform) (get-node-tag-data))
-                                ((juxt :tag :url :browser :platform) (get-browser-tag-data))))))
+  (prepare-tag-line (if (in-node-context?)
+                      ((juxt :tag :platform) (get-node-tag-data))
+                      ((juxt :tag :url :browser :platform) (get-browser-tag-data)))))
