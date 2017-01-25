a86617adaff8e5e77810ea2ee465704aa26ca61c
Antonin Hildebrand <antonin@hildebrand.cz>
2017-01-25 20:43:18 +0100

    runtime: remove dependency on cljs-oops
    we don't want to bring the dependency to dirac.runtime because cljs-oops
    depends on spec

--- project.clj
+++ project.clj
@@ -15,7 +15,6 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.nrepl "0.2.12"]
-                 [binaryage/oops "0.5.2"]
                  [binaryage/env-config "0.1.1"]
                  [http-kit "2.2.0"]
                  [version-clj "0.1.2"]
@@ -24,6 +23,7 @@
 
                  ; we cannot use :dependencies under individual profiles because Cursive recognizes only root level
                  ; thus we mark extra deps with :scope "test" and filter them later when producing jar library
+                 [binaryage/oops "0.5.2" :scope "test"]
                  [binaryage/chromex "0.5.2" :scope "test"]
                  [binaryage/devtools "0.9.0" :scope "test"]
                  [environ "1.1.0" :scope "test"]
---
+++ src/runtime/dirac/runtime/oops.clj
@@ -0,0 +1,34 @@
+(ns dirac.runtime.oops)
+
+; this is a poor man's version of cljs-oops, we don't want to bring dependency to dirac.runtime,
+; because cljs-oops depends on spec
+
+(defmacro ocall [o name & params]
+  `(let [o# ~o]
+     (.call (goog.object/get o# ~name) o# ~@params)))
+
+(defmacro oapply [o name param-coll]
+  `(let [o# ~o]
+     (.apply (goog.object/get o# ~name) o# (into-array ~param-coll))))
+
+(defmacro oget
+  ([o k1] `(goog.object/get ~o ~k1))
+  ([o k1 k2] `(when-let [o# (goog.object/get ~o ~k1)]
+                (goog.object/get o# ~k2)))
+  ([o k1 k2 & ks] `(when-let [o# (goog.object/get ~o ~k1)]
+                     (oget o# ~k2 ~@ks))))
+
+(defmacro oset [o ks val]
+  (let [keys (butlast ks)
+        obj-sym (gensym)]
+    `(let [~obj-sym ~o
+           target# ~(if (seq keys) `(devtools.util/oget ~obj-sym ~@keys) obj-sym)]
+       (assert target# (str "unable to locate object path " ~keys " in " ~obj-sym))
+       (goog.object/set target# (last ~ks) ~val)
+       ~obj-sym)))
+
+(defmacro safe-call [f exceptional-result & args]
+  `(try
+     (~f ~@args)
+     (catch :default e#
+       ~exceptional-result)))
