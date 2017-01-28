366586a9e5700b48392ef171dfee5b5075043087
Antonin Hildebrand <antonin@hildebrand.cz>
2017-01-28 20:08:54 +0100

    squash 'resources/unpacked/devtools' changes from ee9c554c9..16693753d
    16693753d DevTools: Type all properties in SuggestBox
    dfc4a9041 DevTools: replace the settings icon with gear.
    ed6ef78e5 [DevTools] Make all SDKModels take a single argument in constructor.
    2429f0c0b DevTools: Turn pie chart into a donut chart.
    d55399981 DevTools: Make checkbox labels clickable again
    8f72fab6b Revert of DevTools: Move radio button input element into Shadow DOM (patchset #1 id:1 of https://codereview.chromium.org/2650213005/ )
    0f63c02a8 DevTools: skip specific tests for backwards compatibility testing
    7a1881299 [DevTools] exposed promise creation frame in call stack sidebar
    2c0cd6aa9 DevTools: render console level filter as drop box.
    a7204bdfc [Devtools] Added padding to child nodes in network group experiment
    b80aa2aa6 DevTools: Update network overview with two level bars.
    2abe3dd78 DevTools: flip lighthouse binary to be a remote module.
    50ab93d9b DevTools: Make datagrid default layout compact again.
    2762b7471 Timeline: move filters to TimelineTreeView, where they're actually used
    3995c4013 [DevTools] Center console icons vertically.
    
    git-subtree-dir: resources/unpacked/devtools
    git-subtree-split: 16693753da616edb9af8dc014e074ec18266672f

--- front_end/sdk/DebuggerModel.js
+++ front_end/sdk/DebuggerModel.js
@@ -36,7 +36,7 @@ SDK.DebuggerModel = class extends SDK.SDKModel {
    * @param {!SDK.Target} target
    */
   constructor(target) {
-    super(SDK.DebuggerModel, target);
+    super(target);
 
     target.registerDebuggerDispatcher(new SDK.DebuggerDispatcher(this));
     this._agent = target.debuggerAgent();
@@ -84,9 +84,7 @@ SDK.DebuggerModel = class extends SDK.SDKModel {
    * @return {?SDK.DebuggerModel}
    */
   static fromTarget(target) {
-    if (!target || !target.hasJSCapability())
-      return null;
-    return target.model(SDK.DebuggerModel);
+    return target ? target.model(SDK.DebuggerModel) : null;
   }
 
   /**
@@ -837,6 +835,8 @@ SDK.DebuggerModel = class extends SDK.SDKModel {
   }
 };
 
+SDK.SDKModel.register(SDK.DebuggerModel, SDK.Target.Capability.JS);
+
 /** @typedef {{location: ?SDK.DebuggerModel.Location, functionName: string}} */
 SDK.DebuggerModel.FunctionDetails;
 
@@ -1355,7 +1355,7 @@ SDK.DebuggerPausedDetails = class extends SDK.SDKObject {
     while (stack) {
       if (stack.description === 'async function' && stack.callFrames.length)
         stack.callFrames.shift();
-      if (previous && !stack.callFrames.length)
+      if (previous && (!stack.callFrames.length && !stack.promiseCreationFrame))
         previous.parent = stack.parent;
       else
         previous = stack;
