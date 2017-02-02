5d3a6fbfd4abf460257daa665cecbba5d752dd71
Antonin Hildebrand <antonin@hildebrand.cz>
2017-02-02 18:23:02 +0100

    devtools: add support for handshake prior frontend initialization
    // The goal of handshake is to pass backend_api and backend_css from extension into frontend.
    // This info is needed early during frontend initialization, but we don't have sync apis to
    // communicate between extension and frontend so we have to pass this via url params.
    // Unfortunately, these parameters can be pretty long and this would obfuscate url params visible from
    // chrome://extensions and cause other usability issues (for example refreshing frontend via hitting enter in omnibox
    // would fail due to url truncation).
    //
    // We work around these issues by opening this handshake.html first and passing it these ugly url params.
    // Here we store the data into localStorage and let frontend read it later.
    // See Protocol.InspectorBackendExtensionMode and SDK.SupportedCSSPropertiesExtensionMode for further details.

---
+++ resources/unpacked/devtools/front_end/handshake.js
@@ -0,0 +1,22 @@
+// The goal of handshake is to pass backend_api and backend_css from extension into frontend.
+// This info is needed early during frontend initialization, but we don't have sync apis to
+// communicate between extension and frontend so we have to pass this via url params.
+// Unfortunately, these parameters can be pretty long and this would obfuscate url params visible from
+// chrome://extensions and cause other usability issues (for example refreshing frontend via hitting enter in omnibox
+// would fail).
+//
+// We work around these issues by opening this handshake.html first and passing it these ugly url params.
+// Here we store the data into localStorage and let frontend read it later.
+// See Protocol.InspectorBackendExtensionMode and SDK.SupportedCSSPropertiesExtensionMode for further details.
+
+const encodedBackendAPI = Runtime.queryParam("backend_api");
+if (encodedBackendAPI) {
+  const backendAPI = decodeURIComponent(encodedBackendAPI);
+  localStorage.setItem('dirac_backend_api', backendAPI);
+}
+
+const encodedBackendCSS = Runtime.queryParam("backend_css");
+if (encodedBackendCSS) {
+  const backendCSS = decodeURIComponent(encodedBackendCSS);
+  localStorage.setItem('dirac_backend_css', backendCSS);
+}
