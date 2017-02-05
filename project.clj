8493fc8b5b6e753636b7317fc54805007db30c51
Antonin Hildebrand <antonin@hildebrand.cz>
2017-02-05 17:40:04 +0100

    project: bump chromex library to 0.5.3

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@
                  ; we cannot use :dependencies under individual profiles because Cursive recognizes only root level
                  ; thus we mark extra deps with :scope "test" and filter them later when producing jar library
                  [binaryage/oops "0.5.2" :scope "test"]
-                 [binaryage/chromex "0.5.2" :scope "test"]
+                 [binaryage/chromex "0.5.3" :scope "test"]
                  [binaryage/devtools "0.9.0" :scope "test"]
                  [environ "1.1.0" :scope "test"]
                  [cljs-http "0.1.42" :scope "test"]
