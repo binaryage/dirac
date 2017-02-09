cad8df328d6f6b070cd6dec5fe039a77260c0d18
Antonin Hildebrand <antonin@hildebrand.cz>
2017-02-09 22:24:23 +0100

    project: lein trampolines not worth the trouble

--- project.clj
+++ project.clj
@@ -472,13 +472,13 @@
             "test-browser"               ["shell" "scripts/test-browser.sh"]                                                  ; this will run browser tests against fully optimized dirac extension (release build)
             "test-browser-dev"           ["shell" "scripts/test-browser-dev.sh"]                                              ; this will run browser tests against unpacked dirac extension
 
-            "run-backend-tests-17"       ["with-profile" "+test-runner,+clojure17" "trampoline" "run" "-m" "dirac.tests.backend.runner"]
-            "run-backend-tests-18"       ["with-profile" "+test-runner,+clojure18" "trampoline" "run" "-m" "dirac.tests.backend.runner"]
-            "run-backend-tests-19"       ["with-profile" "+test-runner,+clojure19" "trampoline" "run" "-m" "dirac.tests.backend.runner"]
+            "run-backend-tests-17"       ["with-profile" "+test-runner,+clojure17" "run" "-m" "dirac.tests.backend.runner"]
+            "run-backend-tests-18"       ["with-profile" "+test-runner,+clojure18" "run" "-m" "dirac.tests.backend.runner"]
+            "run-backend-tests-19"       ["with-profile" "+test-runner,+clojure19" "run" "-m" "dirac.tests.backend.runner"]
 
             "run-browser-tests"          ["shell" "scripts/run-browser-tests.sh" "dirac.tests.browser.runner"]
             "run-browser-tests-dev"      ["shell" "scripts/run-browser-tests.sh" "dirac.tests.browser.runner/-dev-main"]
-            "run-browser-tests-agent"    ["with-profile" "+test-runner,+debugger-5005" "trampoline" "run" "-m" "dirac.tests.browser.runner/run-agent"]
+            "run-browser-tests-agent"    ["with-profile" "+test-runner,+debugger-5005" "run" "-m" "dirac.tests.browser.runner/run-agent"]
 
             "dev-browser-tests"          ["shell" "scripts/dev-browser-tests.sh"]
             "dev-dirac-sample"           ["shell" "scripts/dev-dirac-sample.sh"]
