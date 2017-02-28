a91b26c8ab1ce9c78c3b44b6b1f0de36a6c8b96f
Antonin Hildebrand <antonin@hildebrand.cz>
2017-02-28 23:31:38 +0100

    squash 'resources/unpacked/devtools' changes from 82a742e74..059eb33e9
    059eb33e9 DevTools: create only one uiSourceCode per URL per target.
    40cd340b2 DevTools: make coverage start/stop an action, use standard record button rendering
    8acce6cf9 [DevTools] Remove target.serviceWorkerManager field.
    df42b4d5d [DevTools] Add header for OOPIF's elements trees.
    b24abe0b0 DevTools: fix object popover title
    95995eb0c DevTools: Don't show CSS variables in ComputedStyleWidget unless showing all
    8da9d8568 [DevTools] show inlined shortcuts for go to location
    dc533618f [Devtools] A11y tree: preselect elements when traversing using the keyboard; press Enter to inspect
    c7be61ba8 [DevTools] Remove full screenshot from UI.
    6dd7479f7 DevTools: add support for JS coverage
    
    git-subtree-dir: resources/unpacked/devtools
    git-subtree-split: 059eb33e9e4fbecff443f31a21990d7f15548bcb

--- front_end/coverage/CoverageView.js
+++ front_end/coverage/CoverageView.js
@@ -8,7 +8,7 @@ Coverage.RangeUsage;
 /** @typedef {{styleSheetHeader: !SDK.CSSStyleSheetHeader, ranges: !Array<!Coverage.RangeUsage>}} */
 Coverage.StyleSheetUsage;
 
-/** @typedef {{url: string, totalSize: number, unusedSize: number, usedSize: number,
+/** @typedef {{url: string, size: (number|undefined), unusedSize: (number|undefined), usedSize: (number|undefined),
  *      ranges: !Array<!Coverage.RangeUsage>}} */
 Coverage.CoverageInfo;
 
@@ -21,10 +21,9 @@ Coverage.CoverageView = class extends UI.VBox {
     var toolbarContainer = this.contentElement.createChild('div', 'coverage-toolbar-container');
     var topToolbar = new UI.Toolbar('coverage-toolbar', toolbarContainer);
 
-    this._recordButton =
-        new UI.ToolbarToggle(Common.UIString('Start recording'), 'largeicon-resume', 'largeicon-pause');
-    this._recordButton.addEventListener(UI.ToolbarButton.Events.Click, () => this._toggleRecording(!this._isRecording));
-    topToolbar.appendToolbarItem(this._recordButton);
+    this._toggleRecordAction =
+        /** @type {!UI.Action }*/ (UI.actionRegistry.action('coverage.toggle-recording'));
+    topToolbar.appendToolbarItem(UI.Toolbar.createActionButton(this._toggleRecordAction));
 
     var clearButton = new UI.ToolbarButton(Common.UIString('Clear all'), 'largeicon-clear');
     clearButton.addEventListener(UI.ToolbarButton.Events.Click, this._reset.bind(this));
@@ -36,8 +35,6 @@ Coverage.CoverageView = class extends UI.VBox {
 
     this._statusToolbarElement = this.contentElement.createChild('div', 'coverage-toolbar-summary');
     this._statusMessageElement = this._statusToolbarElement.createChild('div', 'coverage-message');
-
-    this._isRecording = false;
   }
 
   _reset() {
@@ -52,17 +49,10 @@ Coverage.CoverageView = class extends UI.VBox {
     this._statusMessageElement.textContent = '';
   }
 
-  /**
-   * @param {boolean} enable
-   */
-  _toggleRecording(enable) {
-    if (enable === this._isRecording)
-      return;
+  _toggleRecording() {
+    var enable = !this._toggleRecordAction.toggled();
 
-    this._isRecording = enable;
-    this._recordButton.setToggled(this._isRecording);
-
-    if (this._isRecording)
+    if (enable)
       this._startRecording();
     else
       this._stopRecording();
@@ -76,114 +66,217 @@ Coverage.CoverageView = class extends UI.VBox {
     var cssModel = mainTarget.model(SDK.CSSModel);
     if (!cssModel)
       return;
-    this._recordButton.setTitle(Common.UIString('Stop recording'));
+    this._toggleRecordAction.setToggled(true);
     cssModel.startRuleUsageTracking();
+    mainTarget.profilerAgent().startPreciseCoverage();
 
     this._progressElement.textContent = Common.UIString('Recording...');
   }
 
-  _stopRecording() {
-    var mainTarget = SDK.targetManager.mainTarget();
-    if (!mainTarget)
-      return;
-
-    this._recordButton.setTitle(Common.UIString('Start recording'));
+  async _stopRecording() {
+    this._toggleRecordAction.setToggled(false);
     this._progressElement.textContent = Common.UIString('Fetching results...');
 
-    var cssModel = mainTarget.model(SDK.CSSModel);
-    if (!cssModel)
-      return;
+    var cssCoverageInfoPromise = this._stopCSSCoverage();
+    var jsCoverageInfoPromise = this._stopJSCoverage();
+    var cssCoverageInfo = await cssCoverageInfoPromise;
+    var jsCoverageInfo = await jsCoverageInfoPromise;
+    this._updateViews(cssCoverageInfo.concat(jsCoverageInfo));
+  }
 
-    cssModel.ruleListPromise().then(processRuleList.bind(this)).then(updateViews.bind(this));
+  /**
+   * @param {!Array<!Coverage.CoverageInfo>} coverageInfo
+   * @this {!Coverage.CoverageView}
+   */
+  _updateViews(coverageInfo) {
+    coverageInfo = Coverage.CoverageView._coalesceByURL(coverageInfo);
+    this._updateStats(coverageInfo);
+    this._updateGutter(coverageInfo);
+    this._coverageResultsElement.removeChildren();
+    this._listView.update(coverageInfo);
+    this._listView.show(this._coverageResultsElement);
+  }
 
-    /**
-     * @param {!Array<!SDK.CSSModel.RuleUsage>} ruleUsageList
-     * @this {!Coverage.CoverageView}
-     * @return {!Promise<!Array<!Coverage.CoverageInfo>>}
-     */
-    function processRuleList(ruleUsageList) {
-      /** @type {!Map<?SDK.CSSStyleSheetHeader, !Array<!Coverage.RangeUsage>>} */
-      var rulesByStyleSheet = new Map();
-      for (var rule of ruleUsageList) {
-        var styleSheetHeader = cssModel.styleSheetHeaderForId(rule.styleSheetId);
-        var ranges = rulesByStyleSheet.get(styleSheetHeader);
-        if (!ranges) {
-          ranges = [];
-          rulesByStyleSheet.set(styleSheetHeader, ranges);
-        }
-        ranges.push({range: Common.TextRange.fromObject(rule.range), wasUsed: rule.wasUsed});
+  /**
+   * @param {!Array<!Coverage.CoverageInfo>} coverageInfo
+   * @return {!Array<!Coverage.CoverageInfo>}
+   */
+  static _coalesceByURL(coverageInfo) {
+    coverageInfo.sort((a, b) => (a.url || '').localeCompare(b.url));
+    var result = [];
+    for (var entry of coverageInfo) {
+      if (!entry.url)
+        continue;
+      if (result.length && result.peekLast().url === entry.url) {
+        var lastEntry = result.peekLast();
+        lastEntry.size += entry.size;
+        lastEntry.usedSize += entry.usedSize;
+        lastEntry.unusedSize += entry.unusedSize;
+      } else {
+        result.push(entry);
       }
-      return Promise.all(
-          Array.from(rulesByStyleSheet.entries(), entry => this._convertToCoverageInfo(entry[0], entry[1])));
     }
+    return result;
+  }
 
-    /**
-     * @param {!Array<!Coverage.CoverageInfo>} coverageInfo
-     * @this {!Coverage.CoverageView}
-     */
-    function updateViews(coverageInfo) {
-      coverageInfo = coalesceByURL(coverageInfo);
-      this._updateStats(coverageInfo);
-      this._updateGutter(coverageInfo);
-      this._coverageResultsElement.removeChildren();
-      this._listView.update(coverageInfo);
-      this._listView.show(this._coverageResultsElement);
-    }
+  /**
+   * @return {!Promise<!Array<!Coverage.CoverageInfo>>}
+   */
+  async _stopJSCoverage() {
+    var mainTarget = SDK.targetManager.mainTarget();
+    var profilerAgent = mainTarget && mainTarget.profilerAgent();
+    if (!profilerAgent)
+      return [];
+    var coveragePromise = profilerAgent.takePreciseCoverage((error, result) => error ? [] : result);
+    profilerAgent.stopPreciseCoverage();
+    var rawCoverageData = await coveragePromise;
+    return Coverage.CoverageView._processJSCoverage(
+        /** @type !SDK.DebuggerModel */ (SDK.DebuggerModel.fromTarget(mainTarget)), rawCoverageData);
+  }
 
-    /**
-     * @param {!Array<!Coverage.CoverageInfo>} coverageInfo
-     * @return {!Array<!Coverage.CoverageInfo>}
-     */
-    function coalesceByURL(coverageInfo) {
-      coverageInfo.sort((a, b) => (a.url || '').localeCompare(b.url));
-      var result = [];
-      for (var entry of coverageInfo) {
-        if (!entry.url)
-          continue;
-        if (result.length && result.peekLast().url === entry.url) {
-          var lastEntry = result.peekLast();
-          lastEntry.size += entry.size;
-          lastEntry.usedSize += entry.usedSize;
-          lastEntry.unusedSize += entry.unusedSize;
-        } else {
-          result.push(entry);
+  /**
+   * @param {!SDK.DebuggerModel} debuggerModel
+   * @param {!Array<!Protocol.Profiler.ScriptCoverage>} scriptsCoverage
+   * @return {!Promise<!Array<!Coverage.CoverageInfo>>}
+   */
+  static async _processJSCoverage(debuggerModel, scriptsCoverage) {
+    var promises = [];
+    for (var entry of scriptsCoverage) {
+      var ranges = [];
+      var script = debuggerModel.scriptForId(entry.scriptId);
+      if (!script)
+        continue;
+      for (var func of entry.functions) {
+        for (var range of func.ranges) {
+          var textRange = new Common.TextRange(
+              range.startLineNumber, range.startColumnNumber, range.endLineNumber, range.endColumnNumber);
+          ranges.push({range: textRange, wasUsed: !!range.count});
         }
       }
-      return result;
+      promises.push(convertToCoverageInfo(script, ranges));
+    }
+    return Promise.all(promises);
+
+    /**
+     * @param {!SDK.Script} script
+     * @param {!Array<!Coverage.RangeUsage>} ranges
+     * @return {!Promise<!Coverage.CoverageInfo>}
+     */
+    function convertToCoverageInfo(script, ranges) {
+      return script.requestContent().then(
+          content => Coverage.CoverageView._coverageInfoForText(
+              script.contentURL(), script.lineOffset, script.columnOffset, content, ranges));
     }
   }
 
   /**
-   * @param {!SDK.CSSStyleSheetHeader} styleSheetHeader
-   * @param {!Array<!Coverage.RangeUsage>} ranges
-   * @return {!Promise<!Coverage.CoverageInfo>}
+   * @return {!Promise<!Array<!Coverage.CoverageInfo>>}
    */
-  _convertToCoverageInfo(styleSheetHeader, ranges) {
+  async _stopCSSCoverage() {
+    var mainTarget = SDK.targetManager.mainTarget();
+    const cssModel = mainTarget && mainTarget.model(SDK.CSSModel);
+    if (!cssModel)
+      return Promise.resolve([]);
+
+    var rawCoverageData = await cssModel.ruleListPromise();
+    return Coverage.CoverageView._processCSSCoverage(
+        /** @type !SDK.CSSModel */ (cssModel), rawCoverageData);
+  }
+
+  /**
+   * @param {!SDK.CSSModel} cssModel
+   * @param {!Array<!SDK.CSSModel.RuleUsage>} ruleUsageList
+   * @return {!Promise<!Array<!Coverage.CoverageInfo>>}
+   */
+  static async _processCSSCoverage(cssModel, ruleUsageList) {
+    /** @type {!Map<?SDK.CSSStyleSheetHeader, !Array<!Coverage.RangeUsage>>} */
+    var rulesByStyleSheet = new Map();
+    for (var rule of ruleUsageList) {
+      var styleSheetHeader = cssModel.styleSheetHeaderForId(rule.styleSheetId);
+      var ranges = rulesByStyleSheet.get(styleSheetHeader);
+      if (!ranges) {
+        ranges = [];
+        rulesByStyleSheet.set(styleSheetHeader, ranges);
+      }
+      var textRange = new Common.TextRange(
+          rule.range.startLine + styleSheetHeader.startLine,
+          rule.range.startColumn + (rule.range.startLine ? 0 : styleSheetHeader.startColumn),
+          rule.range.endLine + styleSheetHeader.startLine,
+          rule.range.endColumn + (rule.range.endLine ? 0 : styleSheetHeader.startColumn));
+      ranges.push({range: textRange, wasUsed: rule.wasUsed});
+    }
+    return Promise.all(Array.from(rulesByStyleSheet.entries(), entry => convertToCoverageInfo(entry[0], entry[1])));
+
+    /**
+     * @param {!SDK.CSSStyleSheetHeader} styleSheetHeader
+     * @param {!Array<!Coverage.RangeUsage>} ranges
+     * @return {!Promise<!Coverage.CoverageInfo>}
+     */
+    function convertToCoverageInfo(styleSheetHeader, ranges) {
+      return styleSheetHeader.requestContent().then(
+          content => Coverage.CoverageView._coverageInfoForText(
+              styleSheetHeader.sourceURL, styleSheetHeader.startLine, styleSheetHeader.startColumn, content, ranges));
+    }
+  }
+
+  /**
+   * @param {string} url
+   * @param {number} startLine
+   * @param {number} startColumn
+   * @param {?string} content
+   * @param {!Array<!Coverage.RangeUsage>} ranges
+   * @return {!Coverage.CoverageInfo}
+   */
+  static _coverageInfoForText(url, startLine, startColumn, content, ranges) {
     var coverageInfo = {
-      url: styleSheetHeader.sourceURL,
+      url: url,
       ranges: ranges,
     };
-    return styleSheetHeader.requestContent().then(content => {
-      if (!content)
-        return coverageInfo;
-      var text = new Common.Text(content);
-      var usedSize = 0;
-      var unusedSize = 0;
-      for (var entry of ranges) {
-        var range = entry.range;
-        var size = text.offsetFromPosition(range.endLine, range.endColumn) -
-            text.offsetFromPosition(range.startLine, range.startColumn);
-        if (entry.wasUsed)
-          usedSize += size;
-        else
-          unusedSize += size;
-      }
-      coverageInfo.size = content.length;
-      coverageInfo.usedSize = usedSize;
-      coverageInfo.unusedSize = unusedSize;
-
+    if (!content)
       return coverageInfo;
+
+    var text = new Common.Text(content);
+    var offsetRanges = ranges.map(r => {
+      var range = r.range.relativeTo(startLine, startColumn);
+      return {
+        start: text.offsetFromPosition(range.startLine, range.startColumn),
+        end: text.offsetFromPosition(range.endLine, range.endColumn),
+        wasUsed: r.wasUsed
+      };
     });
+
+    var stack = [];
+    offsetRanges.sort((a, b) => a.start - b.start);
+    for (var entry of offsetRanges) {
+      while (stack.length && stack.peekLast().end <= entry.start)
+        stack.pop();
+
+      entry.ownSize = entry.end - entry.start;
+      var top = stack.peekLast();
+      if (top) {
+        if (top.end < entry.end) {
+          console.assert(
+              false, `Overlapping coverage entries in ${url}: ${top.start}-${top.end} vs. ${entry.start}-${entry.end}`);
+        }
+        top.ownSize -= entry.ownSize;
+      }
+      stack.push(entry);
+    }
+
+    var usedSize = 0;
+    var unusedSize = 0;
+    for (var entry of offsetRanges) {
+      if (entry.wasUsed)
+        usedSize += entry.ownSize;
+      else
+        unusedSize += entry.ownSize;
+    }
+
+    coverageInfo.size = content.length;
+    coverageInfo.usedSize = usedSize;
+    coverageInfo.unusedSize = unusedSize;
+
+    return coverageInfo;
   }
 
   /**
@@ -250,3 +343,23 @@ Coverage.CoverageView.LineDecorator = class {
 };
 
 Coverage.CoverageView.LineDecorator.type = 'coverage';
+
+/**
+ * @implements {UI.ActionDelegate}
+ */
+Coverage.CoverageView.RecordActionDelegate = class {
+  /**
+   * @override
+   * @param {!UI.Context} context
+   * @param {string} actionId
+   * @return {boolean}
+   */
+  handleAction(context, actionId) {
+    var coverageViewId = 'coverage';
+    UI.viewManager.showView(coverageViewId)
+        .then(() => UI.viewManager.view(coverageViewId).widget())
+        .then(widget => /** @type !Coverage.CoverageView} */ (widget)._toggleRecording());
+
+    return true;
+  }
+};
