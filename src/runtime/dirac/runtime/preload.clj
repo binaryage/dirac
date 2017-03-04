444cc1f37aa1d41bd5c6a2c1640007326bf3fac5
Antonin Hildebrand <antonin@hildebrand.cz>
2017-03-04 20:12:57 +0100

    runtime: preload config must be treated as data
    naive export could case evaluation of some config parts,
    e.g. :reveal-url-request-handler (fn [config url line column])
    would not survive

--- src/runtime/dirac/runtime/preload.clj
+++ src/runtime/dirac/runtime/preload.clj
@@ -5,4 +5,4 @@
     (get-in @cljs.env/*compiler* [:options :external-config :dirac.runtime/config])))                                         ; https://github.com/bhauman/lein-figwheel/commit/80f7306bf5e6bd1330287a6f3cc259ff645d899b
 
 (defmacro gen-config []
-  (or (read-config) {}))
+  `'~(or (read-config) {}))
