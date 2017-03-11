ba2b8c6f6c756fc23cb39541066c735951eedb9a
Antonin Hildebrand <antonin@hildebrand.cz>
2017-03-11 17:13:40 +0100

    scripts: privileged mode is no longer needed in docker-run.sh

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -10,18 +10,13 @@ pushd "$DOCKER_TESTS_DIR"
 docker volume create --name "dirac-data-root" > /dev/null
 docker volume create --name "dirac-data-var-cache-apt" > /dev/null
 
-# for shm-size, see https://bugs.chromium.org/p/chromedriver/issues/detail?id=732#c31
 # for dns see https://forums.docker.com/t/intermittent-dns-resolving-issues/9584/17?u=drwin
 docker run \
   --name "dirac-job" \
   --dns=8.8.8.8 \
-  --privileged \
-  --shm-size=1g \
-  --net=host \
   -v "dirac-data-root:/root" \
   -v "dirac-data-var-cache-apt:/var/cache/apt" \
   -v "$DIRAC_DOCKER_TEST_STAGE_DIR:/root/binaryage/dirac" \
-  -e JAVA_OPTS='-Xmx1g' \
   --rm \
   -it dirac \
   "$@"
