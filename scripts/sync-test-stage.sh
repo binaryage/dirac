eeeca72157da5ecddbfe9060266f878a55b589c7
Antonin Hildebrand <antonin@hildebrand.cz>
2017-03-11 14:03:59 +0100

    scripts: fix broken default parameter DIRAC_TEST_STAGE_DIR

--- scripts/sync-test-stage.sh
+++ scripts/sync-test-stage.sh
@@ -3,7 +3,7 @@
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 false && source _config.sh # never executes, this is here just for IntelliJ Bash support to understand our sourcing
 
-TEST_STAGE=${1:-DIRAC_TEST_STAGE_DIR}
+TEST_STAGE=${1:-$DIRAC_TEST_STAGE_DIR}
 
 pushd "$ROOT"
 
