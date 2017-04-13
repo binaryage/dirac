dcdc7bd5297fc444b132da4da2705f8f92c368b1
Antonin Hildebrand <antonin@hildebrand.cz>
2017-04-13 23:47:49 +0200

    project: bump clojurescript to "1.9.518"

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
         :url  "https://github.com/binaryage/dirac"}
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha15" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.473" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.518" :scope "provided"]
                  [org.clojure/core.async "0.3.442"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.cli "0.3.5"]
