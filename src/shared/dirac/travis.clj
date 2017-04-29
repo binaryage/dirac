2ddb2270b6c20e2436076f1e629e46fdfdb8bdc2
Antonin Hildebrand <antonin@hildebrand.cz>
2017-04-29 23:42:04 +0200

    shared: instruct travis to add timing info to folded commands

--- src/shared/dirac/travis.clj
+++ src/shared/dirac/travis.clj
@@ -1,16 +1,44 @@
 (ns dirac.travis
-  (:require [cuerdas.core :as cuerdas]))
+  (:require [cuerdas.core :as cuerdas])
+  (:import (java.time Instant)))
 
 (def ANSI_CLEAR "\033[0K")
+(def CLEAR_LINE (str "\r" ANSI_CLEAR))
+
+; -- raw commands -----------------------------------------------------------------------------------------------------------
 
 (defn travis-fold-command [action name]
-  (str "travis_fold:" action ":" name "\r" ANSI_CLEAR))
+  (str "travis_fold:" action ":" name CLEAR_LINE))
 
-(defmacro with-travis-fold [title name & body]
+(defn travis-start-time-command [timer-id]
+  (str "travis_time:start:" timer-id CLEAR_LINE))
+
+(defn travis-end-time-command [timer-id start-time end-time]
+  (str "travis_time:end:" timer-id ":start=" start-time ",finish=" end-time ",duration=" (- end-time start-time) CLEAR_LINE))
+
+; -- wrappers ---------------------------------------------------------------------------------------------------------------
+
+(defn wrap-with-timing [forms]
+  (let [timer-id (name (gensym "timer"))]
+    `(let [start-time# (.getNano (Instant/now))]
+       (print (travis-start-time-command ~timer-id))
+       (try
+         ~@forms
+         (finally
+           (let [end-time# (.getNano (Instant/now))]
+             (print (travis-end-time-command ~timer-id start-time# end-time#))))))))
+
+(defn wrap-with-folding [title name forms]
   `(let [sanitized-name# (cuerdas/kebab ~name)]
      (print (travis-fold-command "start" sanitized-name#))
      (println ~title)
      (try
-       ~@body
+       ~@forms
        (finally
          (print (travis-fold-command "end" sanitized-name#))))))
+
+; -- public api -------------------------------------------------------------------------------------------------------------
+
+(defmacro with-travis-fold [title name & body]
+  (wrap-with-timing
+    (list (wrap-with-folding title name body))))
