0e901133fa02811a856b9b23c058baf35f7100d4
Antonin Hildebrand <antonin@hildebrand.cz>
2018-03-17 18:05:15 +0100

    runtime: fix a typo 'boostrap' -> 'bootstrap', affects some private apis

--- src/runtime/dirac/runtime/bootstrap.cljs
+++ src/runtime/dirac/runtime/bootstrap.cljs
@@ -1,12 +1,12 @@
 (ns dirac.runtime.bootstrap
-  (:require [clojure.browser.repl :as brepl]))
+  (:require [clojure.browser.repl :as browser-repl]))
 
-; boostrap must be called after document finishes loading, because there may be some code coming after the bootstrap call
+; bootstrap must be called after document finishes loading, because there may be some code coming after the bootstrap call
 ; which expects goog.require to work synchronously, like @bendlas discovered
 
-(def ^:dynamic *boostrapped?* false)
-(def ^:dynamic *boostrap-timeout* 100)
-(def ^:dynamic *boostrap-listeners* (array))                                                                                  ; a native array of plain functions
+(def ^:dynamic *bootstrapped?* false)
+(def ^:dynamic *bootstrap-timeout* 100)
+(def ^:dynamic *bootstrap-listeners* (array))                                                                                 ; a native array of plain functions
 
 ; -- async bootstrap wrapper ------------------------------------------------------------------------------------------------
 
@@ -14,19 +14,19 @@
   (doseq [listener listeners]
     (listener)))
 
-(defn boostrap-if-needed! []
-  (when-not *boostrapped?*
-    (brepl/bootstrap)
-    (set! *boostrapped?* true))
-  (notify-listeners! *boostrap-listeners*)
-  (set! *boostrap-listeners* (array)))
+(defn bootstrap-if-needed! []
+  (when-not *bootstrapped?*
+    (browser-repl/bootstrap)
+    (set! *bootstrapped?* true))
+  (notify-listeners! *bootstrap-listeners*)
+  (set! *bootstrap-listeners* (array)))
 
 (defn call-after-document-finished-loading [f timeout]
   ; Our strategy:
   ;   If document is already loaded, we simply execute our function.
   ;   If not, we try to schedule another check with timeout 0ms (fast path).
   ;   If still not loaded, we repeatedly schedule future checks with timeout 100ms (until loaded).
-  ;   The magic constant *boostrap-timeout* (100ms) should be good enough for our bootstrapping use-case:
+  ;   The magic constant *bootstrap-timeout* (100ms) should be good enough for our bootstrapping use-case:
   ;     1. we don't want to starve event queue in case of slow loading, hence non-zero timeouts
   ;     2. but we also want to minimize the risk of potentially missing first eval request expecting bootstrapped env
   ;        note: our own `evaluate-javascript` has a guard, but there could be some other code doing their own evals
@@ -35,7 +35,7 @@
   ; We do not want to hook DOMContentLoaded or onreadystatechange events which could interfere with user's own handlers.
   ;
   (if (= (.-readyState js/document) "loading")
-    (js/setTimeout #(call-after-document-finished-loading f *boostrap-timeout*) timeout)
+    (js/setTimeout #(call-after-document-finished-loading f *bootstrap-timeout*) timeout)
     (f)))
 
 (defn bootstrap!
@@ -43,8 +43,8 @@
   in goog.base to support re-loading of namespaces after page load.
 
   Note that this function might do its job asynchronously if at the time of calling the document is still loading.
-  You may provide a callback which will be called immediatelly after bootstrapping happens.
-  It has no effect if called after bootstrapping has been already done. Only the callback is called immediatelly."
+  You may provide a callback which will be called immediately after bootstrapping happens.
+  It has no effect if called after bootstrapping has been already done. Only the callback is called immediately."
   ([] (bootstrap! nil))
   ([callback]
     ; patching goog methods before document finished loading can unexpectedly break following goog.require code:
@@ -53,7 +53,7 @@
    (when (some? callback)
      (assert (fn? callback) (str "The callback parameter to clojure.browser.repl/bootstrap expected to be a function."
                                  "Got " (type callback) " instead."))
-     (.push *boostrap-listeners* callback))
+     (.push *bootstrap-listeners* callback))
    (if-not (exists? js/document)
-     (boostrap-if-needed!)                                                                                                    ; in node.js
-     (call-after-document-finished-loading boostrap-if-needed! 0))))                                                          ; in browser
+     (bootstrap-if-needed!)                                                                                                   ; in node.js
+     (call-after-document-finished-loading bootstrap-if-needed! 0))))                                                         ; in browser
