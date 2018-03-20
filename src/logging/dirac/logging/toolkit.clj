d65634820231efc7705ae8153bd7cc5938c180bc
Antonin Hildebrand <antonin@hildebrand.cz>
2018-03-20 14:38:23 +0100

    logging: implement prefix construction from env inside logging toolkit

--- src/logging/dirac/logging/toolkit.clj
+++ src/logging/dirac/logging/toolkit.clj
@@ -1,6 +1,7 @@
 (ns dirac.logging.toolkit
   "Common code for logging into js console"
-  (:require [dirac.shared.utils :refer [advanced-mode?]]))
+  (:require [dirac.shared.utils :refer [advanced-mode?]]
+            [clojure.string :as string]))
 
 (defn get-prefix-style [opts]
   (let [fg-color (get opts :fg-color "white")
@@ -11,15 +12,32 @@
          "padding:0px 3px;"
          "border-radius:2px;")))
 
+(defn massage-ns-name [ns-name]
+  (-> ns-name
+      (string/replace-first #"^dirac\." "D:")
+      (string/replace-first #"^marion\." "M:")))
+
+(defn compute-prefix [opts]
+  (if-some [env (get opts :env)]
+    (let [ns-name (name (:name (:ns env)))
+          prefix (massage-ns-name ns-name)]
+      prefix)
+    "?"))
+
+(defn get-or-compute-prefix [opts]
+  (if-some [prefix (get opts :prefix)]
+    prefix
+    (compute-prefix opts)))
+
 (defn gen-dev-console-log [method args opts]
   (let [api (str "console." method)
-        prefix (get opts :prefix "?")
+        prefix (get-or-compute-prefix opts)
         prefix-formatting ["%c%s" (get-prefix-style opts) prefix]]
     `(oops.core/gcall!+ ~api ~@prefix-formatting ~@args)))
 
 (defn gen-raw-console-log [method args opts]
   (let [api (str "console." method)
-        prefix (get opts :prefix "?")
+        prefix (get-or-compute-prefix opts)
         printed-args (map (fn [arg] `(dirac.shared.utils/pprint-str ~arg)) args)
         msg `(str "[" ~prefix "] " ~@printed-args "---")]
     `(oops.core/gcall!+ ~api ~msg)))
