8779a1d17bfb76f6fe1e16bda55f8c0241283b5b
Antonin Hildebrand <antonin@hildebrand.cz>
2018-03-20 13:25:28 +0100

    init figwheel only when dev mode and not testing

--- test/marion/src/background/marion/figwheel.cljs
+++ test/marion/src/background/marion/figwheel.cljs
@@ -1,10 +1,12 @@
 (ns marion.figwheel
   (:require [figwheel.client :as figwheel]
-            [dirac.shared.utils :refer [runonce]]))
+            [dirac.shared.utils :refer [runonce when-not-advanced-mode when-not-dirac-test-mode]]))
 
 ; -------------------------------------------------------------------------------------------------------------------
 ; has to be included before boot
 
-(runonce
-  (figwheel/start
-    {:websocket-url "ws://localhost:7200/figwheel-ws"}))
+(when-not-advanced-mode
+  (when-not-dirac-test-mode
+    (runonce
+      (figwheel/start
+        {:websocket-url "ws://localhost:7200/figwheel-ws"}))))
