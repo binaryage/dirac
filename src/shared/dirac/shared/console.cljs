45c006eeee1561b31a0dab73fc33bc81f4754251
Antonin Hildebrand <antonin@hildebrand.cz>
2018-03-20 13:46:41 +0100

    modernize dirac.shared.console

--- src/shared/dirac/shared/console.cljs
+++ src/shared/dirac/shared/console.cljs
@@ -1,22 +1,28 @@
 (ns dirac.shared.console
-  (:require [goog.log :as glog])
-  (:import goog.debug.Console))
+  (:require [goog.log :as goog-log]
+            [goog.debug :as goog-debug]
+            [goog.debug.Logger.Level :as level]
+            [oops.core :refer [oget oset! ocall ocall! gcall oapply]]))
 
 ; taken from https://gist.github.com/caskolkm/39d823f5bac7051d3062
 
-(def logger
-  (glog/getLogger "app"))
+(def logger (goog-log/getLogger "app"))
 
-(def levels {:severe  goog.debug.Logger.Level.SEVERE
-             :warning goog.debug.Logger.Level.WARNING
-             :info    goog.debug.Logger.Level.INFO
-             :config  goog.debug.Logger.Level.CONFIG
-             :fine    goog.debug.Logger.Level.FINE
-             :finer   goog.debug.Logger.Level.FINER
-             :finest  goog.debug.Logger.Level.FINEST})
+(def levels
+  {:severe  level/SEVERE
+   :warning level/WARNING
+   :info    level/INFO
+   :config  level/CONFIG
+   :fine    level/FINE
+   :finer   level/FINER
+   :finest  level/FINEST})
+
+(defn make-console []
+  (goog-debug/Console.))
 
 (defn log-to-console! []
-  (.setCapturing (goog.debug.Console.) true))
+  (ocall! (make-console) "setCapturing" true))
 
-(defn set-level! [level]
-  (.setLevel logger (get levels level (:info levels))))
+(defn set-level! [level-keyword]
+  (let [wanted-level (get levels level-keyword (:info levels))]
+    (ocall! logger "setLevel" wanted-level)))
