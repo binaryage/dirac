a55ae170da351a35fbeaf1883c0354e3871fb19e
Antonin Hildebrand <antonin@hildebrand.cz>
2018-05-28 11:51:44 +0200

    Revert "devtools: disable consoleBelowPrompt experiment for now"
    902b036260d6764a60bbbfb84dadeda789ce44b5

--- resources/unpacked/devtools/front_end/main/Main.js
+++ resources/unpacked/devtools/front_end/main/Main.js
@@ -151,7 +151,7 @@ Main.Main = class {
     }
 
     Runtime.experiments.setDefaultExperiments(
-        ['colorContrastRatio', 'stepIntoAsync', 'oopifInlineDOM' /* darwin:, 'consoleBelowPrompt' */]);
+        ['colorContrastRatio', 'stepIntoAsync', 'oopifInlineDOM', 'consoleBelowPrompt']);
   }
 
   /**
