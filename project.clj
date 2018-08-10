20d9eee49e516281016b96efe320de1117f805b4
Antonin Hildebrand <antonin@hildebrand.cz>
2018-08-10 21:51:12 +0200

    project: bump selenium deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 (def figwheel-version "0.5.16")
 (def selected-clojure-version "1.9.0")
 (def selected-clojurescript-version "1.10.339")
-(def selenium-version "3.13.0")
+(def selenium-version "3.14.0")
 
 (def provided-deps
   [['org.clojure/clojure selected-clojure-version :scope "provided"]
