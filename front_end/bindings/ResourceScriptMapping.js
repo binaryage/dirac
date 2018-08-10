ef7e701b4b8cb05a89bd05d7aa21c65223a3c0f5
Antonin Hildebrand <antonin@hildebrand.cz>
2018-08-10 21:20:51 +0200

    squash 'resources/unpacked/devtools' changes from 561b74374..3debfb2ac
    3debfb2ac Fix devtools.timeline DOM GC slices
    856889db5 [DevTools] migrate CallStackSidebarPane to live locations
    fa45e5ba0 DevTools: fix regressed Object popovers, dark theme context
    bda612e31 DevTools: Make SoftContextMenu accessible
    6c7d7cfca [DevTools] automapping should be less eager
    b3382f698 DevTools: Report modules in Heap snapshot text view.
    73b30b6ae [DevTools] DebuggerWorkspaceBinding.uiLocationToRawLocations
    0f5e44fc9 [DevTools] remove separate event for FailedToParseScriptSource
    11cf1a6d8 DevTools: catch FormatterWorker acorn errors
    c935ee206 DevTools: New look for SoftContextMenus
    33f9509c6 Developer Tools/Network - fixed copy as fetch not emitting headers.
    e032dac56 [DevTools] simplify script with source map blackboxing
    63010f5f7 DevTools: expanded Objects hide overflow
    143ceaae0 DevTools: follow up to r578918, adding missing file manager changes.
    1deb53280 DevTools: make timeline respect main target that does not have tracing capability.
    
    git-subtree-dir: resources/unpacked/devtools
    git-subtree-split: 3debfb2ac4df22e99462345d027699e17f5db0c4

--- front_end/bindings/ResourceScriptMapping.js
+++ front_end/bindings/ResourceScriptMapping.js
@@ -53,8 +53,6 @@ Bindings.ResourceScriptMapping = class {
     this._eventListeners = [
       this._debuggerModel.addEventListener(SDK.DebuggerModel.Events.ParsedScriptSource, this._parsedScriptSource, this),
       this._debuggerModel.addEventListener(
-          SDK.DebuggerModel.Events.FailedToParseScriptSource, this._parsedScriptSource, this),
-      this._debuggerModel.addEventListener(
           SDK.DebuggerModel.Events.GlobalObjectCleared, this._globalObjectCleared, this),
       runtimeModel.addEventListener(
           SDK.RuntimeModel.Events.ExecutionContextDestroyed, this._executionContextDestroyed, this),
@@ -113,18 +111,18 @@ Bindings.ResourceScriptMapping = class {
    * @param {!Workspace.UISourceCode} uiSourceCode
    * @param {number} lineNumber
    * @param {number} columnNumber
-   * @return {?SDK.DebuggerModel.Location}
+   * @return {!Array<!SDK.DebuggerModel.Location>}
    */
-  uiLocationToRawLocation(uiSourceCode, lineNumber, columnNumber) {
+  uiLocationToRawLocations(uiSourceCode, lineNumber, columnNumber) {
     const scriptFile = this._uiSourceCodeToScriptFile.get(uiSourceCode);
     if (!scriptFile)
-      return null;
+      return [];
     const script = scriptFile._script;
     if (script.isInlineScriptWithSourceURL()) {
-      return this._debuggerModel.createRawLocation(
-          script, lineNumber + script.lineOffset, lineNumber ? columnNumber : columnNumber + script.columnOffset);
+      return [this._debuggerModel.createRawLocation(
+          script, lineNumber + script.lineOffset, lineNumber ? columnNumber : columnNumber + script.columnOffset)];
     }
-    return this._debuggerModel.createRawLocation(script, lineNumber, columnNumber);
+    return [this._debuggerModel.createRawLocation(script, lineNumber, columnNumber)];
   }
 
   /**
