c28bd18840e8aa53a8936ad8de900e247208bb24
Antonin Hildebrand <antonin@hildebrand.cz>
2018-11-05 18:46:44 +0100

    devtools: regenerate protocol-related files

--- resources/unpacked/devtools/front_end/SupportedCSSProperties.js
+++ resources/unpacked/devtools/front_end/SupportedCSSProperties.js
@@ -1,5 +1,5 @@
-Protocol.BakedSupportedCSSPropertiesChromeTag='72.0.3593.2';
-Protocol.BakedSupportedCSSPropertiesChromeRev='f4225c913f3aef75d56ca12f7da5b32e0752d5c1';
+Protocol.BakedSupportedCSSPropertiesChromeTag='72.0.3602.1';
+Protocol.BakedSupportedCSSPropertiesChromeRev='85aa920b45323e92e5d2dfb09ce61109c52147c9';
 Protocol.BakedSupportedCSSProperties=''
 + '[{"name": "-webkit-app-region"},\n'
 + ' {"name": "-webkit-appearance"},\n'
