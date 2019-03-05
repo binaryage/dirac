b8c5f862e87858b999c19fb679e0bf7fcddbb363
Antonin Hildebrand <antonin@hildebrand.cz>
2019-03-05 19:44:33 +0100

    project: update deps

--- project.clj
+++ project.clj
@@ -25,7 +25,7 @@
 (def test-deps
   [; we cannot use :dependencies under individual profiles because Cursive recognizes only root level
    ; thus we mark extra deps with :scope "test" and filter them later when producing jar library
-   ['binaryage/oops "0.6.4" :scope "test"]
+   ['binaryage/oops "0.7.0" :scope "test"]
    ['binaryage/chromex "0.7.2" :scope "test"]
    ['binaryage/devtools "0.9.10" :scope "test"]
    ['environ "1.1.0" :scope "test"]
