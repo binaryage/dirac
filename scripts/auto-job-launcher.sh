33ddbb8843cfae94be37ef56484d9c6840a8b35b
Antonin Hildebrand <antonin@hildebrand.cz>
2019-06-29 02:21:30 +0200

    scripts: review launchd scripts

--- scripts/auto-job-launcher.sh
+++ scripts/auto-job-launcher.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
+set -ex
+
+# this will combine stdout and stderr and tee logs into syslog as well
+# see http://elevated-dev.com/TechTips/Launchd%20&%20Logging/
+exec 1> >(tee >(logger -t "chromex auto-job")) 2>&1
+
+die_if_dirty_working_copy () {
+  if [[ -n "$(git status -uno --porcelain)" ]]; then
+    echo "working copy is not clean in '$(pwd)'"
+    exit 1
+  fi
+}
+
 # ensure we start in scripts folder
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-SCRIPTS=`pwd`
+ROOT="$(pwd)"
+SCRIPTS="$ROOT/scripts"
 
-"$SCRIPTS/auto-job.sh" 2>&1
+# update our code to latest
+git fetch origin
+git checkout -B master origin/master
+
+# run the job
+exec "$SCRIPTS/auto-job.sh"
--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 echo "-----------------------------------------------------------------------------------------------------------------------"
 echo "running ${BASH_SOURCE[0]} on $(date)"
 
@@ -15,37 +17,34 @@ finish () {
 }
 trap finish EXIT
 
-source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
-false && source _config.sh # never executes, this is here just for IntelliJ Bash support to understand our sourcing
+cd "$(dirname "${BASH_SOURCE[0]}")"
+source _config.sh
 
 # prepare oracle
 mkdir -p "$ROOT_TMP_DIR"
 ORACLE_CHECKOUT_DIR="$ROOT_TMP_DIR/docker-chromium-oraculum"
 if [[ -d "$ORACLE_CHECKOUT_DIR" ]]; then
-  pushd "$ORACLE_CHECKOUT_DIR"
+  cd "$ORACLE_CHECKOUT_DIR"
   git fetch
   git checkout master
   git reset --hard origin/master
   git clean -fd
-  popd
 else
-  pushd "$ROOT_TMP_DIR"
+  cd "$ROOT_TMP_DIR"
   git clone https://github.com/binaryage/docker-chromium-oraculum.git
-  popd
 fi
 
 # get latest chromium revision and other info
-pushd "$ORACLE_CHECKOUT_DIR"
+cd "$ORACLE_CHECKOUT_DIR"
 ./oraculum.sh prune-cache
 ./oraculum.sh build
 CHROMIUM_REVISION=$(./oraculum.sh latest-revision)
-CHROMIUM_VERSION=$(./oraculum.sh version ${CHROMIUM_REVISION})
-CHROMIUM_REPO_URL=$(./oraculum.sh describe ${CHROMIUM_REVISION} redirect_url)
-CHROMIUM_DOWNLOAD_URL=$(./oraculum.sh download-link ${CHROMIUM_REVISION})
-popd
+CHROMIUM_VERSION=$(./oraculum.sh version "${CHROMIUM_REVISION}")
+CHROMIUM_REPO_URL=$(./oraculum.sh describe "${CHROMIUM_REVISION}" redirect_url)
+CHROMIUM_DOWNLOAD_URL=$(./oraculum.sh download-link "${CHROMIUM_REVISION}")
 
 # checkout or create health-check branch
-pushd "$ROOT"
+cd "$ROOT"
 
 # configure repo
 git config user.email "bot@binaryage.com"
@@ -54,17 +53,13 @@ git config user.name "BinaryAge Bot"
 git checkout master
 git pull origin
 
-TAG=${1:-`git describe --tags --match "v*" --abbrev=0 master`}
+TAG=${1:-$(git describe --tags --match "v*" --abbrev=0 master)}
 
 echo "TAG=$TAG"
 
-set +e
-git rev-parse --verify health-check
-if [[ $? -ne 0 ]]; then
-  set -e
+if ! git rev-parse --verify health-check; then
   git checkout -b health-check "$TAG"
 else
-  set -e
   git checkout health-check
 fi
 
@@ -88,5 +83,3 @@ fi
 
 # return to master branch
 git checkout master
-
-popd
