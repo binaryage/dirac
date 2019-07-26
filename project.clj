2ab48c13a2a17fe7225f45fe46da6719336a0a7d
Antonin Hildebrand <antonin@hildebrand.cz>
2019-07-26 12:09:01 +0200

    version 1.3.10

--- project.clj
+++ project.clj
@@ -59,7 +59,7 @@
 (def lib-deps (concat provided-deps required-deps))
 (def all-deps (concat lib-deps test-deps))
 
-(defproject binaryage/dirac "1.3.9"
+(defproject binaryage/dirac "1.3.10"
   :description "Dirac DevTools - a Chrome DevTools fork for ClojureScript developers."
   :url "https://github.com/binaryage/dirac"
   :license {:name         "MIT License"
