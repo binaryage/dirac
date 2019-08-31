dc84d355d125e26cc5ed7fb50e51d21b0501d42b
Antonin Hildebrand <antonin@hildebrand.cz>
2019-09-01 00:48:58 +0200

    scripts: do not reconfigure git repos in auto scripts, use --author

--- scripts/diff-upstream.sh
+++ scripts/diff-upstream.sh
@@ -87,9 +87,7 @@ git reset --hard "$LAST_MERGED_DEVTOOLS_SHA"
 git rm -rf *
 cp -R "$OURS/$DEVTOOLS_DIRAC_PREFIX"/* .
 git add -A
-git config user.email "bot@binaryage.com"
-git config user.name "BinaryAge Bot"
-git commit -m "devtools -> dirac as of $FULL_SHA"
+git commit -m "devtools -> dirac as of $FULL_SHA" --author="BinaryAge Bot <bot@binaryage.com>"
 
 if [[ ! -z "$FORCE_PUSH" ]] || confirm "Do you want to force push new $DIFF_BRANCH of github's Dirac repo? [y/N]" ; then
   git push -f git@github.com:binaryage/dirac.git "$DIFF_BRANCH"
