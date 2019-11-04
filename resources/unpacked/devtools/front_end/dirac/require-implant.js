c7fa4ed1007eebedf187ad10a2e611f2bde81229
Antonin Hildebrand <antonin@hildebrand.cz>
2019-11-05 00:18:13 +0100

    devtools: make require-implant no-op in release mode

--- resources/unpacked/devtools/front_end/dirac/require-implant.js
+++ resources/unpacked/devtools/front_end/dirac/require-implant.js
@@ -1,2 +1,6 @@
-goog.require("dirac.devtools");
-goog.require("dirac.implant");
+// in advanced build this code is not needed
+// and goog namespace is not present
+if (typeof goog !== "undefined") {
+  goog.require("dirac.devtools");
+  goog.require("dirac.implant");
+}
