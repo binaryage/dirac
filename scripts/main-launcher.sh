6c84d900a98c426b3cca9d1aa0966f2803f6900b
Antonin Hildebrand <antonin@hildebrand.cz>
2020-01-08 21:27:03 +0100

    scripts: update main launcher script

--- scripts/main-launcher.sh
+++ scripts/main-launcher.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-# this script tries to launch dirac.main from command line
+# this a wrapper script for launching dirac.main from command line
 #
 # usage ./main-launcher.sh [args]
 #
 
-set -e
+set -e -o pipefail
 
-VERSION=${DIRAC_MAIN_VERSION:-LATEST} # e.g. 0.1.0 or LATEST
-GROUP=${DIRAC_MAIN_GROUP:-binaryage}
-ARTIFACT=${DIRAC_MAIN_ARTIFACT:-dirac}
+DEPS='{:deps {binaryage/dirac {:mvn/version "1.5.0"} clj-logging-config {:mvn/version "1.9.12"}}}'
 
-DIRAC_DEP=${DIRAC_MAIN_DIRAC_DEP:-"{$GROUP/$ARTIFACT {:mvn/version \"$VERSION\"}}"}
-DEPS=${DIRAC_MAIN_DEPS:-"{:deps $DIRAC_DEP}"}
-CLI_NS=${DIRAC_MAIN_CLI_NS:-"dirac.main"}
-
-# $DEPS should be something like '{:deps {binaryage/dirac {:mvn/version "LATEST"}}}'
-set -x
-exec clojure -Sdeps "$DEPS" -m "$CLI_NS" "$@"
+#set -x
+exec clojure -Sdeps "$DEPS" -m dirac.main "$@"
