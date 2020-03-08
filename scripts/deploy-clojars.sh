ecc69be983690e00fa2a4ccbb829e1640544d932
Antonin Hildebrand <antonin@hildebrand.cz>
2020-03-08 18:37:42 +0100

    scripts: review deploy-clojars

--- scripts/deploy-clojars.sh
+++ scripts/deploy-clojars.sh
@@ -15,7 +15,7 @@ fi
 
 ./scripts/list-jar.sh "$LIB_PROFILES"
 
-LEIN_VERSION=`cat "$PROJECT_FILE" | grep "defproject" | cut -d' ' -f3 | cut -d\" -f2`
+LEIN_VERSION=$(grep "defproject" <"$PROJECT_FILE" | cut -d' ' -f3 | cut -d\" -f2)
 
 # http://stackoverflow.com/a/1885534/84283
 echo "Are you sure to publish version ${LEIN_VERSION}? [Yy]"
