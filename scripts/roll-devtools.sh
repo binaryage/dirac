192cb45ce22a0c3f55f47732d9a78444ce574bb9
Antonin Hildebrand <antonin@hildebrand.cz>
2020-11-08 17:45:39 +0100

    scripts: fix broken roll-devtools.sh

--- scripts/roll-devtools.sh
+++ scripts/roll-devtools.sh
@@ -4,13 +4,21 @@ set -e -o pipefail
 # shellcheck source=_config.sh
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 
+die_if_dirty_working_copy () {
+  if [[ -n "$(git status -uno --porcelain)" ]]; then
+    echo "working copy is not clean in '$(pwd)'"
+    exit 1
+  fi
+}
+
 cd "$ROOT"
 
 set -x
 
-die_if_dirty_working
-
 cd "$DEVTOOLS_FRONTEND_DIR"
+
+die_if_dirty_working_copy
+
 # assume:
 # git remote add official https://github.com/ChromeDevTools/devtools-frontend.git
 git fetch official
